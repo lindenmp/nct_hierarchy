9f1092aa114c89268a735f7b220920d58d02458a
Linden Parkes <lindenparkes@gmail.com>
2021-01-27 14:09:29 -0500

    init commit

---
+++ 1_code/0_get_sample.py
@@ -0,0 +1,324 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Essentials
+import os, sys, glob
+import pandas as pd
+import numpy as np
+import nibabel as nib
+import scipy.io as sio
+from tqdm import tqdm
+
+# Stats
+import scipy as sp
+from scipy import stats
+import statsmodels.api as sm
+import pingouin as pg
+
+# Plotting
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.rcParams['svg.fonttype'] = 'none'
+
+
+# In[2]:
+
+
+sys.path.append('/Users/lindenmp/Google-Drive-Penn/work/research_projects/pathlength_tuning/1_code/')
+from func import set_proj_env, my_get_cmap, rank_int
+
+
+# In[3]:
+
+
+parc_str = 'schaefer'
+parc_scale = 200
+edge_weight = 'streamlineCount'
+# parcel_names, parcel_loc, drop_parcels, num_parcels, yeo_idx, yeo_labels = set_proj_env(parc_str = parc_str, parc_scale = parc_scale, edge_weight = edge_weight)
+set_proj_env()
+
+
+# ### Setup directory variables
+
+# In[4]:
+
+
+print(os.environ['PIPELINEDIR'])
+if not os.path.exists(os.environ['PIPELINEDIR']): os.makedirs(os.environ['PIPELINEDIR'])
+
+
+# In[5]:
+
+
+outputdir = os.path.join(os.environ['PIPELINEDIR'], '0_get_sample', 'out')
+print(outputdir)
+if not os.path.exists(outputdir): os.makedirs(outputdir)
+
+
+# In[6]:
+
+
+figdir = os.path.join(os.environ['OUTPUTDIR'], 'figs')
+print(figdir)
+if not os.path.exists(figdir): os.makedirs(figdir)
+
+
+# # Load in demographic and symptom data
+
+# In[7]:
+
+
+# LTN and Health Status
+health = pd.read_csv(os.path.join(os.environ['DATADIR'], 'external/pncDataFreeze20170905/n1601_dataFreeze/health/n1601_health_20170421.csv'))
+# Protocol
+prot = pd.read_csv(os.path.join(os.environ['DATADIR'], 'external/pncDataFreeze20170905/n1601_dataFreeze/neuroimaging/n1601_pnc_protocol_validation_params_status_20161220.csv'))
+# T1 QA
+t1_qa = pd.read_csv(os.path.join(os.environ['DATADIR'], 'external/pncDataFreeze20170905/n1601_dataFreeze/neuroimaging/t1struct/n1601_t1QaData_20170306.csv'))
+# DTI QA
+dti_qa = pd.read_csv(os.path.join(os.environ['DATADIR'], 'external/pncDataFreeze20170905/n1601_dataFreeze/neuroimaging/dti/n1601_dti_qa_20170301.csv'))
+# Rest QA
+rest_qa = pd.read_csv(os.path.join(os.environ['DATADIR'], 'external/pncDataFreeze20170905/n1601_dataFreeze/neuroimaging/rest/n1601_RestQAData_20170714.csv'))
+# Demographics
+demog = pd.read_csv(os.path.join(os.environ['DATADIR'], 'external/pncDataFreeze20170905/n1601_dataFreeze/demographics/n1601_demographics_go1_20161212.csv'))
+# Brain volume
+brain_vol = pd.read_csv(os.path.join(os.environ['DATADIR'], 'external/pncDataFreeze20170905/n1601_dataFreeze/neuroimaging/t1struct/n1601_ctVol20170412.csv'))
+# Clinical diagnostic 
+clinical = pd.read_csv(os.path.join(os.environ['DATADIR'], 'external/pncDataFreeze20170905/n1601_dataFreeze/clinical/n1601_goassess_psych_summary_vars_20131014.csv'))
+clinical_ps = pd.read_csv(os.path.join(os.environ['DATADIR'], 'external/pncDataFreeze20170905/n1601_dataFreeze/clinical/n1601_diagnosis_dxpmr_20170509.csv'))
+# GOASSESS Bifactor scores
+goassess = pd.read_csv(os.path.join(os.environ['DATADIR'], 'external/GO1_clinical_factor_scores_psychosis_split_BIFACTOR.csv'))
+# Cognition
+cnb = pd.read_csv(os.path.join(os.environ['DATADIR'], 'external/pncDataFreeze20170905/n1601_dataFreeze/cnb/n1601_cnb_factor_scores_tymoore_20151006.csv'))
+
+# merge
+df = health
+df = pd.merge(df, prot, on=['scanid', 'bblid']) # prot
+df = pd.merge(df, t1_qa, on=['scanid', 'bblid']) # t1_qa
+df = pd.merge(df, dti_qa, on=['scanid', 'bblid']) # dti_qa
+df = pd.merge(df, rest_qa, on=['scanid', 'bblid']) # rest_qa
+df = pd.merge(df, demog, on=['scanid', 'bblid']) # demog
+df = pd.merge(df, brain_vol, on=['scanid', 'bblid']) # brain_vol
+df = pd.merge(df, clinical, on=['scanid', 'bblid']) # clinical
+df = pd.merge(df, clinical_ps, on=['scanid', 'bblid']) # clinical
+df = pd.merge(df, goassess, on=['bblid']) # goassess
+df = pd.merge(df, cnb, on=['scanid', 'bblid']) # goassess
+
+print(df.shape[0])
+df.set_index(['bblid', 'scanid'], inplace = True)
+
+
+# # Filter subjects
+
+# In[8]:
+
+
+# 1) Primary sample filter
+df = df[df['healthExcludev2'] == 0]
+print('N after initial exclusion:', df.shape[0])
+
+# 2) T1 exclusion
+df = df[df['t1Exclude'] == 0]
+print('N after T1 exclusion:', df.shape[0])
+
+# 3) Diffusion exclusion
+df = df[df['b0ProtocolValidationStatus'] == 1]
+df = df[df['dti64ProtocolValidationStatus'] == 1]
+df = df[df['dti64Exclude'] == 0]
+print('N after Diffusion exclusion:', df.shape[0])
+
+
+# In[9]:
+
+
+df['dti64QAManualScore'].unique()
+
+
+# In[10]:
+
+
+np.sum(df['averageManualRating'] == 2)
+
+
+# In[11]:
+
+
+np.sum(df['dti64QAManualScore'] == 2)
+
+
+# In[12]:
+
+
+# Convert age to years
+df['ageAtScan1_Years'] = np.round(df.ageAtScan1/12, decimals=1)
+
+
+# In[13]:
+
+
+# find unique ages
+age_unique = np.unique(df.ageAtScan1_Years)
+print('There are', age_unique.shape[0], 'unique age points')
+
+
+# # Define discovery and replication splits
+
+# In[14]:
+
+
+df['disc_repl'] = np.zeros(df.shape[0]).astype(int)
+
+n=np.round(df.shape[0]*.5).astype(int)
+print(n)
+
+hold_out = df.sample(n=n, random_state=0, replace=False, axis=0).index
+df.loc[hold_out,'disc_repl'] = 1
+print('Train:', np.sum(df['disc_repl'] == 0), 'Test:', np.sum(df['disc_repl'] == 1))
+
+
+# ## Symptom dimensions
+
+# In[15]:
+
+
+# phenos = ['Overall_Psychopathology','Psychosis_Positive','F3_Executive_Efficiency','Overall_Speed']
+phenos = ['Overall_Psychopathology','Psychosis_Positive','Psychosis_NegativeDisorg','AnxiousMisery','Externalizing','Fear','F1_Exec_Comp_Res_Accuracy','F3_Executive_Efficiency','Overall_Speed']
+print(phenos)
+
+
+# In[16]:
+
+
+for pheno in phenos:
+    if df.loc[:,pheno].isna().any():
+        print('NaN replacement: ', pheno)
+        x = np.nanmedian(df.loc[:,pheno])
+        df.loc[df.loc[:,pheno].isna(),pheno] = x
+
+
+# In[17]:
+
+
+# Normalize
+rank_r = np.zeros(len(phenos),)
+
+for i, pheno in enumerate(phenos):
+    # normalize regional metric
+#     x = sp.stats.yeojohnson(df.loc[:,pheno])[0]
+    x = rank_int(df.loc[:,pheno])
+    # check if rank order is preserved
+    rank_r[i] = sp.stats.spearmanr(df.loc[:,pheno],x)[0]
+    # store normalized version
+    df.loc[:,pheno] = x
+
+print(np.sum(rank_r < 1))
+
+
+# In[18]:
+
+
+df.loc[:,phenos].var()
+
+
+# ## Export
+
+# In[19]:
+
+
+header = ['disc_repl', 'squeakycleanExclude','ageAtScan1', 'ageAtScan1_Years','sex','race2','handednessv2', 'averageManualRating', 'dti64QAManualScore', 'restProtocolValidationStatus', 'restExclude',
+          'dti64MeanAbsRMS','dti64MeanRelRMS','dti64MaxAbsRMS','dti64MaxRelRMS','mprage_antsCT_vol_TBV', 'averageManualRating',  'goassessSmryMood', 'goassessSmryMan', 'goassessSmryDep',
+          'goassessSmryEat', 'goassessSmryBul', 'goassessSmryAno', 'goassessSmryAnx', 'goassessSmryGad', 'goassessSmrySep', 'goassessSmryPhb', 'goassessSmrySoc', 'goassessSmryPan',
+          'goassessSmryAgr', 'goassessSmryOcd', 'goassessSmryPtd', 'goassessSmryPsy', 'goassessSmryDel', 'goassessSmryHal', 'goassessSmryHalAv', 'goassessSmryHalAs', 'goassessSmryHalVh',
+          'goassessSmryHalOh', 'goassessSmryHalTh', 'goassessSmryBeh', 'goassessSmryAdd', 'goassessSmryOdd', 'goassessSmryCon', 'goassessSmryPrimePos1', 'goassessSmryPrimeTot',
+          'goassessSmryPrimePos2', 'goassessSmryPsychOverallRtg',
+          'goassessDxpmr4'] + phenos
+df.to_csv(os.path.join(outputdir, 'df.csv'), columns = header)
+
+
+# # Plots
+
+# In[20]:
+
+
+if not os.path.exists(figdir): os.makedirs(figdir)
+os.chdir(figdir)
+sns.set(style='white', context = 'paper', font_scale = 1)
+cmap = my_get_cmap('pair')
+
+labels = ['Discovery', 'Replication']
+# phenos_label_short = ['Ov. Psych.', 'Psy. (pos.)', 'Psy. (neg.)', 'Anx.-mis.', 'Ext.', 'Fear']
+# phenos_label = ['Overall psychopathology','Psychosis (positive)','Psychosis (negative)','Anxious-misery','Externalizing','Fear']
+
+
+# ## Age
+
+# In[21]:
+
+
+f, axes = plt.subplots(1,2)
+f.set_figwidth(6.5)
+f.set_figheight(2.5)
+colormap = sns.color_palette("pastel", 2)
+
+sns.distplot(df.loc[df['disc_repl'] == 0,'ageAtScan1_Years'], bins=20, hist=True, kde=False, rug=False, label = labels[0],
+             hist_kws={"histtype": "step", "linewidth": 2, "alpha": 1}, color=list(cmap[0]), ax = axes[0]);
+sns.distplot(df.loc[df['disc_repl'] == 1,'ageAtScan1_Years'], bins=20, hist=True, kde=False, rug=False, label = labels[1],
+             hist_kws={"histtype": "step", "linewidth": 2, "alpha": 1}, color=list(cmap[1]), ax = axes[0]);
+axes[0].set_xlabel('Age (years)');
+axes[0].set_ylabel('Number of participants');
+axes[0].set_xticks(np.arange(np.min(np.round(age_unique,0)), np.max(np.round(age_unique,0)), 2))
+
+# set width of bar
+barWidth = 0.25
+
+# Sex
+y_train = [np.sum(df.loc[df['disc_repl'] == 0,'sex'] == 1), np.sum(df.loc[df['disc_repl'] == 0,'sex'] == 2)]
+y_test = [np.sum(df.loc[df['disc_repl'] == 1,'sex'] == 1), np.sum(df.loc[df['disc_repl'] == 1,'sex'] == 2)]
+r1 = np.arange(len(y_train))+barWidth/2
+r2 = [x + barWidth for x in r1]
+axes[1].bar(r1, y_train, width = barWidth, color = cmap[0], label = labels[0])
+axes[1].bar(r2, y_test, width = barWidth, color = cmap[1], label = labels[1])
+axes[1].set_xlabel('Sex')
+# axes[1].set_ylabel('Number of participants')
+axes[1].set_xticks([r + barWidth for r in range(len(y_train))])
+axes[1].set_xticklabels(['Male', 'Female'])
+
+f.savefig('age_distributions.png', dpi = 150, bbox_inches = 'tight', pad_inches = 0)
+
+
+# ## Symptom dimensions
+
+# In[22]:
+
+
+df_rc = pd.melt(df, id_vars = 'disc_repl', value_vars = phenos)
+
+f, ax = plt.subplots()
+f.set_figwidth(2.5)
+f.set_figheight(4)
+ax = sns.violinplot(y='variable', x='value', hue='disc_repl', data=df_rc, palette = cmap, split=True, scale='width', inner = 'quartile', orient = 'h')
+ax.get_legend().remove()
+ax.set_yticklabels(phenos)
+ax.set_ylabel('Psychopathology phenotypes')
+ax.set_xlabel('Phenotype score')
+f.savefig('symptoms_distributions.png', dpi = 150, bbox_inches = 'tight', pad_inches = 0)
+
+
+# ### Export sample for FC gradients
+
+# In[23]:
+
+
+# 4) rs-fMRI exclusion
+df = df[df['restProtocolValidationStatus'] == 1]
+df = df[df['restExclude'] == 0]
+print('N after rs-fMRI exclusion:', df.shape[0])
+
+
+# In[24]:
+
+
+df.to_csv(os.path.join(outputdir, 'df_gradients.csv'), columns = header)
+
---
+++ 1_code/func.py
@@ -0,0 +1,224 @@
+# Linden Parkes, 2020
+# lindenmp@seas.upenn.edu
+
+# Essentials
+import os, sys, glob
+import pandas as pd
+import numpy as np
+import nibabel as nib
+
+# Stats
+import scipy as sp
+from scipy import stats
+import statsmodels.api as sm
+import pingouin as pg
+
+# Plotting
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.rcParams['svg.fonttype'] = 'none'
+
+
+def set_proj_env():
+
+    # Project root directory
+    projdir = '/Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients'
+    os.environ['PROJDIR'] = projdir
+
+    # Data directory
+    datadir = os.path.join(projdir, '0_data')
+    os.environ['DATADIR'] = datadir
+
+    # Imaging derivatives
+    derivsdir = os.path.join('/Volumes/work_ssd/research_data/PNC/')
+    os.environ['DERIVSDIR'] = derivsdir
+
+    # Pipeline directory
+    pipelinedir = os.path.join(projdir, '2_pipeline')
+    os.environ['PIPELINEDIR'] = pipelinedir
+
+    # Output directory
+    outputdir = os.path.join(projdir, '3_output')
+    os.environ['OUTPUTDIR'] = outputdir
+
+    return projdir, datadir, derivsdir, pipelinedir, outputdir
+    # return parcel_names, parcel_loc, drop_parcels, num_parcels, yeo_idx, yeo_labels
+
+
+def my_get_cmap(which_type = 'qual1', num_classes = 8):
+    # Returns a nice set of colors to make a nice colormap using the color schemes
+    # from http://colorbrewer2.org/
+    #
+    # The online tool, colorbrewer2, is copyright Cynthia Brewer, Mark Harrower and
+    # The Pennsylvania State University.
+
+    if which_type == 'linden':
+        cmap_base = np.array([[255,105,97],[97,168,255],[178,223,138],[117,112,179],[255,179,71]])
+    elif which_type == 'pair':
+        cmap_base = np.array([[124,230,199],[255,169,132]])
+    elif which_type == 'qual1':
+        cmap_base = np.array([[166,206,227],[31,120,180],[178,223,138],[51,160,44],[251,154,153],[227,26,28],
+                            [253,191,111],[255,127,0],[202,178,214],[106,61,154],[255,255,153],[177,89,40]])
+    elif which_type == 'qual2':
+        cmap_base = np.array([[141,211,199],[255,255,179],[190,186,218],[251,128,114],[128,177,211],[253,180,98],
+                            [179,222,105],[252,205,229],[217,217,217],[188,128,189],[204,235,197],[255,237,111]])
+    elif which_type == 'seq_red':
+        cmap_base = np.array([[255,245,240],[254,224,210],[252,187,161],[252,146,114],[251,106,74],
+                            [239,59,44],[203,24,29],[165,15,21],[103,0,13]])
+    elif which_type == 'seq_blu':
+        cmap_base = np.array([[247,251,255],[222,235,247],[198,219,239],[158,202,225],[107,174,214],
+                            [66,146,198],[33,113,181],[8,81,156],[8,48,107]])
+    elif which_type == 'redblu_pair':
+        cmap_base = np.array([[222,45,38],[49,130,189]])
+    elif which_type == 'yeo17':
+        cmap_base = np.array([[97,38,107], # VisCent
+                            [194,33,39], # VisPeri
+                            [79,130,165], # SomMotA
+                            [44,181,140], # SomMotB
+                            [75,148,72], # DorsAttnA
+                            [23,116,62], # DorsAttnB
+                            [149,77,158], # SalVentAttnA
+                            [222,130,177], # SalVentAttnB
+                            [75,87,61], # LimbicA
+                            [149,166,110], # LimbicB
+                            [210,135,47], # ContA
+                            [132,48,73], # ContB
+                            [92,107,131], # ContC
+                            [218,221,50], # DefaultA
+                            [175,49,69], # DefaultB
+                            [41,38,99], # DefaultC
+                            [53,75,158] # TempPar
+                            ])
+    elif which_type == 'yeo7':
+        cmap_base = np.array([[162,81,172], # visual
+                            [120,154,192], # somatomotor
+                            [63,153,50], # dorsalAttention
+                            [223,101,255], # salienceVentralAttention
+                            [247,253,201], # limbic
+                            [241,185,68], # frontoparietalControl
+                            [217,112,123]]) # default
+
+    if cmap_base.shape[0] > num_classes: cmap = cmap_base[0:num_classes]
+    else: cmap = cmap_base
+
+    cmap = cmap / 255
+
+    return cmap
+
+
+def roi_to_vtx(roi_data, parcel_names, parc_file):
+    # roi_data      = (num_nodes,) array vector of node-level data to plot onto surface
+    # 
+    # 
+    # parcel_names     = (num_nodes,) array vector of strings containg roi names
+    #               corresponding to roi_data
+    # 
+    # parc_file    = full path and file name to surface file
+    #               Note, I used fsaverage/fsaverage5 surfaces
+
+    # Load freesurfer file
+    labels, ctab, surf_names = nib.freesurfer.read_annot(parc_file)
+
+    # convert FS surf_names to array of strings
+    if type(surf_names[0]) != str:
+        for i in np.arange(0,len(surf_names)):
+            surf_names[i] = surf_names[i].decode("utf-8")
+
+    if 'myaparc' in parc_file:
+        hemi = os.path.basename(parc_file)[0:2]
+
+        # add hemisphere to surface surf_names
+        for i in np.arange(0,len(surf_names)):
+            surf_names[i] = hemi + "_" + surf_names[i]
+
+    # Find intersection between parcel_names and surf_names
+    overlap = np.intersect1d(parcel_names, surf_names, return_indices = True)
+    overlap_names = overlap[0]
+    idx_in = overlap[1] # location of surf_names in parcel_names
+    idx_out = overlap[2] # location of parcel_names in surf_names
+
+    # check for weird floating nans in roi_data
+    fckn_nans = np.zeros((roi_data.shape)).astype(bool)
+    for i in range(0,fckn_nans.shape[0]): fckn_nans[i] = math.isnan(roi_data[i])
+    if any(fckn_nans): roi_data[fckn_nans] = 0
+
+    # broadcast roi data to FS space
+    # initialise idx vector with the dimensions of the FS labels, but data type corresponding to the roi data
+    vtx_data = np.zeros(labels.shape, type(roi_data))
+    # vtx_data = vtx_data - 1000
+
+    # for each entry in fs names
+    for i in range(0, overlap_names.shape[0]):
+        vtx_data[labels == idx_out[i]] = roi_data[idx_in[i]]
+
+    # get min/max for plottin
+    x = np.sort(np.unique(vtx_data))
+
+    if x.shape[0] > 1:
+        vtx_data_min = x[0]
+        vtx_data_max = x[-1]
+    else:
+        vtx_data_min = 0
+        vtx_data_max = 0
+
+    # i = 0
+    # while vtx_data_min == -1000: vtx_data_min = x[i]; i += 1
+
+    return vtx_data, vtx_data_min, vtx_data_max
+
+
+def rank_to_normal(rank, c, n):
+    # Standard quantile function
+    x = (rank - c) / (n - 2*c + 1)
+    return sp.stats.norm.ppf(x)
+
+
+def rank_int(series, c=3.0/8):
+    # Check input
+    assert(isinstance(series, pd.Series))
+    assert(isinstance(c, float))
+
+    # Set seed
+    np.random.seed(123)
+
+    # Drop NaNs
+    series = series.loc[~pd.isnull(series)]
+
+    # Get rank, ties are averaged
+    rank = sp.stats.rankdata(series, method="average")
+
+    # Convert numpy array back to series
+    rank = pd.Series(rank, index=series.index)
+
+    # Convert rank to normal distribution
+    transformed = rank.apply(rank_to_normal, c=c, n=len(rank))
+    
+    return transformed
+
+
+def get_fdr_p(p_vals, alpha = 0.05):
+    out = multitest.multipletests(p_vals, alpha = alpha, method = 'fdr_bh')
+    p_fdr = out[1] 
+
+    return p_fdr
+
+
+def get_fdr_p_df(p_vals, alpha = 0.05, rows = False):
+    
+    if rows:
+        p_fdr = pd.DataFrame(index = p_vals.index, columns = p_vals.columns)
+        for row, data in p_vals.iterrows():
+            p_fdr.loc[row,:] = get_fdr_p(data.values)
+    else:
+        p_fdr = pd.DataFrame(index = p_vals.index,
+                            columns = p_vals.columns,
+                            data = np.reshape(get_fdr_p(p_vals.values.flatten(), alpha = alpha), p_vals.shape))
+
+    return p_fdr
+
+
+def get_exact_p(x,y):
+    pval = 2*np.min([np.mean(x-y>=0), np.mean(x-y<=0)])
+    
+    return pval
+
