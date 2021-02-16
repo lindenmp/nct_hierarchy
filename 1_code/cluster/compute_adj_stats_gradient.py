1df4df20044dcde00cb3cf1c6f9fa8e1a0c442e7
Linden Parkes <lindenparkes@gmail.com>
2021-02-16 13:58:05 -0500

    added joint variance to the gradient variance function

--- 1_code/cluster/compute_adj_stats_gradient.py
+++ 1_code/cluster/compute_adj_stats_gradient.py
@@ -42,14 +42,20 @@ def matrix_to_states(x, cluster_labels):
     return x_out
 
 
-def get_tm_convergence(shortest_path, gradients, return_abs = False):
+def get_gradient_variance(shortest_path, gradients, return_abs = False):
 
-    tm_convergence = np.diff(gradients[shortest_path,:], axis = 0)
+    # calculate the differences between coordinates in adjacent nodes along shortest path
+    gradient_diff = np.diff(gradients[shortest_path,:], axis = 0)
 
     if return_abs == True:
-         tm_convergence = np.abs(tm_convergence)
+         gradient_diff = np.abs(gradient_diff)
+
+    mean_diff = np.mean(gradient_diff, axis = 0) # mean the differences along the shortest path
+    var_diff = np.var(gradient_diff, axis = 0) # get the variance of the differences along the shortest path
+
+    euclidean_var = np.var(np.sqrt(np.sum(np.square(gradient_diff), axis = 1))) # get the variance of the euclidean distance
     
-    return np.mean(tm_convergence, axis = 0), np.var(tm_convergence, axis = 0)
+    return mean_diff, var_diff, euclidean_var
 
 
 def get_adj_stats(A, gradients, cluster_labels, return_abs = False):
@@ -68,25 +74,33 @@ def get_adj_stats(A, gradients, cluster_labels, return_abs = False):
     tm_var_tmp = np.zeros((num_parcels,num_parcels))
     smv_tmp = np.zeros((num_parcels,num_parcels))
     smv_var_tmp = np.zeros((num_parcels,num_parcels))
+    joint_var_tmp = np.zeros((num_parcels,num_parcels))
 
     for i in np.arange(num_parcels):
         for j in np.arange(num_parcels):
             shortest_path = retrieve_shortest_path(i,j,hops,Pmat)
             if len(shortest_path) != 0:
                 shortest_path = shortest_path.flatten()
-                x_mean, x_var = get_tm_convergence(shortest_path, gradients, return_abs = return_abs)
-                tm_tmp[i,j] = x_mean[0]
-                tm_var_tmp[i,j] = x_var[0]
-                smv_tmp[i,j] = x_mean[1]
-                smv_var_tmp[i,j] = x_var[1]
+                mean_diff, var_diff, euclidean_var = get_gradient_variance(shortest_path, gradients, return_abs = return_abs)
+
+                tm_tmp[i,j] = mean_diff[0]
+                tm_var_tmp[i,j] = var_diff[0]
+
+                smv_tmp[i,j] = mean_diff[1]
+                smv_var_tmp[i,j] = var_diff[1]
+
+                joint_var_tmp[i,j] = euclidean_var
 
     # downsample transmodal convergence to cluster-based states
     tm_con = matrix_to_states(tm_tmp, cluster_labels)
     tm_var = matrix_to_states(tm_var_tmp, cluster_labels)
+
     smv_con = matrix_to_states(smv_tmp, cluster_labels)
     smv_var = matrix_to_states(smv_var_tmp, cluster_labels)
+    
+    joint_var = matrix_to_states(joint_var_tmp, cluster_labels)
             
-    return D_mean, hops_mean, tm_con, tm_var, smv_con, smv_var
+    return D_mean, hops_mean, tm_con, tm_var, smv_con, smv_var, joint_var
 
 
 # --------------------------------------------------------------------------------------------------------------------
@@ -102,15 +116,16 @@ kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(gradients)
 # --------------------------------------------------------------------------------------------------------------------
 adj_stats = {}
 
-D_mean, hops_mean, tm_con, tm_var, smv_con, smv_var = get_adj_stats(A, gradients, kmeans.labels_, return_abs = False)
+D_mean, hops_mean, tm_con, tm_var, smv_con, smv_var, joint_var = get_adj_stats(A, gradients, kmeans.labels_, return_abs = False)
 adj_stats['D_mean'] = D_mean
 adj_stats['hops_mean'] = hops_mean
 adj_stats['tm_con'] = tm_con
 adj_stats['tm_var'] = tm_var
 adj_stats['smv_con'] = smv_con
 adj_stats['smv_var'] = smv_var
+adj_stats['joint_var'] = joint_var
 
-D_mean, hops_mean, tm_con, tm_var, smv_con, smv_var = get_adj_stats(A, gradients, kmeans.labels_, return_abs = True)
+D_mean, hops_mean, tm_con, tm_var, smv_con, smv_var, joint_var = get_adj_stats(A, gradients, kmeans.labels_, return_abs = True)
 adj_stats['tm_con_abs'] = tm_con
 adj_stats['tm_var_abs'] = tm_var
 adj_stats['smv_con_abs'] = smv_con
