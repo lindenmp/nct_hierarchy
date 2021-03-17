ed81a73d5beff8466d60fe0c134061c6d9534de2
Linden Parkes <lindenparkes@gmail.com>
2021-03-17 14:33:45 -0400

    init commit

---
+++ utils/utils.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pandas as pd
+
+
+def get_pdist_clusters(coords, labels, method='mean'):
+    unique, counts = np.unique(labels, return_counts=True)
+    n_clusters = len(unique)
+
+    dist = np.zeros((n_clusters, n_clusters))
+
+    for i in np.arange(n_clusters):
+        for j in np.arange(n_clusters):
+            x0 = labels == i
+            xf = labels == j
+
+            x0_coords = coords[x0, :]
+            xf_coords = coords[xf, :]
+
+            tmp = []
+            for r1 in np.arange(x0_coords.shape[0]):
+                for r2 in np.arange(xf_coords.shape[0]):
+                    d = (x0_coords[r1, :] - xf_coords[r2, :]) ** 2
+                    d = np.sum(d)
+                    d = np.sqrt(d)
+                    tmp.append(d)
+
+            if method == 'mean':
+                dist[i, j] = np.mean(tmp)
+            elif method == 'min':
+                dist[i, j] = np.min(tmp)
+            elif method == 'median':
+                dist[i, j] = np.median(tmp)
+
+    return dist
+
+
+def get_disc_repl(df, frac=0.5):
+    df_out = pd.Series(data=np.zeros(df.shape[0], dtype=bool), index=df.index)
+
+    n = np.round(df_out.shape[0] * frac).astype(int)
+    hold_out = df_out.sample(n=n, random_state=0, replace=False, axis=0).index
+    df_out[hold_out] = True
+    print('Train:', np.sum(df_out == 0), 'Test:', np.sum(df_out == 1))
+
+    return df_out
+
+
+def mean_over_clusters(x, cluster_labels):
+    unique = np.unique(cluster_labels, return_counts=False)
+    n_clusters = len(unique)
+
+    x_out = np.zeros((n_clusters, n_clusters))
+    for i in np.arange(n_clusters):
+        for j in np.arange(n_clusters):
+            x_out[i, j] = np.nanmean(np.nanmean(x[cluster_labels == i, :], axis=0)[cluster_labels == j])
+
+    return x_out
