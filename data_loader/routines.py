a9e6cc859f4241fce3d524e7209899f83e37c5ff
Linden Parkes <lindenparkes@gmail.com>
2021-03-19 11:51:19 -0400

    all routines are now classes instead of functions

--- data_loader/routines.py
+++ data_loader/routines.py
@@ -1,74 +1,96 @@
 import numpy as np
 
 
-def load_sc(environment):
-    print('Loading SC data')
+class LoadSC():
+    def __init__(self, environment, Subject):
+        self.environment = environment
+        self.Subject = Subject
 
-    # pull out dataframe, just for convenience
-    df = environment.df
-    Subject = environment.Subject
+    def run(self):
+        print('Routine: loading SC data')
+        n_subs = self.environment.df.shape[0]
+        self.df = self.environment.df.copy()
+        self.A = np.zeros((self.environment.n_parcels, self.environment.n_parcels, n_subs))
 
-    n_subs = df.shape[0]
-    n_parcels = environment.n_parcels
-    A = np.zeros((n_parcels, n_parcels, n_subs))
+        # subject filter
+        subj_filt = np.zeros((n_subs,)).astype(bool)
 
-    # subject filter
-    subj_filt = np.zeros((n_subs,)).astype(bool)
+        for i in np.arange(n_subs):
+            subject = self.Subject(subjid=self.df.index[i])
+            subject.get_file_names()
+            subject.load_sc()
+            self.A[:, :, i] = subject.sc.data.copy()
 
-    for i in np.arange(n_subs):
-        subject = Subject(df.index[i][0], df.index[i][1])
-        subject.get_file_names()
-        subject.load_sc()
-        A[:, :, i] = subject.sc.data.copy()
+            subject.sc.check_disconnected_nodes()
+            if subject.sc.disconnected_nodes:
+                subj_filt[i] = True
 
-        subject.sc.check_disconnected_nodes()
-        if subject.sc.disconnected_nodes:
-            subj_filt[i] = True
-
-    # filter subjects with disconnected nodes from sc matrix
-    if np.any(subj_filt):
-        print('{0} subjects had disconnected nodes in sc matrices'.format(np.sum(subj_filt)))
-        df = df.loc[~subj_filt]
-        A = A[:, :, ~subj_filt]
-
-    environment.df = df
-    environment.A = A
-
-    return environment
+        # filter subjects with disconnected nodes from sc matrix
+        if np.any(subj_filt):
+            print('\t{0} subjects had disconnected nodes in sc matrices'.format(np.sum(subj_filt)))
+            self.df = self.df.loc[~subj_filt]
+            self.A = self.A[:, :, ~subj_filt]
 
 
-def load_fc(environment):
-    print('Loading resting state FC data')
+class LoadFC():
+    def __init__(self, environment, Subject):
+        self.environment = environment
+        self.Subject = Subject
 
-    # pull out dataframe, just for convenience
-    df = environment.df
-    Subject = environment.Subject
+    def run(self):
+        print('Routine: loading resting state FC data')
+        n_subs = self.environment.df.shape[0]
+        self.df = self.environment.df.copy()
+        self.fc = np.zeros((self.environment.n_parcels, self.environment.n_parcels, n_subs))
 
-    n_subs = df.shape[0]
-    n_parcels = environment.n_parcels
-    fc = np.zeros((n_parcels, n_parcels, n_subs))
+        # subject filter
+        subj_filt = np.zeros((n_subs,)).astype(bool)
 
-    # subject filter
-    subj_filt = np.zeros((n_subs,)).astype(bool)
+        for i in np.arange(n_subs):
+            subject = self.Subject(subjid=self.df.index[i])
+            subject.get_file_names()
+            subject.load_rsfc()
+            self.fc[:, :, i] = subject.rsfc.data.copy()
 
-    for i in np.arange(n_subs):
-        subject = Subject(df.index[i][0], df.index[i][1])
-        subject.get_file_names()
-        subject.load_rsfc()
-        fc[:, :, i] = subject.rsfc.data.copy()
+            subject.load_sc()
+            subject.sc.check_disconnected_nodes()
+            if subject.sc.disconnected_nodes:
+                subj_filt[i] = True
 
-        subject.load_sc()
-        subject.sc.check_disconnected_nodes()
-        if subject.sc.disconnected_nodes:
-            subj_filt[i] = True
+        # filter subjects with disconnected nodes from sc matrix
+        if np.any(subj_filt):
+            print('\t{0} subjects had disconnected nodes in sc matrices'.format(np.sum(subj_filt)))
+            self.df = self.df.loc[~subj_filt]
+            self.fc = self.fc[:, :, ~subj_filt]
 
-    # filter subjects with disconnected nodes from sc matrix
-    if np.any(subj_filt):
-        print('{0} subjects had disconnected nodes in sc matrices'.format(np.sum(subj_filt)))
-        df = df.loc[~subj_filt]
-        fc = fc[:, :, ~subj_filt]
 
-    environment.df = df
-    environment.fc = fc
+class LoadRLFP():
+    def __init__(self, environment, Subject):
+        self.environment = environment
+        self.Subject = Subject
 
-    return environment
+    def run(self):
+        print('Routine: loading resting state RLFP data')
+        n_subs = self.environment.df.shape[0]
+        self.df = self.environment.df.copy()
+        self.rlfp = np.zeros((n_subs, self.environment.n_parcels))
+
+        # subject filter
+        subj_filt = np.zeros((n_subs,)).astype(bool)
+
+        for i in np.arange(n_subs):
+            subject = self.Subject(subjid=self.df.index[i])
+            subject.get_file_names()
+            subject.load_rlfp()
+            self.rlfp[i, :] = subject.rlfp.copy()
+
+            subject.load_sc()
+            subject.sc.check_disconnected_nodes()
+            if subject.sc.disconnected_nodes:
+                subj_filt[i] = True
+
+        # filter subjects with disconnected nodes from sc matrix
+        if np.any(subj_filt):
+            print('\t{0} subjects had disconnected nodes in sc matrices'.format(np.sum(subj_filt)))
+            self.df = self.df.loc[~subj_filt]
+            self.rlfp = self.rlfp[~subj_filt, :]
