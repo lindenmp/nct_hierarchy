0665dfe39376f17741937972c8c1391fdd17d7a9
Linden Parkes <lindenparkes@gmail.com>
2021-04-05 11:33:28 -0400

    added E:I ratio and some other minor edits

--- scripts/results_path_traversal.py
+++ scripts/results_path_traversal.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 import seaborn as sns
-from sklearn.cluster import KMeans
+import pandas as pd
 
 from data_loader.routines import LoadSC, LoadRLFP, LoadCT
 from utils.imaging_derivs import DataMatrix, DataVector
-from utils.plotting import my_regplot, my_nullplot
+from utils.plotting import my_regplot
 from utils.utils import get_pdist_clusters, get_disc_repl, mean_over_clusters
 
 # %% Set general plotting params
@@ -127,49 +127,57 @@ sst_pvalb_delta[np.isnan(sst_pvalb_delta)] = np.nanmedian(sst_pvalb_delta)
 sst_pvalb_delta = DataVector(data=sst_pvalb_delta)
 sst_pvalb_delta.regress_nuisance(c=A.hops.mean(axis=0))
 
+# E:I
+excitation = expression.loc[:, ['GRIA1', 'GRIA2', 'GRIA3', 'GRIA4', 'GRIN1', 'GRIN2A', 'GRIN2B', 'GRIN2C']].sum(axis=1)
+inhibition = expression.loc[:, ['GABRA1', 'GABRA2', 'GABRA3', 'GABRA4', 'GABRA5', 'GABRB1',
+                                'GABRB2', 'GABRB3', 'GABRG1', 'GABRG2', 'GABRG3']].sum(axis=1)
+# inhibition = expression.loc[:, ['SST', 'PVALB', 'GABRA1', 'GABRA2', 'GABRA3', 'GABRA4', 'GABRA5', 'GABRB1',
+#                                 'GABRB2', 'GABRB3', 'GABRG1', 'GABRG2', 'GABRG3']].sum(axis=1)
+# inhibition = expression.loc[:, ['SST', 'PVALB']].sum(axis=1)
+ei_ratio = excitation.values/inhibition.values
+ei_ratio[np.isnan(ei_ratio)] = np.nanmedian(ei_ratio)
+ei_ratio = DataVector(data=ei_ratio)
+ei_ratio.regress_nuisance(c=A.hops.mean(axis=0))
+
 # %% Plots
 print('\nGenerating figures')
 gradient = cg.gradients[:, 0]
 
-def my_plot(A, environment, figname='figure.png'):
+def my_gradient_plot(A, environment, figname='figure.png', plot_resid=False):
     grad_slope = DataMatrix(data=A.grad_slope)
     grad_slope.regress_nuisance(c=A.hops)
+    grad_r2 = DataMatrix(data=A.grad_r2)
+    grad_r2.regress_nuisance(c=A.hops)
     grad_resid = DataMatrix(data=A.grad_resid)
     grad_resid.regress_nuisance(c=A.hops)
     grad_var = DataMatrix(data=A.grad_var)
     grad_var.regress_nuisance(c=A.hops)
 
-    indices = np.where(~np.eye(n_parcels, dtype=bool) * ~np.isnan(A.grad_slope))
-    stats = sp.stats.pearsonr(A.grad_slope[indices], A.hops[indices])
-    print('Correlation between slope and hops: {:.2f}, {:.2f}'.format(stats[0], stats[1]))
-    stats = sp.stats.pearsonr(grad_slope.data_resid[indices], A.hops[indices])
-    print('\t after regressing out hops: {:.2f}, {:.2f}'.format(stats[0], stats[1]))
+    if plot_resid:
+        to_plot = [grad_slope.data_resid, grad_r2.data_resid, grad_resid.data_resid, grad_var.data_resid]
+    else:
+        to_plot = [grad_slope.data, grad_r2.data, grad_resid.data, grad_var.data]
+    to_plot_labels = ['Slope', 'R2', 'RMSE', 'Variance']
+    to_plot_ylims = [[-1, 1], [-0.5, 0.5], [-0.3, 0.5], [-0.5, 1]]
+    n_subplots = len(to_plot)
 
-    indices = np.where(~np.eye(n_parcels, dtype=bool) * ~np.isnan(A.grad_resid))
-    stats = sp.stats.pearsonr(A.grad_resid[indices], A.hops[indices])
-    print('Correlation between error and hops: {:.2f}, {:.2f}'.format(stats[0], stats[1]))
-    stats = sp.stats.pearsonr(grad_resid.data_resid[indices], A.hops[indices])
-    print('\t after regressing out hops: {:.2f}, {:.2f}'.format(stats[0], stats[1]))
+    f, ax = plt.subplots(3, n_subplots, figsize=(n_subplots * 5, 15))
+    for i in np.arange(n_subplots):
+        indices = np.where(~np.eye(n_parcels, dtype=bool) * ~np.isnan(to_plot[i]))
+        stats = sp.stats.pearsonr(A.hops[indices], to_plot[i][indices])
+        print('Correlation between hops and {:}: {:.2f}, {:.2f}'.format(to_plot_labels[i], stats[0], stats[1]))
 
-    indices = np.where(~np.eye(n_parcels, dtype=bool) * ~np.isnan(A.grad_var))
-    stats = sp.stats.pearsonr(A.grad_var[indices], A.hops[indices])
-    print('Correlation between variance and hops: {:.2f}, {:.2f}'.format(stats[0], stats[1]))
-    stats = sp.stats.pearsonr(grad_var.data_resid[indices], A.hops[indices])
-    print('\t after regressing out hops: {:.2f}, {:.2f}'.format(stats[0], stats[1]))
+        my_regplot(A.gradient, np.nanmean(np.where(to_plot[i] != 0, to_plot[i], np.nan), axis=1),
+                   'Gradient', to_plot_labels[i] + ' (mean)', ax[0, i])
+        # ax[0, i].set_ylim(to_plot_ylims[i])
 
-    f, ax = plt.subplots(4, 1, figsize=(5, 5 * 4))
-    my_regplot(A.gradient, np.nanmean(np.where(grad_slope.data_resid != 0, grad_slope.data_resid, np.nan), axis=1),
-               'Gradient', 'Slope (mean)', ax[0])
-    ax[0].set_ylim([-1, 1])
-    my_regplot(A.gradient, np.nanstd(np.where(grad_slope.data_resid != 0, grad_slope.data_resid, np.nan), axis=1),
-               'Gradient', 'Slope (std)', ax[1])
-    ax[1].set_ylim([0.2, 0.8])
-    my_regplot(A.gradient, np.nanmean(np.where(grad_resid.data_resid != 0, grad_resid.data_resid, np.nan), axis=1),
-               'Gradient', 'RMSE (mean)', ax[2])
-    ax[2].set_ylim([-0.3, 0.5])
-    my_regplot(A.gradient, np.nanmean(np.where(grad_var.data_resid != 0, grad_var.data_resid, np.nan), axis=1),
-               'Gradient', 'Variance (mean)', ax[3])
-    ax[3].set_ylim([-0.5, 1])
+        my_regplot(A.gradient, np.nanstd(np.where(to_plot[i] != 0, to_plot[i], np.nan), axis=1),
+                   'Gradient', to_plot_labels[i] + ' (std)', ax[1, i])
+        # ax[1, i].set_ylim([0, 1])
+
+        my_regplot(np.nanmean(A.hops, axis=1), np.nanmean(np.where(to_plot[i] != 0, to_plot[i], np.nan), axis=1),
+                   'Hops (mean)', to_plot_labels[i] + ' (mean)', ax[2, i])
+        # ax[1, i].set_ylim([0, 1])
 
     f.subplots_adjust(wspace=0.5)
     f.savefig(os.path.join(environment.figdir, figname),
@@ -178,16 +186,21 @@ def my_plot(A, environment, figname='figure.png'):
 
 # %% 1) paths
 A.get_gradient_slopes(gradient, method='linear')
-my_plot(A, environment, figname='path_traversal_gradient.png')
+my_gradient_plot(A, environment, figname='path_traversal_gradient.png', plot_resid=False)
+my_gradient_plot(A, environment, figname='path_traversal_gradient_resid.png', plot_resid=True)
 
 # %% 2) rlfp
 A.get_gradient_slopes(gradient, x_map=rlfp_mean.data_resid, method='linear')
-my_plot(A, environment, figname='path_traversal_rlfp.png')
+my_gradient_plot(A, environment, figname='path_traversal_rlfp.png')
 
 # %% 3) ct
 A.get_gradient_slopes(gradient, x_map=ct_mean.data_resid, method='linear')
-my_plot(A, environment, figname='path_traversal_ct.png')
+my_gradient_plot(A, environment, figname='path_traversal_ct.png')
 
 # %% 4) sst - pvalb
 A.get_gradient_slopes(gradient, x_map=sst_pvalb_delta.data_resid, method='linear')
-my_plot(A, environment, figname='path_traversal_sst-pvalb.png')
+my_gradient_plot(A, environment, figname='path_traversal_sst-pvalb.png')
+
+# %% 5) E:I
+A.get_gradient_slopes(gradient, x_map=ei_ratio.data_resid, method='linear')
+my_gradient_plot(A, environment, figname='path_traversal_ei.png')
