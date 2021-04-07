10c663e930ca7d3096a81db45a82ed72aff21f07
Linden Parkes <lindenparkes@gmail.com>
2021-04-07 17:13:42 -0400

    code move and minor changes

---
+++ scripts/job_submitter.sh
@@ -0,0 +1,15 @@
+ssh cbica
+
+conda activate pfactor_gradients
+
+# average a matrix
+qsub -N pnc_average_adj -l h_vmem=3G,s_vmem=3G -pe threaded 8 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
+-t 1:4 \
+/cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
+/cbica/home/parkesl/research_projects/pfactor_gradients/scripts/pnc_average_adj_compute_control_energy.py
+
+# pnc subjects, array job
+qsub -N subjects -l h_vmem=3G,s_vmem=3G -pe threaded 4 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
+-t 1:5 \
+/cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
+/cbica/home/parkesl/research_projects/pfactor_gradients/scripts/pnc_compute_control_energy.py
--- scripts/pnc_average_adj_compute_control_energy.py
+++ scripts/pnc_average_adj_compute_control_energy.py
@@ -1,6 +1,7 @@
 # %%
-import sys, os
-sys.path.extend(['/cbica/home/parkesl/research_projects/pfactor_gradients'])
+import sys, os, platform
+if platform.system() == 'Linux':
+    sys.path.extend(['/cbica/home/parkesl/research_projects/pfactor_gradients'])
 from data_loader.pnc import Environment, Subject
 from data_loader.routines import LoadSC, LoadAverageSC, LoadCT, LoadRLFP
 from data_loader.pipelines import ComputeGradients, ComputeMinimumControlEnergy
@@ -8,7 +9,11 @@ from utils.imaging_derivs import DataVector
 import numpy as np
 
 # %% Setup project environment
-computer = 'cbica'
+if platform.system() == 'Linux':
+    computer = 'cbica'
+elif platform.system() == 'Darwin':
+    computer = 'macbook'
+
 parc = 'schaefer'
 n_parcels = 400
 sc_edge_weight = 'streamlineCount'
@@ -27,6 +32,8 @@ compute_gradients.run()
 # %% Load sc data
 load_sc = LoadSC(environment=environment, Subject=Subject)
 load_sc.run()
+# refilter environment due to LoadSC excluding on disconnected nodes
+environment.df = load_sc.df.copy()
 
 spars_thresh = 0.06
 load_average_sc = LoadAverageSC(load_sc=load_sc, spars_thresh=spars_thresh)
@@ -34,12 +41,20 @@ load_average_sc.run()
 
 # %% get control energy
 file_prefix = 'average_adj_n-{0}_s-{1}_'.format(load_average_sc.load_sc.df.shape[0], spars_thresh)
-sge_task_id = int(os.getenv("SGE_TASK_ID"))
+n_subsamples = 20
+try:
+    sge_task_id = int(os.getenv("SGE_TASK_ID"))
+except:
+    sge_task_id = 1
+print(sge_task_id)
 
+# %%
 if sge_task_id == 1:
     nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=load_average_sc.A,
-                                               states=compute_gradients.kmeans.labels_,
+                                               states=compute_gradients.kmeans.labels_, n_subsamples=n_subsamples,
                                                control='minimum', T=1, B='wb', file_prefix=file_prefix)
+    nct_pipeline.run()
+# %%
 elif sge_task_id == 2:
     # load ct data
     load_ct = LoadCT(environment=environment, Subject=Subject)
@@ -51,8 +66,10 @@ elif sge_task_id == 2:
     ct.brain_surface_plot(environment)
 
     nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=load_average_sc.A,
-                                               states=compute_gradients.kmeans.labels_,
+                                               states=compute_gradients.kmeans.labels_, n_subsamples=n_subsamples,
                                                control='minimum', T=1, B=ct, file_prefix=file_prefix)
+    nct_pipeline.run()
+# %%
 elif sge_task_id == 3:
     # load rlfp data
     load_rlfp = LoadRLFP(environment=environment, Subject=Subject)
@@ -64,7 +81,13 @@ elif sge_task_id == 3:
     rlfp.brain_surface_plot(environment)
 
     nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=load_average_sc.A,
-                                               states=compute_gradients.kmeans.labels_,
+                                               states=compute_gradients.kmeans.labels_, n_subsamples=n_subsamples,
                                                control='minimum', T=1, B=rlfp, file_prefix=file_prefix)
-
-nct_pipeline.run()
+    nct_pipeline.run()
+# %%
+elif sge_task_id == 4:
+    file_prefix = 'noise_'+file_prefix
+    nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=load_average_sc.A,
+                                               states=compute_gradients.kmeans.labels_, n_subsamples=n_subsamples,
+                                               control='minimum', T=1, B='wb', file_prefix=file_prefix)
+    nct_pipeline.run(add_noise=True)
