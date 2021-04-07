0a3517ca9f9d2ef0061bd01d73ca33e489866479
Linden Parkes <lindenparkes@gmail.com>
2021-04-07 07:41:15 -0400

    added LoadAverageSC

--- data_loader/routines.py
+++ data_loader/routines.py
@@ -34,6 +34,46 @@ class LoadSC():
             self.A = self.A[:, :, ~subj_filt]
 
 
+class LoadAverageSC():
+    def __init__(self, load_sc, spars_thresh=0.06):
+        self.load_sc = load_sc
+        self.spars_thresh = spars_thresh
+
+    def _check_inputs(self):
+        try:
+            A = self.load_sc.A
+        except AttributeError:
+            self.load_sc.run()
+
+    def _print_settings(self):
+        print('\tsettings:')
+        print('\t\tsparsity: {0}'.format(self.spars_thresh))
+
+    def run(self):
+        print('Routine: loading average SC matrix')
+        self._check_inputs()
+        self._print_settings()
+
+        A = self.load_sc.A
+        n_subs = self.load_sc.df.shape[0]
+        print('\tnumber of subjects in average adj matrix: {0}'.format(n_subs))
+
+        # Get streamline count and network density
+        A_d = np.zeros((n_subs,))
+        for i in range(n_subs):
+            A_d[i] = np.count_nonzero(np.triu(A[:, :, i])) / (
+                        (A[:, :, i].shape[0] ** 2 - A[:, :, i].shape[0]) / 2)
+
+        # Get group average adj. matrix
+        A = np.mean(A, 2)
+        thresh = np.percentile(A, 100 - (self.spars_thresh * 100))
+        A[A < thresh] = 0
+
+        print('\tactual matrix sparsity = {:.2f}'.format(
+            np.count_nonzero(np.triu(A)) / ((A.shape[0] ** 2 - A.shape[0]) / 2)))
+
+        self.A = A
+
 class LoadFC():
     def __init__(self, environment, Subject):
         self.environment = environment
