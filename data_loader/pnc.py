b0f09c4cc593220b66c06451a050211568057992
Linden Parkes <lindenparkes@gmail.com>
2021-04-07 10:50:50 -0400

    Subject now inherits an instance of Environment class, or will use default Envrionment if none passed

--- data_loader/pnc.py
+++ data_loader/pnc.py
@@ -130,41 +130,41 @@ class Environment():
         self.centroids.set_index('Label Name', inplace=True)
         self.centroids.drop('NONE', axis=0, inplace=True)
 
-class Subject(Environment):
-    def __init__(self, subjid='81287_2738'):
-        Environment.__init__(self)
+class Subject():
+    def __init__(self, environment=Environment(), subjid='81287_2738'):
+        self.environment = environment
         self.subjid = subjid
         self.bblid = subjid.split('_')[0]
         self.scanid = subjid.split('_')[1]
 
     def get_file_names(self):
-        if self.parc == 'schaefer':
+        if self.environment.parc == 'schaefer':
             sc_filename = os.path.join('{0}'.format(self.bblid),
                                        '*x{0}'.format(self.scanid),
                                        'tractography', 'connectivity',
                                        '{0}_*x{1}_SchaeferPNC_{2}_dti_{3}_connectivity.mat' \
-                                       .format(self.bblid, self.scanid, self.n_parcels, self.sc_edge_weight))
-            sc_filename = glob.glob(os.path.join(self.scdir, sc_filename))
+                                       .format(self.bblid, self.scanid, self.environment.n_parcels, self.environment.sc_edge_weight))
+            sc_filename = glob.glob(os.path.join(self.environment.scdir, sc_filename))
 
             ct_filename = os.path.join('{0}'.format(self.bblid),
                                        '*x{0}'.format(self.scanid),
-                                       'ct_schaefer{0}_17.txt'.format(self.n_parcels))
-            ct_filename = glob.glob(os.path.join(self.ctdir, ct_filename))
+                                       'ct_schaefer{0}_17.txt'.format(self.environment.n_parcels))
+            ct_filename = glob.glob(os.path.join(self.environment.ctdir, ct_filename))
 
-            if self.n_parcels == 200:
+            if self.environment.n_parcels == 200:
                 rsts_filename = os.path.join('{0}'.format(self.bblid),
                                              '*x{0}'.format(self.scanid),
-                                             'net', 'Schaefer{0}PNC'.format(self.n_parcels),
+                                             'net', 'Schaefer{0}PNC'.format(self.environment.n_parcels),
                                              '{0}_*x{1}_Schaefer{2}PNC_ts.1D' \
-                                             .format(self.bblid, self.scanid, self.n_parcels))
-                rsts_filename = glob.glob(os.path.join(self.rstsdir, rsts_filename))
-            elif self.n_parcels == 400:
+                                             .format(self.bblid, self.scanid, self.environment.n_parcels))
+                rsts_filename = glob.glob(os.path.join(self.environment.rstsdir, rsts_filename))
+            elif self.environment.n_parcels == 400:
                 rsts_filename = os.path.join('{0}'.format(self.bblid),
                                              '*x{0}'.format(self.scanid),
                                              'net', 'SchaeferPNC',
                                              '{0}_*x{1}_SchaeferPNC_ts.1D' \
                                              .format(self.bblid, self.scanid))
-                rsts_filename = glob.glob(os.path.join(self.rstsdir, rsts_filename))
+                rsts_filename = glob.glob(os.path.join(self.environment.rstsdir, rsts_filename))
 
         try: self.sc_filename = sc_filename[0]
         except IndexError: self.sc_filename = []
@@ -194,7 +194,7 @@ class Subject(Environment):
 
         rlfp = np.zeros((n_parcels,))
         for i in np.arange(n_parcels):
-            rlfp[i] = compute_rlfp(rsts[:, i], tr=self.rsfmri_tr, num_bands=5, band_of_interest=1)
+            rlfp[i] = compute_rlfp(rsts[:, i], tr=self.environment.rsfmri_tr, num_bands=5, band_of_interest=1)
 
         self.rlfp = rlfp
 
