3a5ecb2f1cb334292e11ae2a23671ef331c779cf
Linden Parkes <lindenparkes@gmail.com>
2021-04-12 10:55:58 -0400

    minor edits

--- scripts/job_submitter.sh
+++ scripts/job_submitter.sh
@@ -10,7 +10,7 @@ qsub -N avg_adj -l h_vmem=3G,s_vmem=3G -pe threaded 2 -j y -b y -o /cbica/home/p
 
 # average a matrix, null
 qsub -N avg_adj_null -l h_vmem=3G,s_vmem=3G -pe threaded 2 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
--t 1:5000 -tc 250 \
+-t 1:5000 -tc 500 \
 /cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
 /cbica/home/parkesl/research_projects/pfactor_gradients/scripts/pnc_average_adj_compute_control_energy_null.py
 
