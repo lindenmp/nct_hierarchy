5e0bca15dcf8b7b6e639a7a67f2d57b16a069cd6
Linden Parkes <lindenparkes@gmail.com>
2021-04-12 10:29:11 -0400

    init commit

---
+++ scripts/copy_cbica.sh
@@ -0,0 +1,6 @@
+# rsync
+rsync -avzh parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_projects/pfactor_gradients/output_cluster \
+/Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/
+
+# scp -r parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_projects/pfactor_gradients/output_cluster \
+# /Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/
