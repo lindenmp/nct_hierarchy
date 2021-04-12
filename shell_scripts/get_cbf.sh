18d4a89d32ef455978ff09e22b2b8c7531601277
Linden Parkes <lindenparkes@gmail.com>
2021-04-12 17:46:15 -0400

    init commit

---
+++ shell_scripts/get_cbf.sh
@@ -0,0 +1,11 @@
+indir=/Volumes/T7/research_data/PNC/processedData/asl/voxelwiseMaps_cbf/
+outdir=/Volumes/T7/research_data/PNC/processedData/asl/parcelwise_cbf/
+parc_file=/Volumes/T7/research_data/PNC/Schaefer2018_400_17Networks_PNC_2mm.nii.gz
+cd ${indir}
+
+for i in *.nii.gz; do
+  name=$(echo "$i" | cut -f 1 -d '.')
+  echo $name
+  fslmeants -i ${indir}${i} --label=${parc_file} -o ${outdir}${name}_schaefer400_17.txt
+done
+
