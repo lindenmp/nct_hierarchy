b52690a56a73347abe9ad90370e3d0f1e410c523
Linden Parkes <lindenparkes@gmail.com>
2021-04-12 22:40:09 -0400

    minor edits

--- shell_scripts/job_submitter.sh
+++ shell_scripts/job_submitter.sh
@@ -4,7 +4,7 @@ conda activate pfactor_gradients
 
 # average a matrix
 qsub -N avg_adj -l h_vmem=3G,s_vmem=3G -pe threaded 2 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
--t 1:4 \
+-t 1:5 \
 /cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
 /cbica/home/parkesl/research_projects/pfactor_gradients/scripts/pnc_average_adj_compute_control_energy.py
 
@@ -16,6 +16,6 @@ qsub -N avg_adj_null -l h_vmem=3G,s_vmem=3G -pe threaded 2 -j y -b y -o /cbica/h
 
 # pnc subjects, array job
 qsub -N subjects -l h_vmem=3G,s_vmem=3G -pe threaded 1 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
--t 1:20 \
+-t 1:775 \
 /cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
 /cbica/home/parkesl/research_projects/pfactor_gradients/scripts/pnc_compute_control_energy.py
