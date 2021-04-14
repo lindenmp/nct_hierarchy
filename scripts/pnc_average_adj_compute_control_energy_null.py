672715381dfe7912f5cc0fd822addaedbfcfeb31
Linden Parkes <lindenparkes@gmail.com>
2021-04-13 20:51:15 -0400

    clean up

--- scripts/pnc_average_adj_compute_control_energy_null.py
+++ scripts/pnc_average_adj_compute_control_energy_null.py
@@ -74,10 +74,6 @@ ct = DataVector(data=np.nanmean(load_ct.ct, axis=0), name='ct')
 ct.rankdata(descending=False)
 ct.rescale_unit_interval()
 
-# ct_d = DataVector(data=np.nanmean(load_ct.ct, axis=0), name='ct-d')
-# ct_d.rankdata(descending=True)
-# ct_d.rescale_unit_interval()
-
 # %% load rlfp data
 load_rlfp = LoadRLFP(environment=environment, Subject=Subject)
 load_rlfp.run()
@@ -85,10 +81,6 @@ load_rlfp.run()
 rlfp = DataVector(data=np.nanmean(load_rlfp.rlfp, axis=0), name='rlfp')
 rlfp.rankdata(descending=False)
 rlfp.rescale_unit_interval()
-
-# rlfp_d = DataVector(data=np.nanmean(load_rlfp.rlfp, axis=0), name='rlfp-d')
-# rlfp_d.rankdata(descending=True)
-# rlfp_d.rescale_unit_interval()
 
 # %% load cbf data
 load_cbf = LoadCBF(environment=environment, Subject=Subject)
@@ -98,10 +90,6 @@ cbf = DataVector(data=np.nanmean(load_cbf.cbf, axis=0), name='cbf')
 cbf.rankdata(descending=False)
 cbf.rescale_unit_interval()
 
-# cbf_d = DataVector(data=np.nanmean(load_cbf.cbf, axis=0), name='cbf-d')
-# cbf_d.rankdata(descending=True)
-# cbf_d.rescale_unit_interval()
-
 # %% load reho data
 load_reho = LoadREHO(environment=environment, Subject=Subject)
 load_reho.run()
@@ -109,10 +97,6 @@ load_reho.run()
 reho = DataVector(data=np.nanmean(load_reho.reho, axis=0), name='reho')
 reho.rankdata(descending=False)
 reho.rescale_unit_interval()
-
-# reho_d = DataVector(data=np.nanmean(load_reho.reho, axis=0), name='reho-d')
-# reho_d.rankdata(descending=True)
-# reho_d.rescale_unit_interval()
 
 # %% load alff data
 load_alff = LoadALFF(environment=environment, Subject=Subject)
@@ -122,43 +106,27 @@ alff = DataVector(data=np.nanmean(load_alff.alff, axis=0), name='alff')
 alff.rankdata(descending=False)
 alff.rescale_unit_interval()
 
-# alff_d = DataVector(data=np.nanmean(load_alff.alff, axis=0), name='alff-d')
-# alff_d.rankdata(descending=True)
-# alff_d.rescale_unit_interval()
-
 # %% get control energy
 file_prefix = 'average_adj_n-{0}_s-{1}_'.format(load_average_sc.load_sc.df.shape[0], spars_thresh)
 n_subsamples = 0
 
 # %% brain map null (spin test)
-
 ct.shuffle_data(shuffle_indices=environment.spun_indices)
 ct_null = DataVector(data=ct.data_shuf[:, sge_task_id].copy(), name='ct-null-{0}'.format(sge_task_id))
-# ct_d.shuffle_data(shuffle_indices=environment.spun_indices)
-# ct_d_null = DataVector(data=ct_d.data_shuf[:, sge_task_id].copy(), name='ct-d-null-{0}'.format(sge_task_id))
 
 rlfp.shuffle_data(shuffle_indices=environment.spun_indices)
 rlfp_null = DataVector(data=rlfp.data_shuf[:, sge_task_id].copy(), name='rlfp-null-{0}'.format(sge_task_id))
-# rlfp_d.shuffle_data(shuffle_indices=environment.spun_indices)
-# rlfp_d_null = DataVector(data=rlfp_d.data_shuf[:, sge_task_id].copy(), name='rlfp-d-null-{0}'.format(sge_task_id))
 
 cbf.shuffle_data(shuffle_indices=environment.spun_indices)
 cbf_null = DataVector(data=cbf.data_shuf[:, sge_task_id].copy(), name='cbf-null-{0}'.format(sge_task_id))
-# cbf_d.shuffle_data(shuffle_indices=environment.spun_indices)
-# cbf_d_null = DataVector(data=cbf_d.data_shuf[:, sge_task_id].copy(), name='cbf-d-null-{0}'.format(sge_task_id))
 
 reho.shuffle_data(shuffle_indices=environment.spun_indices)
 reho_null = DataVector(data=reho.data_shuf[:, sge_task_id].copy(), name='reho-null-{0}'.format(sge_task_id))
-# reho_d.shuffle_data(shuffle_indices=environment.spun_indices)
-# reho_d_null = DataVector(data=reho_d.data_shuf[:, sge_task_id].copy(), name='reho-d-null-{0}'.format(sge_task_id))
 
 alff.shuffle_data(shuffle_indices=environment.spun_indices)
 alff_null = DataVector(data=alff.data_shuf[:, sge_task_id].copy(), name='alff-null-{0}'.format(sge_task_id))
-# alff_d.shuffle_data(shuffle_indices=environment.spun_indices)
-# alff_d_null = DataVector(data=alff_d.data_shuf[:, sge_task_id].copy(), name='alff-d-null-{0}'.format(sge_task_id))
 
 B_list = [ct_null, rlfp_null, cbf_null, reho_null, alff_null]
-          # ct_d_null, rlfp_d_null, cbf_d_null, reho_d_null, alff_d_null]
 for B_entry in B_list:
     nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A,
                                                states=compute_gradients.grad_bins, n_subsamples=n_subsamples,
@@ -166,36 +134,8 @@ for B_entry in B_list:
                                                force_rerun=False, save_outputs=True, verbose=True)
     nct_pipeline.run()
 
-# %% brain map null (random, no spatial preservation)
-# ct.shuffle_data(n_shuffles=10000)
-# if descending:
-#     ct_rnull = DataVector(data=ct.data_shuf[:, sge_task_id].copy(), name='ct-d-rnull-{0}'.format(sge_task_id))
-# else:
-#     ct_rnull = DataVector(data=ct.data_shuf[:, sge_task_id].copy(), name='ct-rnull-{0}'.format(sge_task_id))
-#
-# rlfp.shuffle_data(n_shuffles=10000)
-# if descending:
-#     rlfp_rnull = DataVector(data=rlfp.data_shuf[:, sge_task_id].copy(), name='rlfp-d-rnull-{0}'.format(sge_task_id))
-# else:
-#     rlfp_rnull = DataVector(data=rlfp.data_shuf[:, sge_task_id].copy(), name='rlfp-rnull-{0}'.format(sge_task_id))
-#
-# cbf.shuffle_data(n_shuffles=10000)
-# if descending:
-#     cbf_rnull = DataVector(data=cbf.data_shuf[:, sge_task_id].copy(), name='cbf-d-rnull-{0}'.format(sge_task_id))
-# else:
-#     cbf_rnull = DataVector(data=cbf.data_shuf[:, sge_task_id].copy(), name='cbf-rnull-{0}'.format(sge_task_id))
-#
-# B_list = [ct_rnull, rlfp_rnull, cbf_rnull]
-# for B_entry in B_list:
-#     nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A,
-#                                                states=compute_gradients.grad_bins, n_subsamples=n_subsamples,
-#                                                control='minimum_fast', T=1, B=B_entry, file_prefix=file_prefix,
-#                                                force_rerun=False, save_outputs=True, verbose=True)
-#     nct_pipeline.run()
-
 # %% network null
 A_list = [Wwp, ]
-# A_list = [Wwp, Wsp, Wssp]
 file_prefixes = ['average_adj_n-{0}_s-{1}_null-mni-wwp-{2}_'.format(load_average_sc.load_sc.df.shape[0], spars_thresh, sge_task_id),
                  'average_adj_n-{0}_s-{1}_null-mni-wsp-{2}_'.format(load_average_sc.load_sc.df.shape[0], spars_thresh, sge_task_id),
                  'average_adj_n-{0}_s-{1}_null-mni-wssp-{2}_'.format(load_average_sc.load_sc.df.shape[0], spars_thresh, sge_task_id)]
