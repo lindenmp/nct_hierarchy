3d047779722f31236d611ab11f08f7a97b3d29cc
Linden Parkes <lindenparkes@gmail.com>
2021-04-15 08:08:07 -0400

    refactor

--- scripts/results_meanadj_control_energy.py
+++ scripts/results_meanadj_control_energy.py
@@ -3,11 +3,12 @@ import numpy as np
 import scipy as sp
 import pandas as pd
 from pfactor_gradients.pnc import Environment, Subject
-from pfactor_gradients.routines import LoadSC, LoadCT, LoadRLFP
+from pfactor_gradients.routines import LoadSC, LoadCT, LoadRLFP, LoadCBF, LoadREHO, LoadALFF, LoadAverageBrainMaps
 from pfactor_gradients.imaging_derivs import DataMatrix, DataVector
 from pfactor_gradients.pipelines import ComputeGradients
 from pfactor_gradients.plotting import my_regplot
-from pfactor_gradients.utils import get_pdist_clusters, get_exact_p
+from pfactor_gradients.utils import get_pdist_clusters, get_fdr_p
+from tqdm import tqdm
 
 # %% Plotting
 import matplotlib.pyplot as plt
@@ -43,120 +44,91 @@ load_sc.run()
 # refilter environment due to LoadSC excluding on disconnected nodes
 environment.df = load_sc.df.copy()
 
-# %% load ct data
-load_ct = LoadCT(environment=environment, Subject=Subject)
-load_ct.run()
+# %% load mean brain maps
+descending=False
+loaders_dict = {
+    'ct': LoadCT(environment=environment, Subject=Subject),
+    # 'rlfp': LoadRLFP(environment=environment, Subject=Subject),
+    'cbf': LoadCBF(environment=environment, Subject=Subject),
+    'reho': LoadREHO(environment=environment, Subject=Subject),
+    'alff': LoadALFF(environment=environment, Subject=Subject)
+}
 
-ct = DataVector(data=np.nanmean(load_ct.ct, axis=0), name='ct')
-ct.rankdata()
-ct.rescale_unit_interval()
+load_average_bms = LoadAverageBrainMaps(loaders_dict=loaders_dict)
+load_average_bms.run()
 
-# %% load rlfp data
-load_rlfp = LoadRLFP(environment=environment, Subject=Subject)
-load_rlfp.run()
+for key in load_average_bms.brain_maps:
+    load_average_bms.brain_maps[key].mean_between_states(compute_gradients.grad_bins)
 
-rlfp = DataVector(data=np.nanmean(load_rlfp.rlfp, axis=0), name='rlfp')
-rlfp.rankdata()
-rlfp.rescale_unit_interval()
-
-# %%  1) mni/gradient distance between hierarchy clusters
-dist_mni = get_pdist_clusters(environment.centroids.values, compute_gradients.kmeans.labels_, method='median')
-dist_mni[np.eye(dist_mni.shape[0]) == 1] = np.nan
-
-dist_h = sp.spatial.distance.squareform(sp.spatial.distance.pdist(compute_gradients.kmeans.cluster_centers_))
-dist_h[np.eye(dist_h.shape[0]) == 1] = np.nan
-
-# Plot
-f, ax = plt.subplots(1, 3, figsize=(15, 4))
-sns.heatmap(dist_mni, ax=ax[0], square=True)
-ax[0].set_title('Distance (MNI)')
-sns.heatmap(dist_h, ax=ax[1], square=True)
-ax[1].set_title('Distance (Hierarchy)')
-my_regplot(dist_mni, dist_h, 'Distance (MNI)', 'Distance (Hierarchy)', ax[2])
-f.savefig(os.path.join(environment.figdir, 'distance_vs_distance.png'), dpi=150, bbox_inches='tight', pad_inches=0.1)
-plt.close()
-
-# %% get minimum control energy
-mask = ~np.eye(compute_gradients.kmeans.n_clusters, dtype=bool)
+n_clusters = len(np.unique(compute_gradients.grad_bins))
+mask = ~np.eye(n_clusters, dtype=bool)
 indices = np.where(mask)
 
-files = ['average_adj_n-775_s-0.06_ns-20-20_c-minimum_fast_T-1_B-wb_E.npy',
-         'average_adj_n-775_s-0.06_ns-20-20_c-minimum_fast_T-1_B-ct_E.npy',
-         'average_adj_n-775_s-0.06_ns-20-20_c-minimum_fast_T-1_B-rlfp_E.npy',
-         ]
-# 'noise_average_adj_n-775_s-0.06_ns-20-20_c-minimum_fast_T-1_B-wb_E.npy',
+# %% get minimum control energy
+my_list = ['wb',] + list(load_average_bms.brain_maps.keys())
+df = pd.DataFrame()
 
-column_labels = ['identity', 'ct', 'rlfp']
-
-df_plot = pd.DataFrame()
-
-for i, file in enumerate(files):
+for i in my_list:
+    file = 'average_adj_n-775_s-0.06_ns-40-0_c-minimum_fast_T-1_B-{0}_E.npy'.format(i)
     E = np.load(os.path.join(environment.pipelinedir, 'minimum_control_energy', file))
-    E = DataMatrix(data=E)
-    df_plot[column_labels[i]] = E.data[indices]
-
-# unique, counts = np.unique(compute_gradients.kmeans.labels_, return_counts=True)
-# # np.tile(counts, (len(counts), 1))
-# for i in np.arange(20):
-#     mask = ~np.isnan(E.data[i, :])
-#     print(sp.stats.pearsonr(counts[mask], E.data[i, :][mask]))
-
-# %% Plots
+    df[i] = E[indices]
 
 # %% energy distributions
-f, ax = plt.subplots(1, 1, figsize=(5, 5))
-sns.violinplot(data=df_plot, ax=ax)
+f, ax = plt.subplots(1, 1, figsize=(5, 2.5))
+sns.violinplot(data=df, ax=ax, scale='width')
 f.savefig(os.path.join(environment.figdir, 'energy_dist.png'), dpi=150, bbox_inches='tight',
           pad_inches=0.1)
 plt.close()
 
-print(df_plot['ct'].mean() - df_plot['rlfp'].mean(),
-      get_exact_p(df_plot['ct'], df_plot['rlfp']),
-      sp.stats.ttest_rel(df_plot['ct'], df_plot['rlfp']))
+for i in my_list:
+    for j in my_list:
+        if i != j:
+            if sp.stats.ttest_rel(df[i], df[j])[1] < 0.05:
+                print('t test', i, j, np.round(sp.stats.ttest_rel(df[i], df[j]), 4))
+            if sp.stats.ks_2samp(df[i], df[j])[1] < 0.05:
+                print('ks test', i, j, np.round(sp.stats.ks_2samp(df[i], df[j]), 4))
+
+# %% Does energy correlate to corresponding brain maps?
+for key in load_average_bms.brain_maps:
+    f, ax = plt.subplots(1, 1, figsize=(5, 5))
+    my_regplot(load_average_bms.brain_maps[key].data_mean[indices], df[key], key, 'Energy ({0})'.format(key), ax)
+    f.savefig(os.path.join(environment.figdir, 'corr({0},energy_{0}).png'.format(key)), dpi=150, bbox_inches='tight',
+              pad_inches=0.1)
+    plt.close()
+
+# %% How does energy diverge from identity when adding a brain map?
+for key in load_average_bms.brain_maps:
+    f, ax = plt.subplots(1, 1, figsize=(5, 5))
+    my_regplot(df['wb'], df[key], 'Energy (wb)', 'Energy ({0})'.format(key), ax)
+    f.savefig(os.path.join(environment.figdir, 'corr(energy_wb,energy_{0}).png'.format(key)), dpi=150, bbox_inches='tight',
+              pad_inches=0.1)
+    plt.close()
 
 # %% Does energy vary over the state transitions according to the neurobiological feature that is used for optimization?
-f, ax = plt.subplots(1, 1, figsize=(5, 5))
-my_regplot(ct.data, rlfp.data, 'ct', 'rlfp', ax)
-f.savefig(os.path.join(environment.figdir, 'corr(ct,rlfp).png'), dpi=150, bbox_inches='tight',
-          pad_inches=0.1)
-plt.close()
+df_corr = pd.DataFrame(index=my_list, columns=my_list)
+df_p = pd.DataFrame(index=my_list, columns=my_list)
 
-ct.mean_over_clusters(compute_gradients.kmeans.labels_)
-rlfp.mean_over_clusters(compute_gradients.kmeans.labels_)
+for i in my_list:
+    for j in my_list:
+        df_corr.loc[i, j] = sp.stats.spearmanr(df[i], df[j])[0]
+        df_p.loc[i, j] = sp.stats.spearmanr(df[i], df[j])[1]
+df_corr = df_corr.astype(float)
+df_p = df_p.astype(float)
 
-f, ax = plt.subplots(1, 1, figsize=(5, 5))
-my_regplot(ct.data_clusters, rlfp.data_clusters, 'ct (states)', 'rlfp (states)', ax)
-f.savefig(os.path.join(environment.figdir, 'corr(ct_states,rlfp_states).png'), dpi=150, bbox_inches='tight',
-          pad_inches=0.1)
-plt.close()
+# Generate a mask for the upper triangle
+mask = np.triu(np.ones_like(df_corr, dtype=bool))
+n_tests = np.sum(~mask.flatten())
 
-f, ax = plt.subplots(1, 1, figsize=(5, 5))
-my_regplot(ct.data_clusters[indices], df_plot['ct'], 'ct', 'Energy (ct)', ax)
-f.savefig(os.path.join(environment.figdir, 'corr(ct,energy_ct).png'), dpi=150, bbox_inches='tight',
-          pad_inches=0.1)
-plt.close()
+# p_corr = get_fdr_p(df_p.values.flatten()[~mask.flatten()])
+# df_p_corr = df_p.copy()
+# df_p_corr[:] = np.nan
+# df_p_corr.iloc[~mask] = p_corr
+# mask = np.logical_or(mask, df_p_corr > 0.05)
 
-f, ax = plt.subplots(1, 1, figsize=(5, 5))
-my_regplot(rlfp.data_clusters[indices], df_plot['rlfp'], 'rlfp', 'Energy (rlfp)', ax)
-f.savefig(os.path.join(environment.figdir, 'corr(rlfp,energy_rlfp).png'), dpi=150, bbox_inches='tight',
-          pad_inches=0.1)
-plt.close()
-
-# %% Does energy correlate across different maps?
-f, ax = plt.subplots(1, 1, figsize=(5, 5))
-my_regplot(df_plot['identity'], df_plot['ct'], 'Energy (identity)', 'Energy (ct)', ax)
-f.savefig(os.path.join(environment.figdir, 'corr(energy_identity,energy_ct).png'), dpi=150, bbox_inches='tight',
-          pad_inches=0.1)
-plt.close()
+mask = np.logical_or(mask, df_p > (0.05 / n_tests))
 
 f, ax = plt.subplots(1, 1, figsize=(5, 5))
-my_regplot(df_plot['identity'], df_plot['rlfp'], 'Energy (identity)', 'Energy (rlfp)', ax)
-f.savefig(os.path.join(environment.figdir, 'corr(energy_identity,energy_rlfp).png'), dpi=150, bbox_inches='tight',
-          pad_inches=0.1)
-plt.close()
-
-f, ax = plt.subplots(1, 1, figsize=(5, 5))
-my_regplot(df_plot['ct'], df_plot['rlfp'], 'Energy (ct)', 'Energy (rlfp)', ax)
-f.savefig(os.path.join(environment.figdir, 'corr(energy_ct,energy_rlfp).png'), dpi=150, bbox_inches='tight',
+sns.heatmap(df_corr, mask=mask, ax=ax, square=True, center=0, cmap='coolwarm')
+f.savefig(os.path.join(environment.figdir, 'corr_energy.png'), dpi=150, bbox_inches='tight',
           pad_inches=0.1)
 plt.close()
