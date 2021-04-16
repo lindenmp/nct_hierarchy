f5c1c4dc8aa64ca5a6b5fa2fd7f9fe7c9c8dac37
Linden Parkes <lindenparkes@gmail.com>
2021-04-16 17:25:07 -0400

    added prediction from just brain maps (e.g., ct) for comparisons

--- scripts/pnc_run_prediction.py
+++ scripts/pnc_run_prediction.py
@@ -4,7 +4,7 @@ import numpy as np
 if platform.system() == 'Linux':
     sys.path.extend(['/cbica/home/parkesl/research_projects/pfactor_gradients'])
 from pfactor_gradients.pnc import Environment, Subject
-from pfactor_gradients.routines import LoadSC
+from pfactor_gradients.routines import LoadSC, LoadCT, LoadRLFP, LoadCBF, LoadREHO, LoadALFF
 from pfactor_gradients.pipelines import ComputeGradients
 from pfactor_gradients.pipelines import Regression
 
@@ -46,16 +46,9 @@ load_sc = LoadSC(environment=environment, Subject=Subject)
 load_sc.run()
 # refilter environment due to LoadSC excluding on disconnected nodes
 environment.df = load_sc.df.copy()
-
-# %% get minimum control energy
 n_subs = environment.df.shape[0]
-n_states = len(np.unique(compute_gradients.grad_bins))
-mask = ~np.eye(n_states, dtype=bool)
-indices = np.where(mask)
-# indices = np.triu_indices(n_states, k=1)
-n_transitions = len(indices[0])
 
-# %% prediction
+# %% prediction settings
 def get_prediction_pipeline(sge_task_id=np.nan):
     X_name = 'wb'
     y_name = 'Overall_Psychopathology'
@@ -96,46 +89,85 @@ def get_prediction_pipeline(sge_task_id=np.nan):
 
     elif sge_task_id == 10:
         X_name = 'wb'
-        alg = 'krr_rbf'
+        runpca = 25
     elif sge_task_id == 11:
         X_name = 'ct'
-        alg = 'krr_rbf'
+        runpca = 25
     elif sge_task_id == 12:
         X_name = 'cbf'
-        alg = 'krr_rbf'
+        runpca = 25
     elif sge_task_id == 13:
         X_name = 'reho'
-        alg = 'krr_rbf'
+        runpca = 25
     elif sge_task_id == 14:
         X_name = 'alff'
-        alg = 'krr_rbf'
+        runpca = 25
 
     elif sge_task_id == 15:
         X_name = 'wb'
-        alg = 'krr_rbf'
         score = 'corr'
+        runpca = 25
     elif sge_task_id == 16:
         X_name = 'ct'
-        alg = 'krr_rbf'
         score = 'corr'
+        runpca = 25
     elif sge_task_id == 17:
         X_name = 'cbf'
-        alg = 'krr_rbf'
         score = 'corr'
+        runpca = 25
     elif sge_task_id == 18:
         X_name = 'reho'
-        alg = 'krr_rbf'
         score = 'corr'
+        runpca = 25
     elif sge_task_id == 19:
         X_name = 'alff'
-        alg = 'krr_rbf'
         score = 'corr'
+        runpca = 25
 
     return X_name, y_name, c_name, covs, alg, score, n_splits, runpca, n_rand_splits
 
-# load settings
 X_name, y_name, c_name, covs, alg, score, n_splits, runpca, n_rand_splits = get_prediction_pipeline(
     sge_task_id=sge_task_id)
+
+y = environment.df.loc[:, y_name].values
+c = environment.df.loc[:, covs]
+c['sex'] = c['sex'] - 1
+c = c.values
+
+# %% prediction from brain maps (no NCT)
+if X_name != 'wb':
+    if X_name == 'ct':
+        loader = LoadCT(environment=environment, Subject=Subject)
+    elif X_name == 'cbf':
+        loader = LoadCBF(environment=environment, Subject=Subject)
+    elif X_name == 'rlfp':
+        loader = LoadRLFP(environment=environment, Subject=Subject)
+    elif X_name == 'reho':
+        loader = LoadREHO(environment=environment, Subject=Subject)
+    elif X_name == 'alff':
+        loader = LoadALFF(environment=environment, Subject=Subject)
+
+    loader.run()
+    X = loader.values
+
+    # ct: 1% = 13 pcs
+    # cbf: 1% = 9 pcs
+    # reho: 1% = 10 pcs
+    # alff: 1% = 8 pcs
+    regression = Regression(environment=environment, X=X, y=y, c=c, X_name=X_name, y_name=y_name, c_name=c_name,
+                            alg=alg, score=score, n_splits=n_splits, runpca=runpca, n_rand_splits=n_rand_splits,
+                            force_rerun=False)
+    regression.run()
+    regression.run_perm()
+else:
+    pass
+
+# %% prediction from energy
+n_states = len(np.unique(compute_gradients.grad_bins))
+mask = ~np.eye(n_states, dtype=bool)
+indices = np.where(mask)
+# indices = np.triu_indices(n_states, k=1)
+n_transitions = len(indices[0])
 
 # load energy
 X = np.zeros((n_subs, n_transitions))
@@ -145,13 +177,13 @@ for i in np.arange(n_subs):
     E = np.load(os.path.join(environment.pipelinedir, 'minimum_control_energy', file))
     X[i, :] = E[indices]
 
-y = environment.df.loc[:, y_name].values
-c = environment.df.loc[:, covs]
-c['sex'] = c['sex'] - 1
-c = c.values
-
-regression = Regression(environment=environment, X=X, y=y, c=c, X_name=X_name, y_name=y_name, c_name=c_name,
+# wb: 1% = 37 pcs
+# ct: 1% = 33 pcs
+# cbf: 1% = 24 pcs
+# reho: 1% = 21 pcs
+# alff: 1% = 19 pcs
+regression = Regression(environment=environment, X=X, y=y, c=c, X_name='energy-{0}'.format(X_name), y_name=y_name, c_name=c_name,
                         alg=alg, score=score, n_splits=n_splits, runpca=runpca, n_rand_splits=n_rand_splits,
-                        force_rerun=True)
+                        force_rerun=False)
 regression.run()
 regression.run_perm()
