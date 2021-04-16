5b3def73fc5625911d65191c899a78f1af8c6218
Linden Parkes <lindenparkes@gmail.com>
2021-04-16 11:32:34 -0400

    added new Regression()

--- pfactor_gradients/pipelines.py
+++ pfactor_gradients/pipelines.py
@@ -1,12 +1,18 @@
 import os
 import numpy as np
-from sklearn.cluster import KMeans
+import pandas as pd
 from pfactor_gradients.routines import LoadFC
 from pfactor_gradients.imaging_derivs import DataVector
-from brainspace.gradient import GradientMaps
-import abagen
-import pandas as pd
 from pfactor_gradients.energy import control_energy_helper
+from pfactor_gradients.prediction import corr_true_pred, root_mean_squared_error, run_reg, run_perm
+from brainspace.gradient import GradientMaps
+from sklearn.cluster import KMeans
+from sklearn.linear_model import Ridge, Lasso, LinearRegression
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.svm import SVR
+from sklearn.metrics import make_scorer, r2_score, mean_squared_error, mean_absolute_error
+import abagen
+from tqdm import tqdm
 
 # %% Plotting
 import matplotlib.pyplot as plt
@@ -243,3 +249,152 @@ class ComputeMinimumControlEnergy():
                 np.save(os.path.join(self._output_dir(), file_prefix+'E'), self.E)
                 if self.control != 'minimum_fast':
                     np.save(os.path.join(self._output_dir(), file_prefix+'n_err'), self.n_err)
+
+
+class Regression():
+    def __init__(self, environment, X, y, c, X_name='X', y_name='y', c_name='c',
+                 alg='rr', score='rmse', n_splits=10, runpca=False, n_rand_splits=100,
+                 force_rerun=False):
+        self.environment = environment
+        self.X = X
+        self.y = y
+        self.c = c
+
+        self.X_name = X_name
+        self.y_name = y_name
+        self.c_name = c_name
+
+        self.alg = alg
+        self.score = score
+        self.n_splits = n_splits
+        self.runpca = runpca
+        self.n_rand_splits = n_rand_splits
+
+        self.force_rerun = force_rerun
+
+    def _output_dir(self):
+        return os.path.join(self.environment.pipelinedir, 'prediction')
+
+    def _get_file_prefix(self):
+        file_prefix = '{0}-{1}-{2}_alg-{3}_score-{4}_pca-{5}'.format(self.X_name, self.y_name, self.c_name,
+                                                                     self.alg, self.score, self.runpca)
+
+        return file_prefix
+
+    def _output_files(self):
+        file_prefix = self._get_file_prefix()
+
+        primary_files = ['{0}_accuracy_mean.txt'.format(file_prefix), '{0}_accuracy_std.txt'.format(file_prefix)]
+        secondary_file = '{0}_accuracy_perm.txt'.format(file_prefix)
+
+        return primary_files, secondary_file
+
+    def _check_primary_outputs(self):
+        if os.path.exists(self._output_dir()):
+            primary_files, secondary_file = self._output_files()
+            file_exists = []
+            for file in primary_files:
+                file_exists.append(os.path.isfile(os.path.join(self._output_dir(), file)))
+
+            if np.all(file_exists):
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def _check_secondary_outputs(self):
+        primary_files, secondary_file = self._output_files()
+
+        if os.path.exists(self._output_dir()) and os.path.isfile(os.path.join(self._output_dir(), secondary_file)):
+            return True
+        else:
+            return False
+
+    def _print_settings(self):
+        print('\tsettings:')
+        print('\t\tX: {0}'.format(self.X_name))
+        print('\t\ty: {0}'.format(self.y_name))
+        print('\t\tc: {0}'.format(self.c_name))
+        print('\t\talg: {0}'.format(self.alg))
+        print('\t\tscore: {0}'.format(self.score))
+        print('\t\tn_splits: {0}'.format(self.n_splits))
+        print('\t\trunpca: {0}'.format(self.runpca))
+        print('\t\tn_rand_splits: {0}'.format(self.n_rand_splits))
+
+    def _get_reg(self):
+        regs = {'linear': LinearRegression(),
+                'rr': Ridge(),
+                'lr': Lasso(),
+                'krr_lin': KernelRidge(kernel='linear'),
+                'krr_rbf': KernelRidge(kernel='rbf'),
+                'svr_lin': SVR(kernel='linear'),
+                'svr_rbf': SVR(kernel='rbf')
+                }
+        self.reg = regs[self.alg]
+
+    def _get_scorer(self):
+        if self.score == 'r2':
+            self.scorer = make_scorer(r2_score, greater_is_better=True)
+        elif self.score == 'corr':
+            self.scorer = make_scorer(corr_true_pred, greater_is_better=True)
+        elif self.score == 'mse':
+            self.scorer = make_scorer(mean_squared_error, greater_is_better=False)
+        elif self.score == 'rmse':
+            self.scorer = make_scorer(root_mean_squared_error, greater_is_better=False)
+        elif self.score == 'mae':
+            self.scorer = make_scorer(mean_absolute_error, greater_is_better=False)
+
+    def run(self):
+        print('Pipeline: prediction (out-of-sample regression)')
+        self._print_settings()
+        file_prefix = self._get_file_prefix()
+        print('\t' + file_prefix)
+
+        if self._check_primary_outputs() and self.force_rerun == False:
+            print('\toutput already exists...skipping')
+            primary_files, secondary_file = self._output_files()
+            self.accuracy_mean = np.loadtxt(os.path.join(self._output_dir(), primary_files[0]))
+            self.accuracy_std = np.loadtxt(os.path.join(self._output_dir(), primary_files[1]))
+        else:
+            self._get_reg()
+            self._get_scorer()
+
+            accuracy_mean = np.zeros(self.n_rand_splits)
+            accuracy_std = np.zeros(self.n_rand_splits)
+
+            for i in tqdm(np.arange(self.n_rand_splits)):
+                accuracy, y_pred_out = run_reg(X=self.X, y=self.y, c=self.c, reg=self.reg,
+                                               scorer=self.scorer, n_splits=self.n_splits, runpca=self.runpca,
+                                               seed=i)
+                accuracy_mean[i] = accuracy.mean()
+                accuracy_std[i] = accuracy.std()
+
+            self.accuracy_mean = accuracy_mean
+            self.accuracy_std = accuracy_std
+
+            # save outputs
+            if not os.path.exists(self._output_dir()): os.makedirs(self._output_dir())
+            primary_files, secondary_file = self._output_files()
+            np.savetxt(os.path.join(self._output_dir(), primary_files[0]), self.accuracy_mean)
+            np.savetxt(os.path.join(self._output_dir(), primary_files[1]), self.accuracy_std)
+
+    def run_perm(self):
+        print('Pipeline: prediction, permutation test')
+        if self._check_secondary_outputs() and self.force_rerun == False:
+            print('\toutput already exists...skipping')
+            primary_files, secondary_file = self._output_files()
+            self.accuracy_perm = np.loadtxt(os.path.join(self._output_dir(), secondary_file))
+        else:
+            self._get_reg()
+            self._get_scorer()
+
+            accuracy_perm = run_perm(X=self.X, y=self.y, c=self.c, reg=self.reg,
+                                    scorer=self.scorer, n_splits=self.n_splits, runpca=self.runpca)
+
+            self.accuracy_perm = accuracy_perm
+
+            # save outputs
+            if not os.path.exists(self._output_dir()): os.makedirs(self._output_dir())
+            primary_files, secondary_file = self._output_files()
+            np.savetxt(os.path.join(self._output_dir(), secondary_file), self.accuracy_perm)
