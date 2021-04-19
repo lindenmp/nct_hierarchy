2d0ae2dfaa571c04392282d2bf0130f1e82f5feb
Linden Parkes <lindenparkes@gmail.com>
2021-04-19 09:09:21 -0400

    prediction now as loop

--- shell_scripts/job_submitter.sh
+++ shell_scripts/job_submitter.sh
@@ -20,7 +20,21 @@ qsub -N subjects -l h_vmem=3G,s_vmem=3G -pe threaded 1 -j y -b y -o /cbica/home/
 /cbica/home/parkesl/research_projects/pfactor_gradients/scripts/pnc_compute_control_energy.py
 
 # prediction
-qsub -N prediction -l h_vmem=3G,s_vmem=3G -pe threaded 8 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
--t 1:20 \
-/cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
-/cbica/home/parkesl/research_projects/pfactor_gradients/scripts/pnc_run_prediction.py
+X_list=('wb' 'ct' 'cbf' 'reho' 'alff')
+alg_list=('rr' 'krr_rbf')
+score_list=('rmse' 'corr' 'r2')
+runpca_list=('1%' '80%' 25 50)
+
+for X_name in "${X_list[@]}"; do
+  for alg in "${alg_list[@]}"; do
+    for score in "${score_list[@]}"; do
+      for runpca in "${runpca_list[@]}"; do
+        qsub -N prediction -l h_vmem=3G,s_vmem=3G -pe threaded 8 -j y -b y -o \
+        /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
+        /cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
+        /cbica/home/parkesl/research_projects/pfactor_gradients/scripts/pnc_run_prediction.py \
+        -X_name $X_name -alg $alg -score $score -runpca $runpca
+      done
+    done
+  done
+done
