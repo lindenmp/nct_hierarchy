6afb0281c213ce053ca15e2aabf3e702dc8ede44
Linden Parkes <lindenparkes@gmail.com>
2021-05-04 22:26:46 -0400

    clean up and bug fix

--- matlab_functions/spdcm_firstlevel.m
+++ matlab_functions/spdcm_firstlevel.m
@@ -1,20 +1,11 @@
 function [] = spdcm_firstlevel(spmdir, rsts, tr, te, outdir)
-    % rng('default')
-
     % where spm12 is
-    % spmdir = '/Users/lindenmp/Google-Drive-Penn/work/matlab_tools/spm12';
     addpath(spmdir)
-
     cd(outdir)
 
-    % rsts_file = '/Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/output_cluster/pnc/schaefer_400_streamlineCount/pipelines/spdcm/rsts_states.npy';
-    % rsts = readNPY(rsts_file);
     dims = size(rsts);
     n_rois = dims(2);
-    % n_rois = 5;
     n_trs = dims(1);
-    % tr = 3;
-    % te = 0.032;
 
     % initialize
     spm('defaults','fmri')
@@ -26,16 +17,10 @@ function [] = spdcm_firstlevel(spmdir, rsts, tr, te, outdir)
     dcm.n = n_rois; % number of regions.
     dcm.v = n_trs; % number of time points.
 
-    % experimental inputs (there are none)
-    dcm.U.u = zeros(n_rois,1);
-    dcm.U.name = {'null'};
-
-    dcm.Y.dt  = tr; % add tr
-    dcm.Y.X0  = ones(n_rois,1); % confounds. none, add constant
-
     % add time series
+    dcm.Y.dt = tr; % add tr
+    dcm.Y.y = rsts;
     for i = 1:dcm.n
-        dcm.Y.y(:,i) = rsts(:, i);
         dcm.Y.name{i} = i;
     end
 
@@ -55,23 +40,22 @@ function [] = spdcm_firstlevel(spmdir, rsts, tr, te, outdir)
     dcm.options.maxnodes = n_rois;
 
     % add connections to model
-     a = ones(n_rois, n_rois); % specify fully connected model
+    a = ones(n_rois, n_rois); % specify fully connected model
 %    a = eye(n_rois);
 %    a = a + diag(ones(n_rois-1,1),1);
 %    a = a + diag(ones(n_rois-1,1),-1);
     dcm.a = a;
     dcm.b = zeros(dcm.n,dcm.n);
     dcm.c = zeros(dcm.n,1);
-    dcm.d = zeros(dcm.n,dcm.n,0);
-    % dcm.d = double.empty(dcm.n,dcm.n,0);
+%    dcm.d = zeros(dcm.n,dcm.n,0);
+    dcm.d = double.empty(dcm.n,dcm.n,0);
 
     % wrap dcm
     DCM{1} = dcm;
+    DCM{1}.name = ['dcm_ns-', num2str(n_rois)]
     DCM{1}
 
     % invert fully connected model
     fprintf(1, '\nInverting model... \n');
     DCM{1} = spm_dcm_fmri_csd(DCM{1});
-    % DCM{1} = spm_dcm_fit(DCM{1});
-    % DCM{1} = DCM{1}{1};
 end
