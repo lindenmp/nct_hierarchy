fbb0763a7d716cde6cff6b1080ef423a93877ef8
Linden Parkes <lindenparkes@gmail.com>
2021-05-13 15:02:19 -0400

    init commit

---
+++ shell_scripts/reslice_pnc_glasser.sh
@@ -0,0 +1,4 @@
+flirt -in /Volumes/T7/research_data/PNC/template/glasser/pncTemplateGlasser_Labels.nii.gz \
+-ref /Volumes/T7/research_data/PNC/template/pnc_template_brain_2mm.nii.gz \
+-out /Volumes/T7/research_data/PNC/template/glasser/pncTemplateGlasser_Labels_2mm.nii.gz \
+-applyxfm -interp nearestneighbour
