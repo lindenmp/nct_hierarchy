56b1ff6b8a7f49072fa6f79e207773e202c16d19
Linden Parkes <lindenparkes@gmail.com>
2021-05-13 18:02:27 -0400

    updates to standardize with get_ct.sh

--- shell_scripts/get_cbf.sh
+++ shell_scripts/get_cbf.sh
@@ -1,22 +1,22 @@
-indir=/Volumes/T7/research_data/PNC/processedData/asl/voxelwiseMaps_cbf/
-outdir=/Volumes/T7/research_data/PNC/processedData/asl/parcelwise_cbf/
-
-# schaefer 400
-parc_file=/Volumes/T7/research_data/PNC/Schaefer2018_400_17Networks_PNC_2mm.nii.gz
+# input data directories
+indir='/Volumes/T7/research_data/PNC/processedData/asl/voxelwiseMaps_cbf'
+outdir='/Volumes/T7/research_data/PNC/processedData/asl/parcelwise_cbf/'
 cd ${indir}
 
-for i in *.nii.gz; do
-  name=$(echo "$i" | cut -f 1 -d '.')
-  echo $name
-  fslmeants -i ${indir}${i} --label=${parc_file} -o ${outdir}${name}_schaefer400_17.txt
-done
+for scan in *_asl_quant_ssT1Std.nii.gz; do
+    echo ${scan}
+    out_label=$(echo "$scan" | cut -f 1 -d '.')
+    scanid=$(echo "$out_label" | cut -f 1 -d '_')
 
-# schaefer 200
-parc_file=/Volumes/T7/research_data/PNC/Schaefer2018_200_17Networks_PNC_2mm.nii.gz
-cd ${indir}
+    # schaefer 400
+    parc_file='/Volumes/T7/research_data/PNC/processedData/gm_masks_template_pnc/'${scanid}'_atropos3class_seg_GmMask_Template_Schaefer400_17.nii.gz'
+    fslmeants -i ${scan} --label=${parc_file} -o ${outdir}${out_label}_schaefer400_17.txt
 
-for i in *.nii.gz; do
-  name=$(echo "$i" | cut -f 1 -d '.')
-  echo $name
-  fslmeants -i ${indir}${i} --label=${parc_file} -o ${outdir}${name}_schaefer200_17.txt
+    # schaefer 200
+    parc_file='/Volumes/T7/research_data/PNC/processedData/gm_masks_template_pnc/'${scanid}'_atropos3class_seg_GmMask_Template_Schaefer200_17.nii.gz'
+    fslmeants -i ${scan} --label=${parc_file} -o ${outdir}${out_label}_schaefer200_17.txt
+
+    # glasser 360
+    parc_file='/Volumes/T7/research_data/PNC/template/glasser/pncTemplateGlasser_Labels_2mm.nii.gz'
+    fslmeants -i ${scan} --label=${parc_file} -o ${outdir}${out_label}_glasser.txt
 done
