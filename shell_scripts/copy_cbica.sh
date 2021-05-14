a50ed18a648d83ec547b26a1ddfe66d843760821
Linden Parkes <lindenparkes@gmail.com>
2021-05-14 09:52:53 -0400

    added rsync line to move data to cbica

--- shell_scripts/copy_cbica.sh
+++ shell_scripts/copy_cbica.sh
@@ -1,6 +1,10 @@
-# rsync
+# rsync outputs from cbica
 rsync -avzh parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_projects/pfactor_gradients/output_cluster \
 /Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/
+
+# rsync data to cbica
+rsync -avzh /Volumes/T7/research_data/PNC/processedData \
+parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_data/PNC/
 
 # scp -r parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_projects/pfactor_gradients/output_cluster \
 # /Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/
