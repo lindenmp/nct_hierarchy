e8857f2ba92ba97b5d41b407b211deef48d9afaf
Linden Parkes <lindenparkes@gmail.com>
2021-05-18 10:04:22 -0400

    minor edits

--- shell_scripts/job_submitter.sh
+++ shell_scripts/job_submitter.sh
@@ -1,17 +1,10 @@
-ssh cbica
-
 conda activate pfactor_gradients
 
-# average a matrix
-qsub -N avg_adj -l h_vmem=3G,s_vmem=3G -pe threaded 2 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
-/cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
-/cbica/home/parkesl/research_projects/pfactor_gradients/scripts/pnc_average_adj_compute_control_energy.py
-
 # average a matrix, null
-qsub -N avg_adj_null -l h_vmem=3G,s_vmem=3G -pe threaded 2 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
+qsub -N e_null -l h_vmem=3G,s_vmem=3G -pe threaded 2 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
 -t 1:10000 -tc 500 \
 /cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
-/cbica/home/parkesl/research_projects/pfactor_gradients/scripts/pnc_average_adj_compute_control_energy_null.py
+/cbica/home/parkesl/research_projects/pfactor_gradients/scripts/average_adj_control_energy_nulls.py
 
 # pnc subjects, array job
 qsub -N subjects -l h_vmem=3G,s_vmem=3G -pe threaded 1 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
