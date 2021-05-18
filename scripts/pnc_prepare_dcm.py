085fb7f89ec076c7d8a4c6f00e2ea1b6a3a9e9ce
Linden Parkes <lindenparkes@gmail.com>
2021-05-18 12:01:54 -0400

    refactor and cleanup

--- scripts/pnc_prepare_dcm.py
+++ scripts/pnc_prepare_dcm.py
@@ -1,34 +1,38 @@
+# %%
 import sys, os, platform
-import numpy as np
-import scipy as sp
-import pandas as pd
+from oct2py import octave
+if platform.system() == 'Linux':
+    sys.path.extend(['/cbica/home/parkesl/research_projects/pfactor_gradients'])
+    sys.path.append('/usr/bin/octave') # octave install path
+    octave.addpath('/gpfs/fs001/cbica/home/parkesl/research_projects/pfactor_gradients/geomsurr') # path to matlab functions
+elif platform.system() == 'Darwin':
+    sys.path.append('usr/local/bin/octave') # octave install path
+    octave.addpath('/Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/geomsurr') # path to matlab functions
+
 from pfactor_gradients.pnc import Environment, Subject
 from pfactor_gradients.routines import LoadSC
 from pfactor_gradients.pipelines import ComputeGradients, DCM
+
 from tqdm import tqdm
-
-# from oct2py import octave
-# if platform.system() == 'Linux':
-#     sys.path.extend(['/cbica/home/parkesl/research_projects/pfactor_gradients'])
-#     sys.path.append('/usr/bin/octave') # octave install path
-#     octave.addpath('/gpfs/fs001/cbica/home/parkesl/research_projects/pfactor_gradients/scripts') # path to matlab functions
-# elif platform.system() == 'Darwin':
-#     sys.path.append('usr/local/bin/octave') # octave install path
-#     octave.addpath('/Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/scripts') # path to matlab functions
-
-# %% Plotting
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-sns.set(style='white', context='talk', font_scale=1)
-import matplotlib.font_manager as font_manager
-fontpath = '/Users/lindenmp/Library/Fonts/PublicSans-Thin.ttf'
-prop = font_manager.FontProperties(fname=fontpath)
-plt.rcParams['font.family'] = prop.get_name()
-plt.rcParams['svg.fonttype'] = 'none'
+import numpy as np
+import scipy as sp
+import pandas as pd
 
 # %% Setup project environment
-computer = 'macbook'
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style='white', context='paper', font_scale=1)
+if platform.system() == 'Linux':
+    computer = 'cbica'
+elif platform.system() == 'Darwin':
+    computer = 'macbook'
+
+    import matplotlib.font_manager as font_manager
+    fontpath = '/Users/lindenmp/Library/Fonts/PublicSans-Thin.ttf'
+    prop = font_manager.FontProperties(fname=fontpath)
+    plt.rcParams['font.family'] = prop.get_name()
+    plt.rcParams['svg.fonttype'] = 'none'
+
 parc = 'schaefer'
 n_parcels = 400
 sc_edge_weight = 'streamlineCount'
@@ -41,16 +45,15 @@ filters = {'healthExcludev2': 0, 't1Exclude': 0,
            'b0ProtocolValidationStatus': 1, 'dti64ProtocolValidationStatus': 1, 'dti64Exclude': 0,
            'psychoactiveMedPsychv2': 0, 'restProtocolValidationStatus': 1, 'restExclude': 0}
 environment.load_metadata(filters)
-n_bins = 40
+n_bins = int(n_parcels/10)
 compute_gradients = ComputeGradients(environment=environment, Subject=Subject, n_bins=n_bins)
 compute_gradients.run()
-n_states = compute_gradients.n_states
 
-# %% indices
-mask = ~np.eye(compute_gradients.n_states, dtype=bool)
+n_states = len(np.unique(compute_gradients.grad_bins))
+mask = ~np.eye(n_states, dtype=bool)
 indices = np.where(mask)
-upper_indices = np.triu_indices(compute_gradients.n_states, k=1)
-lower_indices = np.tril_indices(compute_gradients.n_states, k=-1)
+indices_upper = np.triu_indices(n_states, k=1)
+indices_lower = np.tril_indices(n_states, k=-1)
 
 # %% Load sc data
 load_sc = LoadSC(environment=environment, Subject=Subject)
@@ -78,9 +81,9 @@ plt.close()
 # %%
 # rsts = dcm.rsts_states
 # print(rsts.shape)
-#
+
 # spmdir = '/Users/lindenmp/Google-Drive-Penn/work/matlab_tools/spm12'
 # outdir = '/Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/output_cluster/pnc/schaefer_400_streamlineCount/pipelines/spdcm'
 
 # octave.eval("rand('state',%i)" % 1)
-# octave.spdcm_firstlevel(spmdir, rsts, environment.rsfmri_tr, environment.rsfmri_te, outdir)
+# octave.spdcm_firstlevel_loop(spmdir, rsts, environment.rsfmri_tr, environment.rsfmri_te, outdir)
