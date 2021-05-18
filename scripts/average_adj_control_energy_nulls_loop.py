c381db7a658ceb95d2ddedbcb98c76aa00b755b0
Linden Parkes <lindenparkes@gmail.com>
2021-05-18 14:30:47 -0400

    refactor to reduce memory usage

--- scripts/average_adj_control_energy_nulls_loop.py
+++ scripts/average_adj_control_energy_nulls_loop.py
@@ -67,16 +67,6 @@ load_average_sc = LoadAverageSC(load_sc=load_sc, spars_thresh=spars_thresh)
 load_average_sc.run()
 A = load_average_sc.A.copy()
 
-# rewire mean adjacency matrix
-Wwp = np.zeros((n_parcels, n_parcels, n_perms))
-Wsp = np.zeros((n_parcels, n_parcels, n_perms))
-Wssp = np.zeros((n_parcels, n_parcels, n_perms))
-for sge_task_id in tqdm(np.arange(n_perms)):
-    D = sp.spatial.distance.pdist(environment.centroids, 'euclidean')
-    D = sp.spatial.distance.squareform(D)
-    octave.eval("rand('state',%i)" % sge_task_id)
-    Wwp[:, :, sge_task_id], Wsp[:, :, sge_task_id], Wssp[:, :, sge_task_id] = octave.geomsurr(A, D, 3, 2, nout=3)
-
 # %% load mean brain maps
 loaders_dict = {
     'ct': LoadCT(environment=environment, Subject=Subject),
@@ -118,47 +108,43 @@ for key in load_average_bms.brain_maps:
     np.save(os.path.join(nct_pipeline._output_dir(), '{0}E.npy'.format(out_prefix)), E)
 
 # %% network null
-A_list = [Wwp, Wsp, Wssp]
-# A_list = [Wwp,]
-file_prefixes = ['average_adj_n-{0}_s-{1}_null-mni-wwp'.format(load_average_sc.load_sc.df.shape[0], spars_thresh),
-                 'average_adj_n-{0}_s-{1}_null-mni-wsp'.format(load_average_sc.load_sc.df.shape[0], spars_thresh),
-                 'average_adj_n-{0}_s-{1}_null-mni-wssp'.format(load_average_sc.load_sc.df.shape[0], spars_thresh)]
+A_list = ['wwp', 'wsp', 'wssp']
+B_list = ['wb',] + list(load_average_bms.brain_maps.keys())
 
-for A_idx, A_entry in enumerate(A_list):
-    # 1) null network model without weighted brain map
-    E = np.zeros((compute_gradients.n_states, compute_gradients.n_states, n_perms))
-    for sge_task_id in tqdm(np.arange(n_perms)):
-        file_prefix = file_prefixes[A_idx]
-        file_prefix = '{0}-{1}_'.format(file_prefix, sge_task_id)
-
-        nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A_entry[:, :, sge_task_id],
-                                                   states=compute_gradients.grad_bins, n_subsamples=n_subsamples,
-                                                   control='minimum_fast', T=1, B='wb', file_prefix=file_prefix,
-                                                   force_rerun=False, save_outputs=save_outputs, verbose=False)
-        nct_pipeline.run()
-        E[:, :, sge_task_id] = nct_pipeline.E
-
-        # if output file exists, delete it
-        # if os.path.isfile(os.path.join(nct_pipeline._output_dir(), '{0}E.npy'.format(nct_pipeline._get_file_prefix()))):
-        #     os.remove(os.path.join(nct_pipeline._output_dir(), '{0}E.npy'.format(nct_pipeline._get_file_prefix())))
-
-    # save compiled E
-    my_list = nct_pipeline._get_file_prefix().split('_')
-    my_list[4] = ('-').join(my_list[4].split('-')[:-1])
-    out_prefix = ('_').join(my_list)
-    np.save(os.path.join(nct_pipeline._output_dir(), '{0}E.npy'.format(out_prefix)), E)
-
-    # 2) null network model with brain map
-    for key in load_average_bms.brain_maps:
+for A_entry in A_list:
+    for B in B_list:
         E = np.zeros((compute_gradients.n_states, compute_gradients.n_states, n_perms))
         for sge_task_id in tqdm(np.arange(n_perms)):
-            file_prefix = file_prefixes[A_idx]
-            file_prefix = '{0}-{1}_'.format(file_prefix, sge_task_id)
+            # rewire connectome
+            D = sp.spatial.distance.pdist(environment.centroids, 'euclidean')
+            D = sp.spatial.distance.squareform(D)
+            octave.eval("rand('state',%i)" % sge_task_id)
 
-            nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A_entry[:, :, sge_task_id],
-                                                       states=compute_gradients.grad_bins, n_subsamples=n_subsamples,
-                                                       control='minimum_fast', T=1, B=load_average_bms.brain_maps[key], file_prefix=file_prefix,
-                                                       force_rerun=False, save_outputs=save_outputs, verbose=False)
+            if A_entry == 'wwp':
+                file_prefix = 'average_adj_n-{0}_s-{1}_null-mni-wwp-{2}_'.format(load_average_sc.load_sc.df.shape[0],
+                                                                                 spars_thresh, sge_task_id)
+                A_null, _, _ = octave.geomsurr(A, D, 3, 2, nout=3)
+            elif A_entry == 'wsp':
+                file_prefix = 'average_adj_n-{0}_s-{1}_null-mni-wsp-{2}_'.format(load_average_sc.load_sc.df.shape[0],
+                                                                                 spars_thresh, sge_task_id)
+                _, A_null, _ = octave.geomsurr(A, D, 3, 2, nout=3)
+            elif A_entry == 'wssp':
+                file_prefix = 'average_adj_n-{0}_s-{1}_null-mni-wssp-{2}_'.format(load_average_sc.load_sc.df.shape[0],
+                                                                                  spars_thresh, sge_task_id)
+                _, _, A_null = octave.geomsurr(A, D, 3, 2, nout=3)
+
+            # compute energy
+            if B == 'wb':
+                nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A_null,
+                                                           states=compute_gradients.grad_bins, n_subsamples=n_subsamples,
+                                                           control='minimum_fast', T=1, B=B, file_prefix=file_prefix,
+                                                           force_rerun=False, save_outputs=save_outputs, verbose=False)
+            else:
+                nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A_null,
+                                                                   states=compute_gradients.grad_bins, n_subsamples=n_subsamples,
+                                                                   control='minimum_fast', T=1, B=load_average_bms.brain_maps[B], file_prefix=file_prefix,
+                                                                   force_rerun=False, save_outputs=save_outputs, verbose=False)
+
             nct_pipeline.run()
             E[:, :, sge_task_id] = nct_pipeline.E
 
