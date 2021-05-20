aa16a016df58e3829b21301dec661bfa51c12258
Linden Parkes <lindenparkes@gmail.com>
2021-05-20 13:35:57 -0400

    tweaks

--- shell_scripts/job_submitter.sh
+++ shell_scripts/job_submitter.sh
@@ -28,21 +28,25 @@ qsub -N e_null_collect -l h_vmem=16G,s_vmem=16G -pe threaded 2 -j y -b y -o /cbi
 
 # 3) prediction
 X_list=('wb' 'ct' 'cbf')
-y_list=('F1_Exec_Comp_Res_Accuracy' 'F1_Complex_Reasoning_Efficiency' 'F3_Executive_Efficiency')
+y_list=('F1_Exec_Comp_Res_Accuracy') # 'F1_Complex_Reasoning_Efficiency' 'F3_Executive_Efficiency'
+c_list=('asvm' 'svm' 'vm')
 alg_list=('rr')
-score_list=('rmse' 'corr')
-runpca_list=('1%' '80%' 50)
+score_list=('r2' 'rmse' 'corr')
+#runpca_list=('1%' '80%' 50)
+runpca_list=('80%')
 
 for X_name in "${X_list[@]}"; do
   for y_name in "${y_list[@]}"; do
-    for alg in "${alg_list[@]}"; do
-      for score in "${score_list[@]}"; do
-        for runpca in "${runpca_list[@]}"; do
-          qsub -N prediction -l h_vmem=3G,s_vmem=3G -pe threaded 8 -j y -b y -o \
-          /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
-          /cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
-          /cbica/home/parkesl/research_projects/pfactor_gradients/scripts/run_prediction.py \
-          -X_name $X_name -y_name $y_name -alg $alg -score $score -runpca $runpca
+    for c_name in "${c_list[@]}"; do
+      for alg in "${alg_list[@]}"; do
+        for score in "${score_list[@]}"; do
+          for runpca in "${runpca_list[@]}"; do
+            qsub -N prediction -l h_vmem=3G,s_vmem=3G -pe threaded 4 -j y -b y -o \
+            /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
+            /cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
+            /cbica/home/parkesl/research_projects/pfactor_gradients/scripts/run_prediction.py \
+            -X_name $X_name -y_name $y_name -c_name $c_name -alg $alg -score $score -runpca $runpca
+          done
         done
       done
     done
