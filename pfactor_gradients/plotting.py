e8a25ea30a2a7f6d507f7117cb7d227e0d083d43
Linden Parkes <lindenparkes@gmail.com>
2021-05-21 13:56:45 -0400

    minor edits to regplot

--- pfactor_gradients/plotting.py
+++ pfactor_gradients/plotting.py
@@ -107,15 +107,18 @@ def my_regplot(x, y, xlabel, ylabel, ax, c='gray'):
     except:
         pass
 
+    color_blue = sns.color_palette("Set1")[1]
     try:
         sns.kdeplot(x=x, y=y, ax=ax, color='gray', thresh=0.05, alpha=0.25)
     except:
         pass
-    sns.regplot(x=x, y=y, ax=ax, scatter=False)
+    sns.regplot(x=x, y=y, ax=ax, scatter=False, color=color_blue)
     ax.scatter(x=x, y=y, c=c, s=5, alpha=0.5)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel, labelpad=-1)
     ax.tick_params(pad=-2.5)
+    ax.grid(False)
+    sns.despine(right=True, top=True, ax=ax)
     pearson_stats = sp.stats.pearsonr(x, y)
     spearman_stats = sp.stats.spearmanr(x, y)
     textstr = 'r = {:.2f}, p = {:.2f} \nrho = {:.2f}, p = {:.2f}'.format(pearson_stats[0], pearson_stats[1],
