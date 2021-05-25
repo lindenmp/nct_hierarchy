935fa85e47839f2414b06638a0aca8a45e5663d8
Linden Parkes <lindenparkes@gmail.com>
2021-05-25 14:59:26 -0400

    switched font from PublicSans to Helvetica and made tweaks to figure size

--- scripts/results_energy_asymmetries.py
+++ scripts/results_energy_asymmetries.py
@@ -17,9 +17,12 @@ from nilearn import plotting
 
 sns.set(style='whitegrid', context='paper', font_scale=1)
 import matplotlib.font_manager as font_manager
-fontpath = '/Users/lindenmp/Library/Fonts/PublicSans-Thin.ttf'
+fontpath = '/System/Library/Fonts/HelveticaNeue.ttc'
 prop = font_manager.FontProperties(fname=fontpath)
-plt.rcParams['font.family'] = prop.get_name()
+prop.set_weight = 'thin'
+plt.rcParams['font.family'] = prop.get_family()
+plt.rcParams['font.sans-serif'] = prop.get_name()
+# plt.rcParams['font.weight'] = 'thin'
 plt.rcParams['svg.fonttype'] = 'none'
 
 # %% Setup project environment
@@ -97,13 +100,16 @@ for B in B_list:
     E[B] = nct_pipeline.E
 
 # %% plots
+figsize = 1.5
 
 # data for plotting
 B = 'wb'
 e = E[B] # energy matrix
 ed = e.transpose() - e # energy asymmetry matrix
+# save out mean ed for use in other scripts
+np.save(os.path.join(environment.pipelinedir, 'ed_{0}.npy'.format(B)), ed)
 
-network_null = 'wwp'
+network_null = 'wsp'
 file = 'average_adj_n-{0}_s-{1}_null-mni-{2}_ns-{3}-0_c-minimum_fast_T-1_B-{4}_E.npy'.format(n_subs, spars_thresh,
                                                                                              network_null, n_states, B)
 e_network_null = np.load(os.path.join(environment.pipelinedir, 'minimum_control_energy', file))
@@ -115,19 +121,19 @@ if B != 'wb':
 # %% 1) energy dists: top-down vs bottom-up
 e_norm = rank_int(e)
 df_plot = pd.DataFrame(data=np.vstack((e_norm[indices_upper], e_norm[indices_lower])).transpose(),
-                       columns=['Bottom-up', 'Top-down'])
+                       columns=['bottom-up', 'top-down'])
 
-f, ax = plt.subplots(1, 1, figsize=(2.5, 2.5))
-sns.violinplot(data=df_plot, ax=ax, inner="box", palette="pastel", cut=2, linewidth=2)
+f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
+sns.violinplot(data=df_plot, ax=ax, inner="box", palette="pastel", cut=2, linewidth=1.5)
 sns.despine(left=True, bottom=True)
-ax.set_ylabel("Energy (z-score)")
+ax.set_ylabel("energy (z-score)")
 ax.tick_params(pad=-2.5)
 t, p_val = sp.stats.ttest_rel(a=e_norm[indices_upper], b=e_norm[indices_lower])
 textstr = 't = {:.2f}; p = {:.2f}'.format(np.abs(t), p_val)
-ax.text(0.5, ax.get_ylim()[1]+ax.get_ylim()[1]*0.06, textstr,
+ax.text(0.5, ax.get_ylim()[1]+ax.get_ylim()[1]*0.15, textstr,
         horizontalalignment='center', verticalalignment='top')
 ax.axhline(y=ax.get_ylim()[1]-ax.get_ylim()[1]*0.05, xmin=0.25, xmax=0.75, color='k', linewidth=1)
-f.savefig(os.path.join(environment.figdir, 'e_{0}.png'.format(B)), dpi=300, bbox_inches='tight',
+f.savefig(os.path.join(environment.figdir, 'e_{0}.svg'.format(B)), dpi=300, bbox_inches='tight',
           pad_inches=0.1)
 plt.close()
 
@@ -136,12 +142,14 @@ plot_mask = np.zeros((n_states, n_states))
 plot_mask[indices_upper] = 1
 plot_mask = plot_mask.astype(bool)
 
-f, ax = plt.subplots(1, 1, figsize=(2.5, 2.5))
+f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
 sns.heatmap(rank_int(ed), mask=plot_mask, center=0, square=True, cmap='coolwarm', ax=ax, cbar_kws={"shrink": 0.80})
-ax.set_ylabel("Initial states (i)")
-ax.set_xlabel("Target states (j)")
+ax.set_ylabel("initial states")
+ax.set_xlabel("target states")
+ax.set_yticklabels('')
+ax.set_xticklabels('')
 ax.tick_params(pad=-2.5)
-f.savefig(os.path.join(environment.figdir, 'e_asym_{0}.png'.format(B)), dpi=300, bbox_inches='tight', pad_inches=0.1)
+f.savefig(os.path.join(environment.figdir, 'e_asym_{0}.svg'.format(B)), dpi=300, bbox_inches='tight', pad_inches=0.1)
 plt.close()
 
 # %% 3) energy asymmetry hyperplane
@@ -153,7 +161,7 @@ plot_data = (plot_data - plot_data.mean(axis=0)) / plot_data.std(axis=0)
 # fit hyperplane
 X, Y, Z, c, r2, mse, rmse = fit_hyperplane(plot_data)
 
-f = plt.figure(figsize=(2.5, 2.5))
+f = plt.figure(figsize=(figsize, figsize))
 ax = Axes3D(f)
 cmap = ListedColormap(sns.color_palette("coolwarm", 256, ).as_hex())
 sc = ax.scatter(plot_data[:, 0], plot_data[:, 1], plot_data[:, 2], marker='o', alpha=1, s=10, c=plot_data[:, 2],
@@ -163,17 +171,17 @@ ax.tick_params(pad=-2.5)
 ax.set_xticklabels('')
 ax.set_yticklabels('')
 ax.set_zticklabels('')
-ax.set_xlabel('Initial states (i) \n slope: {:.2f}'.format(c[0]), labelpad=-10)
-ax.set_ylabel('Target states (j) \n slope: {:.2f}'.format(c[1]), labelpad=-10)
-ax.set_zlabel('Energy asymmetry')
+ax.set_xlabel('initial states \n slope: {:.2f}'.format(c[0]), labelpad=-10)
+ax.set_ylabel('target states \n slope: {:.2f}'.format(c[1]), labelpad=-10)
+ax.set_zlabel('energy asymmetry')
 ax.view_init(20, 45)
 # plt.legend(*sc.legend_elements(), bbox_to_anchor=(1.05, 1), loc=2)
 textstr = 'r2 = {:.0f}%'.format(r2*100)
 ax.text2D(0.65, 0.75, textstr, transform=ax.transAxes,
         verticalalignment='top', rotation='horizontal')
 
-f.savefig(os.path.join(environment.figdir, 'e_asym_hyperplane_{0}.png'.format(B)), dpi=300,
-          bbox_inches='tight', pad_inches=0.1)
+f.savefig(os.path.join(environment.figdir, 'e_asym_hyperplane_{0}.svg'.format(B)), dpi=300,
+          bbox_inches='tight', pad_inches=0.2)
 plt.close()
 
 # %% 4) energy asymmetry hyperplane null
@@ -183,9 +191,9 @@ print(observed)
 print(p_vals)
 
 i=0
-f, ax = plt.subplots(1, 1, figsize=(2.5, 2.5))
+f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
 my_nullplot(observed=observed[i], null=asymm_nulls[:, i], p_val=p_vals[i], xlabel='r2 (null network)', ax=ax)
-f.savefig(os.path.join(environment.figdir, 'e_asym_hyperplane_network_null_{0}.png'.format(B)), dpi=300,
+f.savefig(os.path.join(environment.figdir, 'e_asym_hyperplane_network_null_{0}.svg'.format(B)), dpi=300,
           bbox_inches='tight', pad_inches=0.1)
 plt.close()
 
@@ -204,11 +212,11 @@ ecd = ec.transpose() - ec
 ed_norm = rank_int(ed)
 
 # energy asymmetry vs effective connectivity asymmetry
-f, ax = plt.subplots(1, 1, figsize=(2.5, 2.5))
+f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
 my_regplot(x=ed_norm[indices_lower], y=ecd[indices_lower],
-           xlabel='Energy (asymmetry)', ylabel='Effective connectivity (asymmetry)', ax=ax)
+           xlabel='energy (asymmetry)', ylabel='ec (asymmetry)', ax=ax)
 plt.subplots_adjust(wspace=.25)
-f.savefig(os.path.join(environment.figdir, 'corr(e_asym_{0},ec_asym).png'.format(B)), dpi=300, bbox_inches='tight',
+f.savefig(os.path.join(environment.figdir, 'corr(e_asym_{0},ec_asym).svg'.format(B)), dpi=300, bbox_inches='tight',
           pad_inches=0.1)
 plt.close()
 
@@ -230,7 +238,7 @@ if B != 'wb':
                                darkness=.5, cmap=cmap, colorbar=False)
     f.set_figwidth(figwidth)
     f.set_figheight(figheight)
-    f.savefig(os.path.join(environment.figdir, '{0}_lat.png'.format(B)), dpi=300, bbox_inches='tight',
+    f.savefig(os.path.join(environment.figdir, '{0}_lat.svg'.format(B)), dpi=300, bbox_inches='tight',
               pad_inches=0)
     plt.close()
 
@@ -240,7 +248,7 @@ if B != 'wb':
                                darkness=.5, cmap=cmap, colorbar=False)
     f.set_figwidth(figwidth)
     f.set_figheight(figheight)
-    f.savefig(os.path.join(environment.figdir, '{0}_med.png'.format(B)), dpi=300, bbox_inches='tight',
+    f.savefig(os.path.join(environment.figdir, '{0}_med.svg'.format(B)), dpi=300, bbox_inches='tight',
               pad_inches=0)
     plt.close()
 
@@ -249,25 +257,25 @@ if B != 'wb':
     # load_average_bms.brain_maps[B].mean_within_states(compute_gradients.grad_bins) # across regions within target states
     # load_average_bms.brain_maps[B].data_mean = load_average_bms.brain_maps[B].data_mean.transpose() # across regions within initial states
 
-    f, ax = plt.subplots(1, 1, figsize=(2.5, 2.5))
+    f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
     my_regplot(load_average_bms.brain_maps[B].data_mean[indices], E[B][indices],
                '{0} (averaged within state pairs)'.format(B.upper()), '{0}-weighted energy'.format(B.upper()), ax)
-    f.savefig(os.path.join(environment.figdir, 'corr({0},energy_{0}).png'.format(B)), dpi=300, bbox_inches='tight',
+    f.savefig(os.path.join(environment.figdir, 'corr({0},energy_{0}).svg'.format(B)), dpi=300, bbox_inches='tight',
               pad_inches=0.1)
     plt.close()
 
     # %% 8) spin test null
     asymm_null, observed, p_val = helper_null_mean(e, e_spin_null, indices_lower)
-    f, ax = plt.subplots(1, 1, figsize=(2.5, 2.5))
+    f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
     my_nullplot(observed=observed, null=asymm_null, p_val=p_val, xlabel='Mean energy asymmetry (spin-test null)', ax=ax)
-    f.savefig(os.path.join(environment.figdir, 'e_asym_mean_spin_null_{0}.png'.format(B)), dpi=150,
+    f.savefig(os.path.join(environment.figdir, 'e_asym_mean_spin_null_{0}.svg'.format(B)), dpi=150,
               bbox_inches='tight', pad_inches=0.1)
     plt.close()
 
     # %% 9) null network model
     asymm_null, observed, p_val = helper_null_mean(e, e_network_null, indices_lower)
-    f, ax = plt.subplots(1, 1, figsize=(2.5, 2.5))
+    f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
     my_nullplot(observed=observed, null=asymm_null, p_val=p_val, xlabel='Mean energy asymmetry (null network)', ax=ax)
-    f.savefig(os.path.join(environment.figdir, 'e_asym_mean_network_null_{0}.png'.format(B)), dpi=150,
+    f.savefig(os.path.join(environment.figdir, 'e_asym_mean_network_null_{0}.svg'.format(B)), dpi=150,
               bbox_inches='tight', pad_inches=0.1)
     plt.close()
