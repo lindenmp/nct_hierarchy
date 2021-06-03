2202ef358ecf0754a183897d4f39e9943cb23320
Linden Parkes <lindenparkes@gmail.com>
2021-06-03 11:56:56 -0400

    added other directories that need syncing for this project

--- shell_scripts/copy_cbica.sh
+++ shell_scripts/copy_cbica.sh
@@ -3,8 +3,11 @@ rsync -avzh parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_proj
 /Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/
 
 # rsync data to cbica
-rsync -avzh /Volumes/T7/research_data/PNC/processedData \
-parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_data/PNC/
+rsync -avzh /Volumes/T7/research_data/pnc/processedData \
+parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_data/pnc/
 
-# scp -r parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_projects/pfactor_gradients/output_cluster \
-# /Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/
+rsync -avzh /Volumes/T7/research_data/parcellations \
+parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_data/
+
+rsync -avzh /Volumes/T7/research_data/fukutomi_2018ni_brain_maps \
+parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_data/
