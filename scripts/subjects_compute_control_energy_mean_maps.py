67fdb7c05fe78a6c7709c2fb02afd5a508b6f620
Linden Parkes <lindenparkes@gmail.com>
2021-06-04 07:57:10 -0400

    init commitx

---
+++ scripts/subjects_compute_control_energy_mean_maps.py
@@ -0,0 +1,75 @@
+# %%
+import sys, os, platform
+if platform.system() == 'Linux':
+    sys.path.extend(['/cbica/home/parkesl/research_projects/pfactor_gradients'])
+from pfactor_gradients.pnc import Environment, Subject
+from pfactor_gradients.routines import LoadSC, LoadCT, LoadRLFP, LoadCBF, LoadREHO, LoadALFF
+from pfactor_gradients.pipelines import ComputeGradients, ComputeMinimumControlEnergy
+from pfactor_gradients.imaging_derivs import DataVector
+
+# %% Setup project environment
+if platform.system() == 'Linux':
+    computer = 'cbica'
+    sge_task_id = int(os.getenv("SGE_TASK_ID")) - 1
+elif platform.system() == 'Darwin':
+    computer = 'macbook'
+    sge_task_id = 0
+print(sge_task_id)
+
+parc = 'schaefer'
+n_parcels = 400
+sc_edge_weight = 'streamlineCount'
+environment = Environment(computer=computer, parc=parc, n_parcels=n_parcels, sc_edge_weight=sc_edge_weight)
+environment.make_output_dirs()
+environment.load_parc_data()
+
+# %% get clustered gradients
+filters = {'healthExcludev2': 0, 't1Exclude': 0,
+           'b0ProtocolValidationStatus': 1, 'dti64ProtocolValidationStatus': 1, 'dti64Exclude': 0,
+           'psychoactiveMedPsychv2': 0, 'restProtocolValidationStatus': 1, 'restExclude': 0}
+environment.load_metadata(filters)
+compute_gradients = ComputeGradients(environment=environment, Subject=Subject)
+compute_gradients.run()
+
+# %% Load sc data
+load_sc = LoadSC(environment=environment, Subject=Subject)
+load_sc.run()
+# get subject A matrix out
+A = load_sc.A[:, :, sge_task_id].copy()
+print(load_sc.df.index[sge_task_id])
+
+# refilter environment due to LoadSC excluding on disconnected nodes
+environment.df = load_sc.df.copy()
+# retain ith subject
+environment.df = environment.df.iloc[sge_task_id, :].to_frame().transpose()
+print(environment.df.index[0])
+
+# %% load mean brain maps
+loaders_dict = {
+    'ct': LoadCT(environment=environment, Subject=Subject),
+    'cbf': LoadCBF(environment=environment, Subject=Subject)
+}
+
+for key in loaders_dict:
+    loaders_dict[key].run()
+
+# %% compute minimum energy
+file_prefix = '{0}_'.format(environment.df.index[0])
+n_subsamples = 0
+
+nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A,
+                                           states=compute_gradients.grad_bins, n_subsamples=n_subsamples,
+                                           control='minimum_fast', T=1, B='wb', file_prefix=file_prefix,
+                                           force_rerun=False, save_outputs=True, verbose=True)
+nct_pipeline.run()
+
+for key in loaders_dict:
+    bm = DataVector(data=loaders_dict[key].values[0, :].copy(), name=key)
+    bm.rankdata()
+    bm.rescale_unit_interval()
+
+    nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A,
+                                               states=compute_gradients.grad_bins, n_subsamples=n_subsamples,
+                                               control='minimum_fast', T=1, B=bm, file_prefix=file_prefix,
+                                               force_rerun=False, save_outputs=True, verbose=True)
+    nct_pipeline.run()
