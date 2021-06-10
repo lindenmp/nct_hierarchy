b3cc6cde6193f6c03dcfb393f822b083eb34091a
Linden Parkes <lindenparkes@gmail.com>
2021-06-10 11:51:49 -0400

    init commit

---
+++ shell_scripts/copy_cfn.sh
@@ -0,0 +1,3 @@
+# copy freesurfer53 from CFN onto samsung drive
+rsync -avzh lindenmp@chead.uphs.upenn.edu:/data/jag/bassett-lab/lindenmp/pnc/processedData/structural/freesurfer53 \
+/Volumes/T7/research_data/pnc/processedData/structural/
