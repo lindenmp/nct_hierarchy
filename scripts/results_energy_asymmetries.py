bb320fb4c7d9ef74b843d707321559a22c68333e
Linden Parkes <lindenparkes@gmail.com>
2021-06-11 14:19:17 -0400

    minor edits

--- scripts/results_energy_asymmetries.py
+++ scripts/results_energy_asymmetries.py
@@ -2,9 +2,8 @@ import sys, os, platform
 from pfactor_gradients.pnc import Environment, Subject
 from pfactor_gradients.routines import LoadSC, LoadCT, LoadCBF, LoadAverageSC, LoadAverageBrainMaps
 from pfactor_gradients.pipelines import ComputeGradients, ComputeMinimumControlEnergy
-from pfactor_gradients.utils import rank_int, get_fdr_p, fit_hyperplane, helper_null_hyperplane, helper_null_mean,\
-    get_null_p
-from pfactor_gradients.plotting import my_reg_plot, my_null_plot, my_distpair_plot, roi_to_vtx
+from pfactor_gradients.utils import rank_int, get_fdr_p, fit_hyperplane, helper_null_hyperplane, helper_null_mean
+from pfactor_gradients.plotting import my_reg_plot, my_null_plot, my_distpair_plot
 from pfactor_gradients.imaging_derivs import DataVector
 from pfactor_gradients.hcp import BrainMapLoader
 import numpy as np
@@ -16,7 +15,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.colors import ListedColormap
-from nilearn import plotting
 from pfactor_gradients.plotting import set_plotting_params
 set_plotting_params(format='png')
 figsize = 1.5
@@ -99,28 +97,31 @@ for B in B_list:
 
 # %% data for plotting
 # for B in ['wb', 'ct', 'cbf']:
-B = 'ct'
+B = 'wb'
 print(B)
 e = rank_int(E[B]) # normalized energy matrix
 ed = e.transpose() - e # energy asymmetry matrix
 # save out mean ed for use in other scripts
 np.save(os.path.join(environment.pipelinedir, 'ed_{0}.npy'.format(B)), ed)
 
-network_null = 'wwp'
-file = 'average_adj_n-{0}_s-{1}_null-mni-{2}_ns-{3}-0_c-minimum_fast_T-1_B-{4}_E.npy'.format(n_subs, spars_thresh,
-                                                                                             network_null, n_states, B)
-e_network_null = np.load(os.path.join(environment.pipelinedir, 'minimum_control_energy', file))
-# normalize nulls
-for i in np.arange(e_network_null.shape[2]):
-    e_network_null[:, :, i] = rank_int(e_network_null[:, :, i])
-
-if B != 'wb':
-    file = 'average_adj_n-{0}_s-{1}_ns-{2}-0_c-minimum_fast_T-1_B-{3}-spin_E.npy'.format(n_subs, spars_thresh, n_states, B)
-    e_spin_null = np.load(os.path.join(environment.pipelinedir, 'minimum_control_energy', file))
-
+try:
+    network_null = 'mni-wsp'
+    file = 'average_adj_n-{0}_s-{1}_null-{2}_ns-{3}-0_c-minimum_fast_T-1_B-{4}_E.npy'.format(n_subs, spars_thresh,
+                                                                                                 network_null, n_states, B)
+    e_network_null = np.load(os.path.join(environment.pipelinedir, 'minimum_control_energy', file))
     # normalize nulls
-    for i in np.arange(e_spin_null.shape[2]):
-        e_spin_null[:, :, i] = rank_int(e_spin_null[:, :, i])
+    for i in np.arange(e_network_null.shape[2]):
+        e_network_null[:, :, i] = rank_int(e_network_null[:, :, i])
+
+    if B != 'wb':
+        file = 'average_adj_n-{0}_s-{1}_ns-{2}-0_c-minimum_fast_T-1_B-{3}-spin_E.npy'.format(n_subs, spars_thresh, n_states, B)
+        e_spin_null = np.load(os.path.join(environment.pipelinedir, 'minimum_control_energy', file))
+
+        # normalize nulls
+        for i in np.arange(e_spin_null.shape[2]):
+            e_spin_null[:, :, i] = rank_int(e_spin_null[:, :, i])
+except FileNotFoundError:
+    print('Requisite files not found...')
 
 # %% 1) energy dists: top-down vs bottom-up
 f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
@@ -134,10 +135,12 @@ plt.close()
 # 2) energy asymmetry matrix
 plot_mask = np.zeros((n_states, n_states))
 plot_mask[indices_upper] = 1
+plot_mask[np.eye(n_states) == 1] = 1
 plot_mask = plot_mask.astype(bool)
 
 f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
-sns.heatmap(ed, mask=plot_mask, center=0, square=True, cmap='coolwarm', ax=ax, cbar_kws={"shrink": 0.80})
+sns.heatmap(ed, mask=plot_mask, center=0, vmin=np.floor(np.min(ed)), vmax=np.ceil(np.max(ed)),
+            square=True, cmap='vlag', ax=ax, cbar_kws={"shrink": 0.80})
 ax.set_ylabel("initial states", labelpad=-1)
 ax.set_xlabel("target states", labelpad=-1)
 ax.set_yticklabels('')
@@ -178,44 +181,47 @@ f.savefig(os.path.join(environment.figdir, 'e_asym_hyperplane_{0}'.format(B)), d
 plt.close()
 
 # %% 4) energy asymmetry hyperplane null
-asymm_nulls, observed, p_vals = helper_null_hyperplane(e, e_network_null, indices_lower)
-print(np.mean(asymm_nulls, axis=0))
-print(observed)
-print(p_vals)
+try:
+    asymm_nulls, observed, p_vals = helper_null_hyperplane(e, e_network_null, indices_lower)
+    print(np.mean(asymm_nulls, axis=0))
+    print(observed)
+    print(p_vals)
 
-i=0
-f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
-my_null_plot(observed=observed[i], null=asymm_nulls[:, i], p_val=p_vals[i], xlabel='$R^2$\n(null network)', ax=ax)
-f.savefig(os.path.join(environment.figdir, 'e_asym_hyperplane_network_null_{0}'.format(B)), dpi=300,
-          bbox_inches='tight', pad_inches=0.01)
-plt.close()
+    i=0
+    f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
+    my_null_plot(observed=observed[i], null=asymm_nulls[:, i], p_val=p_vals[i], xlabel='$R^2$\n(null network)', ax=ax)
+    f.savefig(os.path.join(environment.figdir, 'e_asym_hyperplane_network_null_{0}'.format(B)), dpi=300,
+              bbox_inches='tight', pad_inches=0.01)
+    plt.close()
+except NameError:
+    print('Requisite variables not found...')
 
 # %% 5) effective connectivity
 
 # load dcm outputs
-file = 'dcm_ns-{0}_A.mat'.format(n_states)
-dcm = sp.io.loadmat(os.path.join(environment.pipelinedir, 'spdcm', file))
-ec = dcm['A']
-ec = np.abs(ec)
-ec = rank_int(ec)
-ecd = ec.transpose() - ec
+try:
+    file = 'dcm_ns-{0}_A.mat'.format(n_states)
+    dcm = sp.io.loadmat(os.path.join(environment.pipelinedir, 'spdcm', file))
+    ec = dcm['A']
+    ec = np.abs(ec)
+    ec = rank_int(ec)
+    ecd = ec.transpose() - ec
 
-# energy asymmetry vs effective connectivity asymmetry
-f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
-my_reg_plot(x=ed[indices_lower], y=ecd[indices_lower],
-           xlabel='energy (asymmetry)', ylabel='ec (asymmetry)', ax=ax)
-plt.subplots_adjust(wspace=.25)
-f.savefig(os.path.join(environment.figdir, 'corr(e_asym_{0},ec_asym)'.format(B)), dpi=300, bbox_inches='tight',
-          pad_inches=0.01)
-plt.close()
+    # energy asymmetry vs effective connectivity asymmetry
+    f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
+    my_reg_plot(x=ed[indices_lower], y=ecd[indices_lower],
+               xlabel='energy (asymmetry)', ylabel='ec (asymmetry)', ax=ax)
+    plt.subplots_adjust(wspace=.25)
+    f.savefig(os.path.join(environment.figdir, 'corr(e_asym_{0},ec_asym)'.format(B)), dpi=300, bbox_inches='tight',
+              pad_inches=0.01)
+    plt.close()
+except FileNotFoundError:
+    print('Requisite files not found...')
 
 # %% note: the following section only runs for weighted control
 if B != 'wb':
     # %% 6) correlations between control set weights and energy
     load_average_bms.brain_maps[B].mean_between_states(compute_gradients.grad_bins) # across all regions for state pairs
-    # load_average_bms.brain_maps[B].mean_within_states(compute_gradients.grad_bins) # across regions within target states
-    # load_average_bms.brain_maps[B].data_mean = load_average_bms.brain_maps[B].data_mean.transpose() # across regions within initial states
-
     f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
     my_reg_plot(load_average_bms.brain_maps[B].data_mean[indices], E[B][indices],
                '{0}\n (averaged within state pairs)'.format(B.upper()), '{0}-weighted energy'.format(B.upper()), ax)
@@ -223,18 +229,40 @@ if B != 'wb':
               pad_inches=0.01)
     plt.close()
 
-    # %% 7) spin test null
-    asymm_null, observed, p_val = helper_null_mean(e, e_spin_null, indices_lower)
-    f, ax = plt.subplots(1, 1, figsize=(figsize, 0.75))
-    my_null_plot(observed=observed, null=asymm_null, p_val=p_val, xlabel='mean asym. (spin-test)', ax=ax)
-    f.savefig(os.path.join(environment.figdir, 'e_asym_mean_spin_null_{0}'.format(B)), dpi=300,
-              bbox_inches='tight', pad_inches=0.01)
+    load_average_bms.brain_maps[B].mean_within_states(compute_gradients.grad_bins) # across regions within target states
+    f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
+    my_reg_plot(load_average_bms.brain_maps[B].data_mean[indices], E[B][indices],
+               '{0}\n (averaged within target states)'.format(B.upper()), '{0}-weighted energy'.format(B.upper()), ax)
+    f.savefig(os.path.join(environment.figdir, 'corr({0}_target,energy_{0})'.format(B)), dpi=300, bbox_inches='tight',
+              pad_inches=0.01)
     plt.close()
 
-    # %% 8) null network model
-    asymm_null, observed, p_val = helper_null_mean(e, e_network_null, indices_lower)
-    f, ax = plt.subplots(1, 1, figsize=(figsize, 0.75))
-    my_null_plot(observed=observed, null=asymm_null, p_val=p_val, xlabel='mean asym. (null network)', ax=ax)
-    f.savefig(os.path.join(environment.figdir, 'e_asym_mean_network_null_{0}'.format(B)), dpi=300,
-              bbox_inches='tight', pad_inches=0.01)
+    load_average_bms.brain_maps[B].data_mean = load_average_bms.brain_maps[B].data_mean.transpose() # across regions within initial states
+    f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
+    my_reg_plot(load_average_bms.brain_maps[B].data_mean[indices], E[B][indices],
+               '{0}\n (averaged within initial states)'.format(B.upper()), '{0}-weighted energy'.format(B.upper()), ax)
+    f.savefig(os.path.join(environment.figdir, 'corr({0}_initial,energy_{0})'.format(B)), dpi=300, bbox_inches='tight',
+              pad_inches=0.01)
     plt.close()
+
+    # %% 7) spin test null
+    try:
+        asymm_null, observed, p_val = helper_null_mean(e, e_spin_null, indices_lower)
+        f, ax = plt.subplots(1, 1, figsize=(figsize, 0.75))
+        my_null_plot(observed=observed, null=asymm_null, p_val=p_val, xlabel='mean asym. (spin-test)', ax=ax)
+        f.savefig(os.path.join(environment.figdir, 'e_asym_mean_spin_null_{0}'.format(B)), dpi=300,
+                  bbox_inches='tight', pad_inches=0.01)
+        plt.close()
+    except NameError:
+        print('Requisite variables not found...')
+
+    # %% 8) null network model
+    try:
+        asymm_null, observed, p_val = helper_null_mean(e, e_network_null, indices_lower)
+        f, ax = plt.subplots(1, 1, figsize=(figsize, 0.75))
+        my_null_plot(observed=observed, null=asymm_null, p_val=p_val, xlabel='mean asym. (null network)', ax=ax)
+        f.savefig(os.path.join(environment.figdir, 'e_asym_mean_network_null_{0}'.format(B)), dpi=300,
+                  bbox_inches='tight', pad_inches=0.01)
+        plt.close()
+    except NameError:
+        print('Requisite variables not found...')
