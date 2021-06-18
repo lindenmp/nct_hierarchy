059c56006ca90d8bc0af5ba3a82a6987729701d0
Linden Parkes <lindenparkes@gmail.com>
2021-06-18 17:11:46 -0400

    refactor for robustness

--- scripts/results_prediction.py
+++ scripts/results_prediction.py
@@ -22,107 +22,6 @@ environment = Environment(computer=computer, parc=parc, n_parcels=n_parcels, sc_
 environment.make_output_dirs()
 # environment.load_parc_data()
 
-# %% load prediction results
-y_name = 'F1_Exec_Comp_Res_Accuracy' # 'F1_Exec_Comp_Res_Accuracy' 'F1_Complex_Reasoning_Efficiency' 'F3_Executive_Efficiency'
-y_label = 'Cognition'
-c_name = 'asvm'
-alg = 'rr'
-score = 'rmse'
-runpca = '80%'
-
-# load unweighted energy
-df_pred = pd.DataFrame(columns=['score', 'B', 'energy'])
-file_prefix = '{0}-{1}-{2}_alg-{3}_score-{4}_pca-{5}_'.format('energy-wb', y_name, c_name, alg, score, runpca)
-accuracy_mean = np.loadtxt(os.path.join(environment.pipelinedir, 'prediction',
-                                        '{0}accuracy_mean.txt'.format(file_prefix)))
-df_tmp = pd.DataFrame(columns=['score', 'B', 'energy'])
-df_tmp['score'] = accuracy_mean
-df_tmp['B'] = 'wb'
-df_tmp['energy'] = 'yes'
-df_pred = pd.concat((df_pred, df_tmp), axis=0)
-
-# load unweighted energy null
-df_pred_null = pd.DataFrame(columns=['score', 'B', 'energy'])
-accuracy_perm = np.loadtxt(os.path.join(environment.pipelinedir, 'prediction', '{0}accuracy_perm.txt'.format(file_prefix)))
-df_tmp = pd.DataFrame(columns=['score', 'B', 'energy'])
-df_tmp['score'] = accuracy_perm
-df_tmp['B'] = 'wb'
-df_tmp['energy'] = 'yes'
-df_pred_null = pd.concat((df_pred_null, df_tmp), axis=0)
-
-# load prediction from brain maps and weighted energgy
-B_list = ['ct', 'cbf', 'energy-ct', 'energy-cbf']
-# B_list = ['ct', 'cbf', 'energy-ct-u', 'energy-cbf-u']
-# B_list = ['ct', 'cbf', 'energy-ct-l', 'energy-cbf-l']
-n_B = len(B_list)
-p_vals = np.zeros(n_B)
-
-for i in np.arange(n_B):
-    # observed prediction performance
-    file_prefix = '{0}-{1}-{2}_alg-{3}_score-{4}_pca-{5}_'.format(B_list[i], y_name, c_name, alg, score, runpca)
-    accuracy_mean = np.loadtxt(os.path.join(environment.pipelinedir, 'prediction', '{0}accuracy_mean.txt'.format(file_prefix)))
-    df_tmp = pd.DataFrame(columns=['score', 'B', 'energy'])
-    df_tmp['score'] = accuracy_mean
-    if len(B_list[i].split('-')) >= 2:
-        df_tmp['B'] = B_list[i].split('-')[1]
-        df_tmp['energy'] = 'yes'
-    elif len(B_list[i].split('-')) == 1:
-        df_tmp['B'] = B_list[i].split('-')[0]
-        df_tmp['energy'] = 'no'
-    df_pred = pd.concat((df_pred, df_tmp), axis=0)
-
-    # null prediction performance
-    accuracy_perm = np.loadtxt(os.path.join(environment.pipelinedir, 'prediction', '{0}accuracy_perm.txt'.format(file_prefix)))
-    df_tmp = pd.DataFrame(columns=['score', 'B', 'energy'])
-    df_tmp['score'] = accuracy_perm
-    if len(B_list[i].split('-')) == 2:
-        df_tmp['B'] = B_list[i].split('-')[1]
-        df_tmp['energy'] = 'yes'
-    elif len(B_list[i].split('-')) == 1:
-        df_tmp['B'] = B_list[i].split('-')[0]
-        df_tmp['energy'] = 'no'
-    df_pred_null = pd.concat((df_pred_null, df_tmp), axis=0)
-
-# %% compute and correct p-values for permutation test
-my_list = [['wb', 'yes', 0], ['ct', 'yes', 1], ['ct', 'no', 1], ['cbf', 'yes', 2], ['cbf', 'no', 2]]
-p_vals_perm = np.zeros(len(my_list))
-for i in np.arange(len(my_list)):
-    B = my_list[i][0]
-    energy = my_list[i][1]
-    x = np.mean(df_pred.loc[np.logical_and(df_pred['B'] == B, df_pred['energy'] == energy), 'score'])
-    null = df_pred_null.loc[np.logical_and(df_pred_null['B'] == B, df_pred_null['energy'] == energy), 'score']
-    p_vals_perm[i] = get_null_p(x, null)
-
-p_vals_perm = get_fdr_p(p_vals_perm)
-print(p_vals_perm < 0.05)
-
-# %% compute and correct p-values for pair wise comparisons
-p_vals_dist = []
-
-# 1) unweighted energy vs weight energy
-x = df_pred.loc[np.logical_and(df_pred['B'] == 'wb', df_pred['energy'] == 'yes'), 'score']
-y = df_pred.loc[np.logical_and(df_pred['B'] == 'ct', df_pred['energy'] == 'yes'), 'score']
-# p_vals_dist.append(get_exact_p(x, y))
-p_vals_dist.append(sp.stats.wilcoxon(x, y)[1])
-y = df_pred.loc[np.logical_and(df_pred['B'] == 'cbf', df_pred['energy'] == 'yes'), 'score']
-# p_vals_dist.append(get_exact_p(x, y))
-p_vals_dist.append(sp.stats.wilcoxon(x, y)[1])
-
-# 2) weighted energy vs just neuro properties
-x = df_pred.loc[np.logical_and(df_pred['B'] == 'ct', df_pred['energy'] == 'yes'), 'score']
-y = df_pred.loc[np.logical_and(df_pred['B'] == 'ct', df_pred['energy'] == 'no'), 'score']
-# p_vals_dist.append(get_exact_p(x, y))
-p_vals_dist.append(sp.stats.wilcoxon(x, y)[1])
-
-x = df_pred.loc[np.logical_and(df_pred['B'] == 'cbf', df_pred['energy'] == 'yes'), 'score']
-y = df_pred.loc[np.logical_and(df_pred['B'] == 'cbf', df_pred['energy'] == 'no'), 'score']
-# p_vals_dist.append(get_exact_p(x, y))
-p_vals_dist.append(sp.stats.wilcoxon(x, y)[1])
-
-p_vals_dist = np.array(p_vals_dist)
-p_vals_dist = get_fdr_p(p_vals_dist)
-print(p_vals_dist < 0.05)
-
 # %% help funcs
 def helper_func(x_pos, y_pos, p_val, energy):
     if p_val < 0.05:
@@ -143,7 +42,115 @@ def helper_func(x_pos, y_pos, p_val, energy):
     ax.text(x_pos + offset, y_pos, textstr, rotation=rotation, fontweight=fontweight, fontsize=8,
             horizontalalignment='center', verticalalignment='center')
 
-# plot
+
+def load_data(B, file_prefix, environment):
+    # laod observed
+    accuracy_mean = np.loadtxt(os.path.join(environment.pipelinedir, 'prediction',
+                                            '{0}accuracy_mean.txt'.format(file_prefix)))
+    df_pred = pd.DataFrame(columns=['score', 'B', 'energy'])
+    df_pred['score'] = accuracy_mean
+    if len(B.split('-')) >= 2:
+        if 'flip' in B.split('-')[1]:
+            df_pred['B'] = B.split('-')[1].split('_')[0]
+        else:
+            df_pred['B'] = B.split('-')[1]
+        df_pred['energy'] = 'yes'
+    elif len(B.split('-')) == 1:
+        df_pred['B'] = B.split('-')[0]
+        df_pred['energy'] = 'no'
+    df_pred['B'] = B
+
+    # load null
+    accuracy_perm = np.loadtxt(os.path.join(environment.pipelinedir, 'prediction',
+                                            '{0}accuracy_perm.txt'.format(file_prefix)))
+    df_null = pd.DataFrame(columns=['score', 'B', 'energy'])
+    df_null['score'] = accuracy_perm
+    if len(B.split('-')) >= 2:
+        if 'flip' in B.split('-')[1]:
+            df_null['B'] = B.split('-')[1].split('_')[0]
+        else:
+            df_null['B'] = B.split('-')[1]
+        df_null['energy'] = 'yes'
+    elif len(B.split('-')) == 1:
+        df_null['B'] = B.split('-')[0]
+        df_null['energy'] = 'no'
+    df_null['B'] = B
+
+    return df_pred, df_null
+
+# %% load prediction results
+y_name = 'F1_Exec_Comp_Res_Accuracy' # 'F1_Exec_Comp_Res_Accuracy' 'F1_Complex_Reasoning_Efficiency' 'F3_Executive_Efficiency'
+y_label = 'Cognition'
+c_name = 'asvm'
+alg = 'rr'
+score = 'rmse'
+runpca = '80%'
+
+B_list = ['energy-wb', 'ct', 'sa', 'energy-ct', 'energy-sa']
+# B_list = ['energy-wb', 'ct', 'sa', 'energy-ct_flip', 'energy-sa_flip']
+# B_list = ['energy-wb', 'ct', 'sa', 'energy-ct', 'energy-sa', 'energy-ct_flip', 'energy-sa_flip']
+
+my_list = [0, 1, 2, 1, 2]
+# my_list = [0, 1, 2, 1, 2, 1, 2]
+
+df_pred = pd.DataFrame(columns=['score', 'B', 'energy'])
+df_null = pd.DataFrame(columns=['score', 'B', 'energy'])
+
+for B in B_list:
+    # observed prediction performance
+    file_prefix = 'histg2_{0}-{1}-{2}_alg-{3}_score-{4}_pca-{5}_'.format(B, y_name, c_name, alg, score, runpca)
+    df_pred_tmp, df_null_tmp = load_data(B, file_prefix, environment)
+    df_pred = pd.concat((df_pred, df_pred_tmp), axis=0)
+    df_null = pd.concat((df_null, df_null_tmp), axis=0)
+
+print(df_pred)
+
+# %% compute and correct p-values for permutation test
+p_vals_perm = np.zeros(len(B_list))
+for i, B in enumerate(B_list):
+    x = np.mean(df_pred.loc[df_pred['B'] == B, 'score'])
+    null = df_null.loc[df_null['B'] == B, 'score']
+    p_vals_perm[i] = get_null_p(x, null)
+
+p_vals_perm = get_fdr_p(p_vals_perm)
+print(p_vals_perm < 0.05)
+
+# %% compute and correct p-values for pair wise comparisons
+p_vals_dist = []
+
+# 1) unweighted energy vs weight energy
+x = df_pred.loc[df_pred['B'] == B_list[0], 'score']
+y = df_pred.loc[df_pred['B'] == B_list[3], 'score']
+# p_vals_dist.append(get_exact_p(x, y))
+p_vals_dist.append(sp.stats.wilcoxon(x, y)[1])
+y = df_pred.loc[df_pred['B'] == B_list[4], 'score']
+# p_vals_dist.append(get_exact_p(x, y))
+p_vals_dist.append(sp.stats.wilcoxon(x, y)[1])
+
+# 2) weighted energy vs just neuro properties
+x = df_pred.loc[df_pred['B'] == B_list[3], 'score']
+y = df_pred.loc[df_pred['B'] == B_list[1], 'score']
+# p_vals_dist.append(get_exact_p(x, y))
+p_vals_dist.append(sp.stats.wilcoxon(x, y)[1])
+
+x = df_pred.loc[df_pred['B'] == B_list[4], 'score']
+y = df_pred.loc[df_pred['B'] == B_list[2], 'score']
+# p_vals_dist.append(get_exact_p(x, y))
+p_vals_dist.append(sp.stats.wilcoxon(x, y)[1])
+
+p_vals_dist = np.array(p_vals_dist)
+p_vals_dist = get_fdr_p(p_vals_dist)
+print(p_vals_dist < 0.05)
+
+# %% plot
+df_pred['B_strip'] = df_pred['B'].map(lambda x: x.lstrip('energy-'))
+df_null['B_strip'] = df_null['B'].map(lambda x: x.lstrip('energy-'))
+# try:
+#     df_pred['B_strip'] = df_pred['B_strip'].map(lambda x: x.rstrip('_flip'))
+#     df_null['B_strip'] = df_null['B_strip'].map(lambda x: x.rstrip('_flip'))
+# except:
+#     pass
+
 # cmap = sns.color_palette("Paired", as_cmap=False)
 cmap = sns.color_palette("pastel", as_cmap=False)
 # cmap = np.array([[255, 105, 97], [97, 168, 255]]) / 255
@@ -154,13 +161,13 @@ sns.despine(left=True, bottom=True)
 ax.tick_params(pad=-2.5)
 
 # nulls (background)
-sns.violinplot(data=df_pred_null, ax=ax, x='B', y='score', hue='energy', split=True, scale='width', palette=cmap,
+sns.violinplot(data=df_null, ax=ax, x='B_strip', y='score', hue='energy', split=True, scale='width', palette=cmap,
                inner=None, linewidth=0.5)
 for violin in ax.collections:
     violin.set_alpha(0.2)
 
 # observed (foreground)
-sns.violinplot(data=df_pred, ax=ax, x='B', y='score', hue='energy', split=True, scale='width', palette=cmap,
+sns.violinplot(data=df_pred, ax=ax, x='B_strip', y='score', hue='energy', split=True, scale='width', palette=cmap,
                inner=None, linewidth=1.5)
 n_violins = len(ax.collections)
 for violin in ax.collections[int(n_violins/2):]:
@@ -170,17 +177,20 @@ handles, labels = ax.get_legend_handles_labels()
 ax.legend(handles=handles[:2], labels=['energy', 'neurobiology only'], title='', bbox_to_anchor=(1, 1.15), loc='upper right')
 ax.set_ylabel('negative {0} (higher = better)'.format(score.upper()))
 ax.set_xlabel('')
-ax.set_xticklabels(['unweighted', 'CT', 'CBF'])
-ax.set_ylim([-0.93, -.86])
+# ax.set_xticklabels(['unweighted', 'CT', 'SA'])
+if score == 'rmse':
+    ax.set_ylim([-0.975, -0.895])
 
 # add permutation p-values to plot
-for i in np.arange(len(my_list)):
-    B = my_list[i][0]
-    energy = my_list[i][1]
-    x_pos = my_list[i][2]
-    y_pos = np.mean(df_pred.loc[np.logical_and(df_pred['B'] == B, df_pred['energy'] == energy), 'score'])
-    helper_func(x_pos, y_pos, p_vals_perm[i], energy)
+for i, B in enumerate(B_list):
+    x_pos = my_list[i]
+    y_pos = np.mean(df_pred.loc[df_pred['B'] == B, 'score'])
+    if 'energy' in B:
+        helper_func(x_pos, y_pos, p_vals_perm[i], 'yes')
+    else:
+        helper_func(x_pos, y_pos, p_vals_perm[i], 'no')
 
-f.savefig(os.path.join(environment.figdir, 'prediction_{0}'.format(y_name)), dpi=300, bbox_inches='tight',
+
+f.savefig(os.path.join(environment.figdir, 'prediction_{0}'.format(y_name)), dpi=600, bbox_inches='tight',
           pad_inches=0.1)
 plt.close()
