34a5bf89cea55d7e05305f2f10c7b05cba2c015f
Linden Parkes <lindenparkes@gmail.com>
2021-07-06 10:47:13 -0400

    restructure to draw on setup_workspace_ave_adj

--- scripts/average_adj_control_energy_nulls.py
+++ scripts/average_adj_control_energy_nulls.py
@@ -1,5 +1,10 @@
-# %%
+# %% import
 import sys, os, platform
+from pfactor_gradients.pipelines import ComputeMinimumControlEnergy
+
+import scipy as sp
+from bct.algorithms.reference import randmio_und
+
 from oct2py import octave
 if platform.system() == 'Linux':
     sys.path.extend(['/cbica/home/parkesl/research_projects/pfactor_gradients'])
@@ -9,17 +14,10 @@ elif platform.system() == 'Darwin':
     sys.path.append('usr/local/bin/octave') # octave install path
     octave.addpath('/Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/geomsurr') # path to matlab functions
 
-from pfactor_gradients.pnc import Environment, Subject
-from pfactor_gradients.routines import LoadSC, LoadCT, LoadSA, LoadAverageSC, LoadAverageBrainMaps
-from pfactor_gradients.pipelines import ComputeGradients, ComputeMinimumControlEnergy
-from pfactor_gradients.imaging_derivs import DataVector
-from pfactor_gradients.utils import get_states_from_gradient
-from pfactor_gradients.hcp import BrainMapLoader
-import scipy as sp
-import numpy as np
-from bct.algorithms.reference import randmio_und
+# %% import workspace
+from setup_workspace_ave_adj import *
 
-# %% Setup project environment
+# %% plotting and task id
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style='white', context='talk', font_scale=1)
@@ -37,56 +35,7 @@ elif platform.system() == 'Darwin':
     plt.rcParams['svg.fonttype'] = 'none'
 print(sge_task_id)
 
-parc = 'schaefer'
-n_parcels = 400
-sc_edge_weight = 'streamlineCount'
-environment = Environment(computer=computer, parc=parc, n_parcels=n_parcels, sc_edge_weight=sc_edge_weight)
-environment.make_output_dirs()
-environment.load_parc_data()
-
-# filter subjects
-filters = {'healthExcludev2': 0, 'psychoactiveMedPsychv2': 0,
-           't1Exclude': 0, 'fsFinalExclude': 0,
-           'b0ProtocolValidationStatus': 1, 'dti64ProtocolValidationStatus': 1, 'dti64Exclude': 0}
-           # 'restProtocolValidationStatus': 1, 'restExclude': 0} # need to add these filters in if doing funcg1 below
-environment.load_metadata(filters)
-
-# %% get states
-which_grad = 'histg2'
-
-if which_grad == 'histg2':
-    if computer == 'macbook':
-        gradient = np.loadtxt('/Volumes/T7/research_data/BigBrainWarp/spaces/fsaverage/Hist_G2_Schaefer2018_400Parcels_17Networks.txt')
-    elif computer == 'cbica':
-        gradient = np.loadtxt('/cbica/home/parkesl/research_data/BigBrainWarp/spaces/fsaverage/Hist_G2_Schaefer2018_400Parcels_17Networks.txt')
-    gradient = gradient * -1
-elif which_grad == 'funcg1':
-    # compute function gradient
-    compute_gradients = ComputeGradients(environment=environment, Subject=Subject)
-    compute_gradients.run()
-    gradient = compute_gradients.gradients[:, 0]
-
-n_bins = int(n_parcels/10)
-states = get_states_from_gradient(gradient=gradient, n_bins=n_bins)
-n_states = len(np.unique(states))
-
-# %% Load sc data
-load_sc = LoadSC(environment=environment, Subject=Subject)
-load_sc.run()
-# refilter environment due to LoadSC excluding on disconnected nodes
-environment.df = load_sc.df.copy()
-
-if parc == 'schaefer' and n_parcels == 400:
-    spars_thresh = 0.06
-elif parc == 'schaefer' and n_parcels == 200:
-    spars_thresh = 0.12
-elif parc == 'glasser' and n_parcels == 360:
-    spars_thresh = 0.07
-load_average_sc = LoadAverageSC(load_sc=load_sc, spars_thresh=spars_thresh)
-load_average_sc.run()
-A = load_average_sc.A.copy()
-
-# rewire mean adjacency matrix with spatial constraints
+# %% rewire mean adjacency matrix with spatial constraints
 D = sp.spatial.distance.pdist(environment.centroids, 'euclidean')
 D = sp.spatial.distance.squareform(D)
 octave.eval("rand('state',%i)" % sge_task_id)
@@ -100,40 +49,22 @@ n_iter = int(n_edge_swaps / n_connections)
 np.random.seed(sge_task_id)
 R, eff = randmio_und(A, itr=n_iter)
 
-# %% load mean brain maps
-loaders_dict = {
-    'ct': LoadCT(environment=environment, Subject=Subject),
-    'sa': LoadSA(environment=environment, Subject=Subject)
-}
-
-load_average_bms = LoadAverageBrainMaps(loaders_dict=loaders_dict)
-load_average_bms.run(return_descending=False)
-
-# append hcp myelin map
-# hcp_brain_maps = BrainMapLoader(computer=computer)
-# hcp_brain_maps.load_myelin(lh_annot_file=environment.lh_annot_file, rh_annot_file=environment.rh_annot_file)
-#
-# data = DataVector(data=hcp_brain_maps.myelin, name='myelin')
-# data.rankdata()
-# data.rescale_unit_interval()
-# load_average_bms.brain_maps['myelin'] = data
-
 # %% get control energy
-file_prefix = 'average_adj_n-{0}_s-{1}_{2}_'.format(load_average_sc.load_sc.df.shape[0], spars_thresh, which_grad)
+file_prefix = 'average_adj_n-{0}_s-{1}_{2}_'.format(load_average_sc.load_sc.df.shape[0], spars_thresh, which_brain_map)
 n_subsamples = 0
 
 # %% brain map null (spin test)
-for key in load_average_bms.brain_maps:
-    load_average_bms.brain_maps[key].shuffle_data(shuffle_indices=environment.spun_indices)
-
-    permuted_bm = DataVector(data=load_average_bms.brain_maps[key].data_shuf[:, sge_task_id].copy(),
-                             name='{0}-spin-{1}'.format(key, sge_task_id))
-
-    nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A,
-                                               states=states, n_subsamples=n_subsamples,
-                                               control='minimum_fast', T=1, B=permuted_bm, file_prefix=file_prefix,
-                                               force_rerun=True, save_outputs=True, verbose=True)
-    nct_pipeline.run()
+# for key in load_average_bms.brain_maps:
+#     load_average_bms.brain_maps[key].shuffle_data(shuffle_indices=environment.spun_indices)
+#
+#     permuted_bm = DataVector(data=load_average_bms.brain_maps[key].data_shuf[:, sge_task_id].copy(),
+#                              name='{0}-spin-{1}'.format(key, sge_task_id))
+#
+#     nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A,
+#                                                states=states, n_subsamples=n_subsamples,
+#                                                control='minimum_fast', T=1, B=permuted_bm, file_prefix=file_prefix,
+#                                                force_rerun=True, save_outputs=True, verbose=True)
+#     nct_pipeline.run()
 
 # %% brain map null (random)
 # for key in load_average_bms.brain_maps:
@@ -162,13 +93,13 @@ for key in load_average_bms.brain_maps:
 # %% network null
 A_list = [Wwp, Wsp, Wssp, R] # Wssp
 file_prefixes = ['average_adj_n-{0}_s-{1}_{2}_null-mni-wwp-{3}_'.format(load_average_sc.load_sc.df.shape[0],
-                                                                        spars_thresh, which_grad, sge_task_id),
+                                                                        spars_thresh, which_brain_map, sge_task_id),
                  'average_adj_n-{0}_s-{1}_{2}_null-mni-wsp-{3}_'.format(load_average_sc.load_sc.df.shape[0],
-                                                                        spars_thresh, which_grad, sge_task_id),
+                                                                        spars_thresh, which_brain_map, sge_task_id),
                  'average_adj_n-{0}_s-{1}_{2}_null-mni-wssp-{3}_'.format(load_average_sc.load_sc.df.shape[0],
-                                                                         spars_thresh, which_grad, sge_task_id),
+                                                                         spars_thresh, which_brain_map, sge_task_id),
                  'average_adj_n-{0}_s-{1}_{2}_null-nospat-{3}_'.format(load_average_sc.load_sc.df.shape[0],
-                                                                       spars_thresh, which_grad, sge_task_id)]
+                                                                       spars_thresh, which_brain_map, sge_task_id)]
 
 for A_idx, A_entry in enumerate(A_list):
     file_prefix = file_prefixes[A_idx]
@@ -179,10 +110,10 @@ for A_idx, A_entry in enumerate(A_list):
                                                force_rerun=True, save_outputs=True, verbose=True)
     nct_pipeline.run()
 
-    for key in load_average_bms.brain_maps:
-        nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A_entry,
-                                                   states=states, n_subsamples=n_subsamples,
-                                                   control='minimum_fast', T=1, B=load_average_bms.brain_maps[key],
-                                                   file_prefix=file_prefix,
-                                                   force_rerun=True, save_outputs=True, verbose=True)
-        nct_pipeline.run()
+    # for key in load_average_bms.brain_maps:
+    #     nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A_entry,
+    #                                                states=states, n_subsamples=n_subsamples,
+    #                                                control='minimum_fast', T=1, B=load_average_bms.brain_maps[key],
+    #                                                file_prefix=file_prefix,
+    #                                                force_rerun=True, save_outputs=True, verbose=True)
+    #     nct_pipeline.run()
