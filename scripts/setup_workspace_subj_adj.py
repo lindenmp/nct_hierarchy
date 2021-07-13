cd8fc34159f031eaec30e82425ea3a2e394c8b5a
lindenmp <lindenparkes@gmail.com>
2021-07-13 13:48:52 -0400

    init commit

---
+++ scripts/setup_workspace_subj_adj.py
@@ -0,0 +1,69 @@
+# %% import
+import sys, os, platform
+if platform.system() == 'Linux':
+    sys.path.extend(['/cbica/home/parkesl/research_projects/pfactor_gradients'])
+from pfactor_gradients.pnc import Environment, Subject
+from pfactor_gradients.routines import LoadSC
+from pfactor_gradients.pipelines import ComputeGradients
+from pfactor_gradients.utils import get_states_from_brain_map
+
+import numpy as np
+
+# %% Setup project environment
+if platform.system() == 'Linux':
+    computer = 'cbica'
+elif platform.system() == 'Darwin':
+    computer = 'macbook'
+parc = 'schaefer'
+n_parcels = 400
+sc_edge_weight = 'streamlineCount'
+environment = Environment(computer=computer, parc=parc, n_parcels=n_parcels, sc_edge_weight=sc_edge_weight)
+environment.make_output_dirs()
+environment.load_parc_data()
+
+# filter subjects
+filters = {'healthExcludev2': 0, 'psychoactiveMedPsychv2': 0,
+           't1Exclude': 0, 'fsFinalExclude': 0,
+           'b0ProtocolValidationStatus': 1, 'dti64ProtocolValidationStatus': 1, 'dti64Exclude': 0,
+           'restProtocolValidationStatus': 1, 'restExclude': 0} # need to add these filters in if doing func-g1 below
+environment.load_metadata(filters)
+
+# %% Load sc data
+# note, this performs more subject exclusion
+load_sc = LoadSC(environment=environment, Subject=Subject)
+load_sc.run()
+# refilter environment due to LoadSC excluding on disconnected nodes
+environment.df = load_sc.df.copy()
+n_subs = environment.df.shape[0]
+
+# %% compute functional gradient
+compute_gradients = ComputeGradients(environment=environment, Subject=Subject)
+compute_gradients.run()
+
+# %% get states
+which_brain_map = 'hist-g2'
+
+if which_brain_map == 'hist-g2':
+    if computer == 'macbook':
+        # bbw_dir = '/Volumes/T7/research_data/BigBrainWarp/spaces/fsaverage/'
+        bbw_dir = '/Users/lindenmp/research_data/BigBrainWarp/spaces/fsaverage/'
+    elif computer == 'cbica':
+        bbw_dir = '/cbica/home/parkesl/research_data/BigBrainWarp/spaces/fsaverage/'
+
+    if parc == 'schaefer':
+        state_brain_map = np.loadtxt(os.path.join(bbw_dir, 'Hist_G2_Schaefer2018_{0}Parcels_17Networks.txt' \
+                                                  .format(n_parcels)))
+    elif parc == 'glasser':
+        state_brain_map = np.loadtxt(os.path.join(bbw_dir, 'Hist_G2_HCP-MMP1.txt'))
+    state_brain_map = state_brain_map * -1
+elif which_brain_map == 'func-g1':
+    state_brain_map = compute_gradients.gradients[:, 0].copy()
+
+n_bins = int(n_parcels/10)
+states = get_states_from_brain_map(brain_map=state_brain_map, n_bins=n_bins)
+n_states = len(np.unique(states))
+
+mask = ~np.eye(n_states, dtype=bool)
+indices = np.where(mask)
+indices_upper = np.triu_indices(n_states, k=1)
+indices_lower = np.tril_indices(n_states, k=-1)
