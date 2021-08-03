9184659c5f827b18c7617ed9ec6d3c4300620abe
lindenmp <lindenparkes@gmail.com>
2021-08-03 11:17:05 -0400

    init commit

---
+++ scripts/ave_adj_energy_bootstrap.py
@@ -0,0 +1,38 @@
+# %% import
+import sys, os, platform
+if platform.system() == 'Linux':
+    sge_task_id = int(os.getenv("SGE_TASK_ID"))-1
+    sys.path.extend(['/cbica/home/parkesl/research_projects/pfactor_gradients'])
+elif platform.system() == 'Darwin':
+    sge_task_id = 0
+print(sge_task_id)
+
+from pfactor_gradients.imaging_derivs import DataMatrix
+from pfactor_gradients.pipelines import ComputeMinimumControlEnergy
+from pfactor_gradients.utils import get_bootstrap_indices
+
+# %% import workspace
+from setup_workspace_ave_adj import *
+
+# %% bootstrap
+bootstrap_indices = get_bootstrap_indices(d_size=n_subs)
+
+file_prefix = 'average_adj_n-{0}_cthr-{1}_smap-{2}_strap-{3}_'.format(load_average_sc.load_sc.df.shape[0],
+                                                            consist_thresh, which_brain_map, sge_task_id)
+
+load_sc_strap = LoadSC(environment=environment, Subject=Subject)
+load_sc_strap.df = load_sc.df.iloc[bootstrap_indices[sge_task_id, :], :]
+load_sc_strap.A = load_sc.A[:, :, bootstrap_indices[sge_task_id, :]]
+
+load_average_sc_strap = LoadAverageSC(load_sc=load_sc_strap, consist_thresh=consist_thresh, verbose=True)
+load_average_sc_strap.run()
+
+# get bootstrapped energy
+B = DataMatrix(data=np.eye(n_parcels), name='identity')
+nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=load_average_sc_strap.A, states=states, B=B,
+                                           control='minimum_fast', T=1,
+                                           file_prefix=file_prefix,
+                                           force_rerun=False, save_outputs=True, verbose=True)
+n = 2
+ds = 0.1
+nct_pipeline.run_with_optimized_b(n=n, ds=ds)
