fd03e8bf9a1f3810b53a16f3f3ac6a0c91bc297b
Linden Parkes <lindenparkes@gmail.com>
2022-01-31 13:40:11 -0500

    init commit. Figure 4 for first full draft of manuscript

---
+++ scripts/results_fig4.py
@@ -0,0 +1,103 @@
+# %% import
+import sys, os, platform
+from pfactor_gradients.imaging_derivs import DataMatrix
+from pfactor_gradients.pipelines import ComputeMinimumControlEnergy
+from pfactor_gradients.plotting import my_reg_plot
+from pfactor_gradients.utils import rank_int
+
+# %% import workspace
+os.environ["MY_PYTHON_WORKSPACE"] = 'ave_adj'
+os.environ["WHICH_BRAIN_MAP"] = 'hist-g2'
+# os.environ["WHICH_BRAIN_MAP"] = 'func-g1'
+from setup_workspace import *
+
+# %% plotting
+import seaborn as sns
+import matplotlib.pyplot as plt
+from pfactor_gradients.plotting import set_plotting_params
+set_plotting_params(format='svg')
+figsize = 1.5
+
+# %% get control energy
+file_prefix = 'average_adj_n-{0}_cthr-{1}_smap-{2}_'.format(load_average_sc.load_sc.df.shape[0],
+                                                            consist_thresh, which_brain_map)
+
+B_dict = dict()
+B = DataMatrix(data=np.eye(n_parcels), name='identity')
+B_dict[B.name] = B
+
+c = 1
+T = 1
+E = dict.fromkeys(B_dict)
+
+for B in B_dict:
+    nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A, states=states, B=B_dict[B],
+                                               control='minimum_fast', c=c, T=T,
+                                               file_prefix=file_prefix,
+                                               force_rerun=False, save_outputs=True, verbose=True)
+    nct_pipeline.run()
+
+    E[B] = nct_pipeline.E
+
+
+# optimized B weights
+nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=A, states=states, B=B_dict['identity'],
+                                           control='minimum_fast', c=c, T=T,
+                                           file_prefix=file_prefix,
+                                           force_rerun=False, save_outputs=True, verbose=True)
+n = 2
+ds = 0.1
+nct_pipeline.run_with_optimized_b(n=n, ds=ds)
+
+E['E_opt'] = nct_pipeline.E_opt[:, 1].reshape(n_states, n_states)
+B_opt = nct_pipeline.B_opt[:, :, 1]
+
+# %% data for plotting
+norm_energy = True
+B = 'identity'
+# B = 'E_opt'
+print(B)
+e = E[B].copy()
+
+if norm_energy:
+    e = rank_int(e) # normalized energy matrix
+
+ed = e - e.transpose() # energy asymmetry matrix
+print(np.all(np.round(np.abs(ed.flatten()), 4) == np.round(np.abs(ed.transpose().flatten()), 4)))
+
+# %% 5) timescales delta
+timescales_delta = np.zeros((n_states, n_states))
+for i in np.arange(n_states):
+    for j in np.arange(n_states):
+        timescales_delta[i, j] = np.nanmean(brain_maps['tau'].data[states == i]) - \
+                                 np.nanmean(brain_maps['tau'].data[states == j])
+# sign of this timescales_delta matrix is currently unintuitive.
+#   if state_i = 0.3 and state_j = 0.5, then 0.3-0.5=-0.2.
+#   likewise, if state_i = 0.5 and state_j = 0.3, then 0.5-0.3=0.2.
+# thus, an increase in rlfp over states is encoded by a negative number and a decrease is encoded by a positive
+# number. Not good! sign flip for intuition
+timescales_delta = timescales_delta * -1
+# now, negative sign represents decreasing over states and positive sign represents  increasing over states.
+
+# matrix
+f, ax = plt.subplots(1, 1, figsize=(figsize * 1.2, figsize * 1.2))
+sns.heatmap(timescales_delta, center=0, vmin=np.floor(np.min(timescales_delta)), vmax=np.ceil(np.max(timescales_delta)),
+            square=True, cmap='coolwarm', ax=ax, cbar_kws={"shrink": 0.60})
+ax.set_ylabel("initial states", labelpad=-1)
+ax.set_xlabel("target states", labelpad=-1)
+ax.set_yticklabels('')
+ax.set_xticklabels('')
+ax.tick_params(pad=-2.5)
+f.savefig(os.path.join(environment.figdir, 'timescales_delta_matrix'), dpi=600, bbox_inches='tight', pad_inches=0.01)
+plt.close()
+
+# energy asymmetry vs timescales delta
+f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
+my_reg_plot(x=timescales_delta[indices_upper], y=ed[indices_upper],
+            xlabel='intrinsic timescales (delta)', ylabel='energy asymmetry',
+            ax=ax, annotate='both')
+ax.set_ylim([-6, 4])
+plt.subplots_adjust(wspace=.25)
+f.savefig(os.path.join(environment.figdir, 'ed_timescales_{0}'.format(B)), dpi=600,
+          bbox_inches='tight', pad_inches=0.1)
+plt.close()
