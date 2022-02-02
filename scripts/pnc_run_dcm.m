b6ff2e3fede15d2793a43b255922758dce3293d0
Linden Parkes <lindenparkes@gmail.com>
2022-02-02 17:31:58 -0500

    minor edits

--- scripts/pnc_run_dcm.m
+++ scripts/pnc_run_dcm.m
@@ -1,8 +1,9 @@
 clear all; clc
 outdir = '/Users/lindenmp/research_projects/pfactor_gradients/output_local/pnc/schaefer_200_streamlineCount/pipelines/spdcm/'
 spmdir = '/Users/lindenmp/Google-Drive-Penn/work/matlab_tools/spm12';
-rsts_file = [outdir 'rsts_states_hist-g2_ns-20.npy'];
-%rsts_file = [outdir 'rsts_states_func-g1_ns-20.npy'];
+fileprefix = 'hist-g2'
+%fileprefix = 'func-g1'
+rsts_file = [outdir 'rsts_states_', fileprefix, '_ns-20.npy'];
 rsts = readNPY(rsts_file);
 size(rsts)
 tr = 3;
@@ -11,6 +12,6 @@ te = 0.032;
 %cd('/Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/matlab_functions')
 %spdcm_firstlevel(spmdir, rsts, tr, te, outdir)
 cd('/Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/matlab_functions')
-spdcm_firstlevel_loop(spmdir, rsts, tr, te, outdir)
+spdcm_firstlevel_loop(spmdir, rsts, tr, te, outdir, fileprefix)
 
 %corr(A(eye(10) ~= 1), DCM.Ep.A(eye(10) ~= 1), 'Type', 'Pearson')
