ba02026ecaed1c01cd57bb01d7983834a651d74e
Linden Parkes <lindenparkes@gmail.com>
2022-08-29 17:16:39 -0400

    refactor for revisions

--- scripts/prepare_dcm.py
+++ scripts/prepare_dcm.py
@@ -1,17 +1,16 @@
 # %% import
 import sys, os, platform
-from pfactor_gradients.pipelines import DCM
+from src.pipelines import DCM
 
 # %% import workspace
 os.environ["MY_PYTHON_WORKSPACE"] = 'ave_adj'
 os.environ["WHICH_BRAIN_MAP"] = 'hist-g2'
-# os.environ["WHICH_BRAIN_MAP"] = 'func-g1'
 from setup_workspace import *
 
 # %% plotting
 import seaborn as sns
 import matplotlib.pyplot as plt
-from pfactor_gradients.plotting import set_plotting_params
+from src.plotting import set_plotting_params
 set_plotting_params(format='png')
 figsize = 1.5
 
@@ -28,13 +27,3 @@ ax.tick_params(pad=-2.5)
 f.savefig(os.path.join(environment.figdir, 'spdcm_ts_{0}.png'.format(which_brain_map)), dpi=500, bbox_inches='tight',
           pad_inches=0.1)
 plt.close()
-
-# %%
-# rsts = dcm.rsts_states
-# print(rsts.shape)
-
-# spmdir = '/Users/lindenmp/Google-Drive-Penn/work/matlab_tools/spm12'
-# outdir = '/Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/output_cluster/pnc/schaefer_400_streamlineCount/pipelines/spdcm'
-
-# octave.eval("rand('state',%i)" % 1)
-# octave.spdcm_firstlevel_loop(spmdir, rsts, environment.rsfmri_tr, environment.rsfmri_te, outdir)
