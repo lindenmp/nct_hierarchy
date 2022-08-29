507abe4a88e60dc10f2f07265b51e45cebe64d57
Linden Parkes <lindenparkes@gmail.com>
2022-08-29 16:38:52 -0400

    refactor for revisions

--- scripts/ave_adj_generate_energy_bootstraps.py
+++ scripts/ave_adj_generate_energy_bootstraps.py
@@ -2,30 +2,29 @@
 import sys, os, platform
 if platform.system() == 'Linux':
     sge_task_id = int(os.getenv("SGE_TASK_ID"))-1
-    sys.path.extend(['/cbica/home/parkesl/research_projects/pfactor_gradients'])
+    sys.path.extend(['/cbica/home/parkesl/research_projects/nct_hierarchy'])
 elif platform.system() == 'Darwin':
     sge_task_id = 0
 print(sge_task_id)
 
-from pfactor_gradients.imaging_derivs import DataMatrix
-from pfactor_gradients.pipelines import ComputeMinimumControlEnergy
-from pfactor_gradients.utils import get_bootstrap_indices
+from src.pipelines import ComputeMinimumControlEnergy
+from src.utils import get_bootstrap_indices
 
 # %% import workspace
 os.environ["MY_PYTHON_WORKSPACE"] = 'ave_adj'
 os.environ["WHICH_BRAIN_MAP"] = 'hist-g2'
-# os.environ["WHICH_BRAIN_MAP"] = 'func-g1'
 from setup_workspace import *
 
 # %% bootstrap
-bootstrap_indices = get_bootstrap_indices(d_size=n_subs)
+n_samples = 10000
+bootstrap_indices = get_bootstrap_indices(d_size=n_subs, frac=1, n_samples=n_samples)
 
 file_prefix = 'average_adj_n-{0}_cthr-{1}_smap-{2}_strap-{3}_'.format(load_average_sc.load_sc.df.shape[0],
-                                                            consist_thresh, which_brain_map, sge_task_id)
+                                                                      consist_thresh, which_brain_map, sge_task_id)
 
 load_sc_strap = LoadSC(environment=environment, Subject=Subject)
-load_sc_strap.df = load_sc.df.iloc[bootstrap_indices[sge_task_id, :], :]
-load_sc_strap.A = load_sc.A[:, :, bootstrap_indices[sge_task_id, :]]
+load_sc_strap.df = load_sc.df.iloc[bootstrap_indices[sge_task_id, :], :].copy()
+load_sc_strap.A = load_sc.A[:, :, bootstrap_indices[sge_task_id, :]].copy()
 
 load_average_sc_strap = LoadAverageSC(load_sc=load_sc_strap, consist_thresh=consist_thresh, verbose=True)
 load_average_sc_strap.run()
