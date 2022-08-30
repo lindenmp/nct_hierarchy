d749f3dda9327e70b292a94a8ce07ffab190cab5
Linden Parkes <lindenparkes@gmail.com>
2022-08-30 16:21:50 -0400

    refactor for revisions

--- shell_scripts/job_submitter.sh
+++ shell_scripts/job_submitter.sh
@@ -1,57 +1,18 @@
+# clean up on cbica
+rm -rf core.*
+rm -rf ./sge/*
+
 conda activate pfactor_gradients
 
 # 1) pnc subjects, array job
-qsub -N subjects -l h_vmem=3G,s_vmem=3G -pe threaded 1 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
--t 1:775 \
+# 793 (schaefer200) | 766 (schaefer400) | 792 (glasser360)
+qsub -N subjects -l h_vmem=8G,s_vmem=8G -pe threaded 2 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
+-t 1:793 \
 /cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
-/cbica/home/parkesl/research_projects/pfactor_gradients/scripts/subjects_compute_control_energy.py
-########################################################################################################################
+/cbica/home/parkesl/research_projects/nct_hierarchy/scripts/subjects_compute_control_energy.py
 
-# 2) average a matrix, null
-# run first
-qsub -N e_null -l h_vmem=3G,s_vmem=3G -pe threaded 2 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
--t 1 \
+# 2) bootstrap mean adj matrix and compute energy
+qsub -N e_bootstrap -l h_vmem=4G,s_vmem=4G -pe threaded 1 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
+-t 1:10000 \
 /cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
-/cbica/home/parkesl/research_projects/pfactor_gradients/scripts/average_adj_control_energy_nulls.py
-
-# run second when above is finished
-qsub -N e_null -l h_vmem=3G,s_vmem=3G -pe threaded 2 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
--t 2:10000 -tc 500 \
-/cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
-/cbica/home/parkesl/research_projects/pfactor_gradients/scripts/average_adj_control_energy_nulls.py
-
-# then run to collate
-qsub -N e_null_collect -l h_vmem=16G,s_vmem=16G -pe threaded 2 -j y -b y -o /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
-/cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
-/cbica/home/parkesl/research_projects/pfactor_gradients/scripts/average_adj_control_energy_nulls_loop.py
-########################################################################################################################
-
-# 3) prediction
-X_list=('wb' 'ct' 'cbf')
-#y_list=('F1_Exec_Comp_Res_Accuracy' 'F1_Complex_Reasoning_Efficiency' 'F3_Executive_Efficiency')
-y_list=('F1_Exec_Comp_Res_Accuracy')
-#c_list=('asvm' 'svm' 'vm')
-c_list=('asvm')
-alg_list=('rr')
-score_list=('r2' 'rmse' 'corr')
-#runpca_list=('1%' '80%' 50)
-runpca_list=('80%')
-
-for X_name in "${X_list[@]}"; do
-  for y_name in "${y_list[@]}"; do
-    for c_name in "${c_list[@]}"; do
-      for alg in "${alg_list[@]}"; do
-        for score in "${score_list[@]}"; do
-          for runpca in "${runpca_list[@]}"; do
-            qsub -N prediction -l h_vmem=3G,s_vmem=3G -pe threaded 4 -j y -b y -o \
-            /cbica/home/parkesl/sge/ -e /cbica/home/parkesl/sge/ \
-            /cbica/home/parkesl/miniconda3/envs/pfactor_gradients/bin/python \
-            /cbica/home/parkesl/research_projects/pfactor_gradients/scripts/run_prediction.py \
-            -X_name $X_name -y_name $y_name -c_name $c_name -alg $alg -score $score -runpca $runpca
-          done
-        done
-      done
-    done
-  done
-done
-########################################################################################################################
+/cbica/home/parkesl/research_projects/nct_hierarchy/scripts/ave_adj_generate_energy_bootstraps.py
