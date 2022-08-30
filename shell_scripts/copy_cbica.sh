8003f35c70d0b8e1231c0b4388f9bdbe2121165e
Linden Parkes <lindenparkes@gmail.com>
2022-08-30 16:22:32 -0400

    refactor for revisions

--- shell_scripts/copy_cbica.sh
+++ shell_scripts/copy_cbica.sh
@@ -1,13 +1,14 @@
-# rsync outputs from cbica
-rsync -avzh parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_projects/pfactor_gradients/output_cluster \
-/Users/lindenmp/Google-Drive-Penn/work/research_projects/pfactor_gradients/
+# rsync code to cbica
+rsync -avzh /Users/lindenmp/Google-Drive-Penn/work/research_projects/nct_hierarchy/src \
+parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_projects/nct_hierarchy/
+rsync -avzh /Users/lindenmp/Google-Drive-Penn/work/research_projects/nct_hierarchy/scripts \
+parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_projects/nct_hierarchy/
 
 # rsync data to cbica
-rsync -avzh /Volumes/T7/research_data/pnc/processedData \
-parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_data/pnc/
+rsync -avzh /Users/lindenmp/research_data \
+parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/
 
-rsync -avzh /Volumes/T7/research_data/parcellations \
-parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_data/
-
-rsync -avzh /Volumes/T7/research_data/fukutomi_2018ni_brain_maps \
-parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_data/
+# rsync outputs from cbica
+mkdir -p /Users/lindenmp/research_projects/nct_hierarchy/output_cluster
+rsync -avzh --exclude '*gmat*' parkesl@cubic-login.uphs.upenn.edu:/cbica/home/parkesl/research_projects/nct_hierarchy/output_cluster/ \
+/Users/lindenmp/research_projects/nct_hierarchy/output_cluster
