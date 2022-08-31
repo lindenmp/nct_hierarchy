dac323c16eeeb595680a72ba9585b771ea66f4e6
Linden Parkes <lindenparkes@gmail.com>
2022-08-31 17:25:36 -0400

    refactor for revisions

--- scripts/results_fig5.py
+++ scripts/results_fig5.py
@@ -1,24 +1,22 @@
 # %% import
 import sys, os, platform
-from pfactor_gradients.imaging_derivs import DataMatrix
-from pfactor_gradients.pipelines import ComputeMinimumControlEnergy
-from pfactor_gradients.plotting import my_reg_plot, my_distpair_plot, my_null_plot
-from pfactor_gradients.energy import expand_states
-from pfactor_gradients.utils import rank_int, get_null_p, get_bootstrap_indices, mean_confidence_interval, get_fdr_p
+from src.pipelines import ComputeMinimumControlEnergy
+from src.plotting import my_bsci_pair_plot
+from src.utils import rank_int, get_null_p, get_fdr_p
+from src.utils import get_bootstrap_indices
 
-import scipy as sp
+from brainsmash.mapgen.base import Base
 from tqdm import tqdm
 
 # %% import workspace
 os.environ["MY_PYTHON_WORKSPACE"] = 'ave_adj'
 os.environ["WHICH_BRAIN_MAP"] = 'hist-g2'
-# os.environ["WHICH_BRAIN_MAP"] = 'func-g1'
 from setup_workspace import *
 
 # %% plotting
 import seaborn as sns
 import matplotlib.pyplot as plt
-from pfactor_gradients.plotting import set_plotting_params
+from src.plotting import set_plotting_params
 set_plotting_params(format='svg')
 figsize = 1.5
 
@@ -56,160 +54,150 @@ nct_pipeline.run_with_optimized_b(n=n, ds=ds)
 E['E_opt'] = nct_pipeline.E_opt[:, 1].reshape(n_states, n_states)
 B_opt = nct_pipeline.B_opt[:, :, 1]
 
-# %% data for plotting
-norm_energy = True
-# B = 'identity'
-B = 'E_opt'
-print(B)
-e = E[B].copy()
+# %%
+e = E['identity'].copy()
+e = rank_int(e)  # normalized energy matrix
+ed = e - e.transpose()  # energy asymmetry matrix
 
-if norm_energy:
-    e = rank_int(e) # normalized energy matrix
+e = E['E_opt'].copy()
+e = rank_int(e)  # normalized energy matrix
+ed_opt = e - e.transpose()  # energy asymmetry matrix
 
-ed = e - e.transpose() # energy asymmetry matrix
-print(np.all(np.round(np.abs(ed.flatten()), 4) == np.round(np.abs(ed.transpose().flatten()), 4)))
+B_opt_matrix = np.zeros((n_states, n_states, n_parcels))
+row = 0
+col = 0
 
-# save out mean ed for use in other scripts
-np.save(os.path.join(environment.pipelinedir, 'e_{0}_{1}.npy'.format(which_brain_map, B)), e)
-np.save(os.path.join(environment.pipelinedir, 'ed_{0}_{1}.npy'.format(which_brain_map, B)), ed)
+n_transitions = n_states * n_states
+for i in tqdm(np.arange(n_transitions)):
+    B_opt_matrix[row, col, :] = B_opt[:, i].copy()
+    if col == n_states-1:
+        row += 1
+        col = 0
+    else:
+        col += 1
 
-# %% Panel B: energy asymmetry
-# top-down vs bottom-up
-f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
-df_plot = pd.DataFrame(data=np.vstack((e[indices_upper], e[indices_lower])).transpose(),
-                       columns=['bottom-up', 'top-down'])
-my_distpair_plot(df=df_plot, ylabel='energy (z-score)', ax=ax)
-f.savefig(os.path.join(environment.figdir, 'e_asym_{0}_dists'.format(B)), dpi=600, bbox_inches='tight',
-          pad_inches=0.01)
-plt.close()
+# %% generate surrogates using brainsmash
+n_surrogates = 10000
+file = 'brainsmash_surrogates_{0}_n{1}.npy'.format(which_brain_map, n_surrogates)
+if os.path.exists(os.path.join(environment.pipelinedir, file)) == False:
+    D = sp.spatial.distance.pdist(environment.centroids, 'euclidean')
+    D = sp.spatial.distance.squareform(D)
 
-# %% Panel C: bootstrap
-# n_samples = 1000
-n_samples = 10
-bootstrap_indices = get_bootstrap_indices(d_size=n_subs, n_samples=n_samples)
+    base = Base(x=state_brain_map, D=D, resample=True)
+    surrogates = base(n=n_surrogates)
 
+    np.save(os.path.join(environment.pipelinedir, file), surrogates)
+else:
+    surrogates = np.load(os.path.join(environment.pipelinedir, file))
+
+# %% correlate hierarchical distance with optimized weights
+observed = np.zeros((n_states, n_states))
+null = np.zeros((n_states, n_states, n_surrogates))
+p_vals = np.zeros((n_states, n_states))
+
+for i in tqdm(np.arange(n_states)):
+    for j in np.arange(n_states):
+        x0 = states == i
+        xf = states == j
+
+        bystanders = ~np.logical_or(x0, xf)
+
+        dist_from_x0 = sp.stats.rankdata(np.abs(state_brain_map[bystanders] - state_brain_map[x0].mean()))
+        dist_from_xf = sp.stats.rankdata(np.abs(state_brain_map[bystanders] - state_brain_map[xf].mean()))
+
+        optimized_weights = sp.stats.rankdata(B_opt_matrix[i, j, bystanders])
+
+        # observed from x0
+        obs_x0, _ = sp.stats.pearsonr(dist_from_x0, optimized_weights)
+
+        # observed from xf
+        obs_xf, _ = sp.stats.pearsonr(dist_from_xf, optimized_weights)
+
+        if np.abs(obs_x0) > np.abs(obs_xf):
+            use_x0 = True
+            observed[i, j] = obs_x0
+        else:
+            use_x0 = False
+            observed[i, j] = obs_xf
+
+        # null
+        for k in np.arange(n_surrogates):
+            if use_x0:
+                dist_surr = np.abs(surrogates[k, bystanders] - surrogates[k, x0].mean())
+            else:
+                dist_surr = np.abs(surrogates[k, bystanders] - surrogates[k, xf].mean())
+
+            null[i, j, k], _ = sp.stats.pearsonr(dist_surr, optimized_weights)
+
+        p_vals[i, j] = get_null_p(observed[i, j], null[i, j, :], version='standard', abs=True)
+
+# %% get bootstrapped energy asymmetries for both uniform and optimized control weights
+n_samples = 10000
 ed_bs = np.zeros(n_samples)
 ed_opt_bs = np.zeros(n_samples)
 
-# loop start
+bootstrap_indices = get_bootstrap_indices(d_size=n_subs, frac=1, n_samples=n_samples)
+
+# set pipelinedir to cluster outputs
+environment.pipelinedir = environment.pipelinedir.replace('output_local', 'output_cluster')
+
 for i in tqdm(np.arange(n_samples)):
     file_prefix = 'average_adj_n-{0}_cthr-{1}_smap-{2}_strap-{3}_'.format(load_average_sc.load_sc.df.shape[0],
-                                                                consist_thresh, which_brain_map, i)
+                                                                          consist_thresh, which_brain_map, i)
 
-    load_sc_strap = LoadSC(environment=environment, Subject=Subject)
-    load_sc_strap.df = load_sc.df.iloc[bootstrap_indices[i, :], :]
-    load_sc_strap.A = load_sc.A[:, :, bootstrap_indices[i, :]]
+    # load_sc_strap = LoadSC(environment=environment, Subject=Subject)
+    # load_sc_strap.df = load_sc.df.iloc[bootstrap_indices[i, :], :].copy()
+    # load_sc_strap.A = load_sc.A[:, :, bootstrap_indices[i, :]].copy()
+    #
+    # load_average_sc_strap = LoadAverageSC(load_sc=load_sc_strap, consist_thresh=consist_thresh, verbose=False)
+    # load_average_sc_strap.run()
+    #
+    # # get bootstrapped energy
+    # nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=load_average_sc_strap.A, states=states, B=B,
+    #                                            control='minimum_fast', c=c, T=T,
+    #                                            file_prefix=file_prefix,
+    #                                            force_rerun=False, save_outputs=False, verbose=False)
+    # n = 2
+    # ds = 0.1
+    # nct_pipeline.run_with_optimized_b(n=n, ds=ds)
+    data = np.load(os.path.join(environment.pipelinedir, 'minimum_control_energy',
+                                file_prefix + 'ns-20_ctrl-minimum_fast_c-1_T-1_B-optimized-n-2-ds-0.1_E.npy'))
 
-    load_average_sc_strap = LoadAverageSC(load_sc=load_sc_strap, consist_thresh=consist_thresh, verbose=False)
-    load_average_sc_strap.run()
-
-    # get bootstrapped energy
-    nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=load_average_sc_strap.A, states=states, B=B_dict['identity'],
-                                               control='minimum_fast', c=c, T=T,
-                                               file_prefix=file_prefix,
-                                               force_rerun=False, save_outputs=True, verbose=False)
-    n = 2
-    ds = 0.1
-    nct_pipeline.run_with_optimized_b(n=n, ds=ds)
-
-    e_strap = nct_pipeline.E_opt[:, 0].reshape(n_states, n_states)
+    # e_strap = nct_pipeline.E_opt[:, 0].reshape(n_states, n_states)
+    e_strap = data[:, 0].reshape(n_states, n_states)
     e_strap = rank_int(e_strap)  # normalized energy matrix
-    ed_strap = e_strap.transpose() - e_strap  # energy asymmetry matrix
+    ed_strap = e_strap - e_strap.transpose()  # energy asymmetry matrix
     ed_strap = np.mean(ed_strap[indices_upper])
     ed_bs[i] = ed_strap
 
-    e_opt_strap = nct_pipeline.E_opt[:, 1].reshape(n_states, n_states)
+    # e_opt_strap = nct_pipeline.E_opt[:, 1].reshape(n_states, n_states)
+    e_opt_strap = data[:, 1].reshape(n_states, n_states)
     e_opt_strap = rank_int(e_opt_strap)  # normalized energy matrix
-    ed_opt_strap = e_opt_strap.transpose() - e_opt_strap  # energy asymmetry matrix
+    ed_opt_strap = e_opt_strap - e_opt_strap.transpose()  # energy asymmetry matrix
     ed_opt_strap = np.mean(ed_opt_strap[indices_upper])
     ed_opt_bs[i] = ed_opt_strap
 
-# plot
-f, ax = plt.subplots(1, 1, figsize=(figsize*0.75, figsize))
-df_plot = pd.DataFrame(data=np.vstack((ed_bs, ed_opt_bs)).transpose(),
-                       columns=['identity', 'optimized'])
-my_distpair_plot(df=df_plot, ylabel='energy delta', ax=ax, test_stat=None, split=True)
-f.savefig(os.path.join(environment.figdir, 'e_asym_dists_bootstrap'), dpi=600, bbox_inches='tight',
-          pad_inches=0.01)
-plt.close()
+# reset pipelinedir to local outputs
+environment.pipelinedir = environment.pipelinedir.replace('output_cluster', 'output_local')
 
-print(np.round(mean_confidence_interval(data=ed_bs), 4))
-print(np.round(mean_confidence_interval(data=ed_opt_bs), 4))
-
-# %% Panel D: null for brain map spatial corrs
-x0_mat, xf_mat = expand_states(states)
-n_transitions = x0_mat.shape[1]
-
-n_perms = 5000
-# network_null = 'mni-wwp'
-network_null = 'mni-wsp'
-
-try:
-    r_null = np.load(os.path.join(environment.pipelinedir, 'optimized_weights_r_null_{0}_{1}.npy' \
-                                  .format(which_brain_map, network_null)))
-    p_vals = np.load(os.path.join(environment.pipelinedir, 'optimized_weights_p_vals_{0}_{1}.npy' \
-                                  .format(which_brain_map, network_null)))
-    observed = np.load(os.path.join(environment.pipelinedir, 'optimized_weights_observed_{0}_{1}.npy' \
-                                  .format(which_brain_map, network_null)))
-except:
-    B_opt_network_null = np.zeros((n_parcels, n_transitions, n_perms))
-
-    for i in tqdm(np.arange(n_perms)):
-        file = 'average_adj_n-{0}_cthr-{1}_smap-{2}_null-{3}-{4}_ns-{5}_ctrl-minimum_fast_c-{6}_T-{7}_B-optimized-n-2-ds-0.1_weights.npy' \
-            .format(n_subs,
-                    consist_thresh,
-                    which_brain_map,
-                    network_null,
-                    i, n_states, c, T)
-        B_opt_network_null[:, :, i] = np.load(os.path.join(environment.pipelinedir, 'minimum_control_energy', file))[:, :, 1]
-
-    observed = np.zeros(n_transitions)
-    r_null = np.zeros((n_transitions, n_perms))
-    p_vals = np.zeros(n_transitions)
-
-    for i in tqdm(np.arange(n_transitions)):
-        bystanders = ~np.logical_or(x0_mat[:, i], xf_mat[:, i])
-        x = sp.stats.rankdata(state_brain_map[bystanders])
-
-        B_bystanders = B_opt[bystanders, i]
-        B_bystanders = sp.stats.rankdata(B_bystanders)
-
-        # observed
-        observed[i], _ = sp.stats.pearsonr(x, B_bystanders)
-
-        # null
-        for j in np.arange(n_perms):
-            B_null = B_opt_network_null[bystanders, i, j]
-            r_null[i, j], _ = sp.stats.pearsonr(x, sp.stats.rankdata(B_null))
-
-        p_vals[i] = get_null_p(observed[i], r_null[i, :], version='smallest', abs= False)
-
-    np.save(os.path.join(environment.pipelinedir, 'optimized_weights_r_null_{0}_{1}.npy'.format(which_brain_map, network_null)), r_null)
-    np.save(os.path.join(environment.pipelinedir, 'optimized_weights_p_vals_{0}_{1}.npy'.format(which_brain_map, network_null)), p_vals)
-    np.save(os.path.join(environment.pipelinedir, 'optimized_weights_observed_{0}_{1}.npy'.format(which_brain_map, network_null)), observed)
-
-# plot
-f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
-observed_mean = np.mean(np.abs(observed))
-r_null_mean = np.mean(np.abs(r_null), axis=0)
-p_val = get_null_p(observed_mean, r_null_mean, abs=True)
-my_null_plot(observed=observed_mean, null=r_null_mean, p_val=p_val, xlabel='spatial corr.\n(null network)', ax=ax)
-f.savefig(os.path.join(environment.figdir, 'corr(smap,B_opt)_null_{0}'.format(network_null)), dpi=600, bbox_inches='tight', pad_inches=0.01)
-plt.close()
-
-# %% Figure S3
-
+# %% Panel B: correlations
+f, ax = plt.subplots(1, 1, figsize=(figsize*1.75, figsize*1.75))
 p_vals_fdr = get_fdr_p(p_vals)
 sig_mask = p_vals_fdr > 0.05
-
-f, ax = plt.subplots(1, 1, figsize=(figsize*1.2, figsize*1.2))
-sns.heatmap(observed.reshape(n_states, n_states), mask=sig_mask.reshape(n_states, n_states),
-# sns.heatmap(observed.reshape(n_states, n_states),
-            center=0, square=True, cmap='coolwarm', ax=ax, cbar_kws={"shrink": 0.80})
+sns.heatmap(observed, mask=sig_mask, center=0, square=True, cmap='coolwarm', ax=ax, cbar_kws={"shrink": 0.80})
 ax.set_ylabel("initial states", labelpad=-1)
 ax.set_xlabel("target states", labelpad=-1)
 ax.set_yticklabels('')
 ax.set_xticklabels('')
-ax.tick_params(pad=-2.5)
-f.savefig(os.path.join(environment.figdir, 'corr(B_opt,brainmap)'), dpi=600, bbox_inches='tight', pad_inches=0.01)
+ax.tick_params(pad=-1)
+f.savefig(os.path.join(environment.figdir, 'corr(observed,brainmap).svg'), dpi=600, bbox_inches='tight', pad_inches=0.01)
+plt.close()
+
+# %% Panel C: bootstrap
+f, ax = plt.subplots(1, 1, figsize=(figsize*2.5, figsize*1.25))
+my_bsci_pair_plot(np.abs(ed_bs), np.abs(np.mean(ed[indices_upper])),
+                  np.abs(ed_opt_bs), np.abs(np.mean(ed_opt[indices_upper])),
+                  xlabel='absolute mean asymmetry (bootstrap)', ax=ax)
+f.savefig(os.path.join(environment.figdir, 'e_ed_asym_bootstrap.svg'), dpi=600, bbox_inches='tight',
+          pad_inches=0.01)
 plt.close()
