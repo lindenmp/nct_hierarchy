9596c7ca9c44061830959feb77f8e3a9258e114e
Linden Parkes <lindenparkes@gmail.com>
2022-08-31 17:27:49 -0400

    refactor for revisions

--- scripts/results_brainmap_corrs.py
+++ scripts/results_brainmap_corrs.py
@@ -1,42 +1,66 @@
 # %% import
 import sys, os, platform
-from pfactor_gradients.imaging_derivs import DataMatrix
+from src.plotting import my_null_plot
+from src.utils import get_null_p
 
-import scipy as sp
+from brainsmash.mapgen.base import Base
+from tqdm import tqdm
 
 # %% import workspace
 os.environ["MY_PYTHON_WORKSPACE"] = 'ave_adj'
 os.environ["WHICH_BRAIN_MAP"] = 'hist-g2'
-# os.environ["WHICH_BRAIN_MAP"] = 'func-g1'
 from setup_workspace import *
 
 # %% plotting
 import seaborn as sns
 import matplotlib.pyplot as plt
-from pfactor_gradients.plotting import set_plotting_params
+from src.plotting import set_plotting_params
 set_plotting_params(format='svg')
 figsize = 1.5
 
-# %% brain maps
-A_tmp = DataMatrix(data=A)
-A_tmp.get_strength()
+# %% generate surrogates using brainsmash
+n_surrogates = 10000
+file = 'brainsmash_surrogates_{0}_n{1}.npy'.format(which_brain_map, n_surrogates)
+if os.path.exists(os.path.join(environment.pipelinedir, file)) == False:
+    D = sp.spatial.distance.pdist(environment.centroids, 'euclidean')
+    D = sp.spatial.distance.squareform(D)
 
-for key in brain_maps:
-    nan_mask = np.isnan(brain_maps[key].data)
+    base = Base(x=state_brain_map, D=D, resample=True)
+    surrogates = base(n=n_surrogates)
 
-    print('state_brain_map vs. {0}'.format(key),
-          sp.stats.pearsonr(state_brain_map[~nan_mask], brain_maps[key].data[~nan_mask]))
-    print('strength vs. {0}'.format(key),
-          sp.stats.pearsonr(A_tmp.S[~nan_mask], brain_maps[key].data[~nan_mask]))
+    np.save(os.path.join(environment.pipelinedir, file), surrogates)
+else:
+    surrogates = np.load(os.path.join(environment.pipelinedir, file))
 
-    # plot brain map
-    brain_maps[key].brain_surface_plot(environment)
 
-print('strength vs. state_brain_map', sp.stats.pearsonr(A_tmp.S, state_brain_map))
+# %% correlation(cyto,micro)
+observed, _ = sp.stats.pearsonr(state_brain_map, brain_map_loader.micro)
+null = np.zeros(n_surrogates)
 
-# plot state brain map
-DataVector(data=state_brain_map, name='state_brain_map').brain_surface_plot(environment)
-DataVector(data=states == 0, name='state_0').brain_surface_plot(environment)
-DataVector(data=states == int(n_states/2), name='state_{0}'.format(int(n_states/2))).brain_surface_plot(environment)
-DataVector(data=states == n_states-1, name='state_{0}'.format(int(n_states-1))).brain_surface_plot(environment)
+for i in tqdm(np.arange(n_surrogates)):
+    null[i], _ = sp.stats.pearsonr(surrogates[i, :], brain_map_loader.micro)
 
+p_val = get_null_p(observed, null, version='standard', abs=True)
+print(p_val)
+
+f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
+my_null_plot(observed=observed, null=null, p_val=p_val, xlabel=' ', ax=ax)
+f.savefig(os.path.join(environment.figdir, 'spat_corr(cyto,micro).png'), dpi=600,
+          bbox_inches='tight', pad_inches=0.01)
+plt.close()
+
+# %% correlation(cyto,func)
+observed, _ = sp.stats.pearsonr(state_brain_map, compute_gradients.gradients[:, 0])
+null = np.zeros(n_surrogates)
+
+for i in tqdm(np.arange(n_surrogates)):
+    null[i], _ = sp.stats.pearsonr(surrogates[i, :], compute_gradients.gradients[:, 0])
+
+p_val = get_null_p(observed, null, version='standard', abs=True)
+print(p_val)
+
+f, ax = plt.subplots(1, 1, figsize=(figsize, figsize))
+my_null_plot(observed=observed, null=null, p_val=p_val, xlabel=' ', ax=ax)
+f.savefig(os.path.join(environment.figdir, 'spat_corr(cyto,func).png'), dpi=600,
+          bbox_inches='tight', pad_inches=0.01)
+plt.close()
