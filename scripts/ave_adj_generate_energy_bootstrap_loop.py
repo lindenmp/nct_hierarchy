76bd6eb61b23d7e97b80315ba86ace0aeebf44e5
Linden Parkes <lindenparkes@gmail.com>
2022-09-27 14:29:26 -0400

    bootstrapped averaged A matrix now saved out

--- scripts/ave_adj_generate_energy_bootstrap_loop.py
+++ scripts/ave_adj_generate_energy_bootstrap_loop.py
@@ -18,9 +18,15 @@ from setup_workspace import *
 B = DataMatrix(data=np.eye(n_parcels), name='identity')
 c = 1
 T = 1
+frac = 0.5
 
-E_file = 'average_adj_n-{0}_cthr-{1}_smap-{2}_bootstrapped_E'.format(load_average_sc.load_sc.df.shape[0],
-                                                                     consist_thresh, which_brain_map)
+A_file = 'average_adj_n-{0}_cthr-{1}_smap-{2}_bootstrapped_frac{3}_Am'.format(load_average_sc.load_sc.df.shape[0],
+                                                                              consist_thresh, which_brain_map,
+                                                                              str(frac).replace('.', ''))
+
+E_file = 'average_adj_n-{0}_cthr-{1}_smap-{2}_bootstrapped_frac{3}_E'.format(load_average_sc.load_sc.df.shape[0],
+                                                                             consist_thresh, which_brain_map,
+                                                                             str(frac).replace('.', ''))
 
 if os.path.exists(os.path.join(environment.pipelinedir, 'minimum_control_energy', E_file+'.npy')):
     print('loading bootstrapped energy')
@@ -28,10 +34,11 @@ if os.path.exists(os.path.join(environment.pipelinedir, 'minimum_control_energy'
     n_samples = e_bs.shape[2]
 else:
     print('generating bootstrapped energy')
-    n_samples = 10000
+    n_samples = 1000
+    Am_bs = np.zeros((n_parcels, n_parcels, n_samples))
     e_bs = np.zeros((n_states, n_states, n_samples))
 
-    bootstrap_indices = get_bootstrap_indices(d_size=n_subs, frac=1, n_samples=n_samples)
+    bootstrap_indices = get_bootstrap_indices(d_size=n_subs, frac=frac, n_samples=n_samples)
 
     for i in tqdm(np.arange(n_samples)):
         file_prefix = 'average_adj_n-{0}_cthr-{1}_smap-{2}_strap-{3}_'.format(load_average_sc.load_sc.df.shape[0],
@@ -43,6 +50,7 @@ else:
 
         load_average_sc_strap = LoadAverageSC(load_sc=load_sc_strap, consist_thresh=consist_thresh, verbose=False)
         load_average_sc_strap.run()
+        Am_bs[:, :, i] = load_average_sc_strap.A
 
         # get bootstrapped energy
         nct_pipeline = ComputeMinimumControlEnergy(environment=environment, A=load_average_sc_strap.A, states=states, B=B,
@@ -53,4 +61,5 @@ else:
         e_bs[:, :, i] = nct_pipeline.E
 
     # save
+    np.save(os.path.join(environment.pipelinedir, 'minimum_control_energy', A_file), Am_bs)
     np.save(os.path.join(environment.pipelinedir, 'minimum_control_energy', E_file), e_bs)
